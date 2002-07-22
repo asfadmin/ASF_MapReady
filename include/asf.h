775f1239f9d193e7fb4557b969c574fdaeb35661
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-07-22 23:27:13 +0000

    Added NAN macro

--- include/asf.h
+++ include/asf.h
@@ -20,6 +20,22 @@
 #define D2R (PI/180.0)
 #define R2D (180.0/PI)
 
+/* Create NAN (Not a Number) since it is not in a standard library.
+ * Conforms to ieee NaN.  Under most architectures will be interpreted
+ * as a signalling NaN (SNaN). SNaN should signal trapping operations,
+ * but to date (July 2002) I am unaware of any operations that trap for
+ * NaN whatsoever. Therefore this NaN should behave like a quiet NaN (QNaN)
+ * and will propagate through equations until it's used in a condional
+ * statement (which should return False) */
+#if defined (lil_endian)
+# define __nan_bytes { 0xff, 0xff, 0xbf, 0x7f }
+#else /*defined (big_endian)*/
+# define __nan_bytes { 0x7f, 0xbf, 0xff, 0xff }
+#endif
+static union { unsigned char __c[4]; float __d; } __nan_union = { __nan_bytes };
+#define NAN __nan_union.__d
+
+
 /* Print an error with printf-style formatting codes and args, then die.  */
 void bail(const char *message, ...)/* ; is coming, don't worry.  */
 /* The GNU C compiler can give us some special help if compiling with
