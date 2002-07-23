0ae3f6182c6ea56d48922b8e0e4fac3340014efe
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-07-23 22:55:25 +0000

    name changes that should have been there long ago

--- src_lib/asf/cla.c
+++ src_lib/asf/cla.c
@@ -5,11 +5,11 @@ int currArg;		/* Points to current command line arg */
 
 /*strmatch is like strcmp, but it permits partial matching (e.g. "-rot" matches "-rotate"),
    and returns a one if the strings match, 0 otherwise.*/
-int strmatch(const char *a, const char *b)
+int strmatch(const char *key, const char *match)
 {
 	int ii;
-	for (ii=0;/*a[ii]&&*/b[ii];ii++)
-		if (a[ii]!=b[ii])
+	for (ii=0;/*key[ii]&&*/match[ii];ii++)
+		if (key[ii]!=match[ii])
 			return 0;
 	return 1;
 }
