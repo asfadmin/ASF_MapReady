afafd6a8269f58acc7814d373f1970f31b60429c
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-07-29 18:01:31 +0000

    Got rid of final_init function.

--- src_lib/asf_meta/meta_init.c
+++ src_lib/asf_meta/meta_init.c
@@ -17,10 +17,8 @@ PROGRAM HISTORY:
 #include "asf_meta.h"
 
 
-/*Raw_init_state:
-	Creates a state_vectors structure big
-enough to hold the given number of state vectors.
-*/
+/* Raw_init_state: Creates a meta_state_vectors array big enough to
+   hold the given number of state vectors.  */
 meta_state_vectors *raw_init_state(int nState)
 {
 	meta_state_vectors *s;
@@ -30,94 +28,83 @@ meta_state_vectors *raw_init_state(int nState)
 	return s;
 }
 
-/*raw_init:
-	Creates and initializes a meta_parameters structure, guessing at
-conceivable values.  These bogus values always end in "989", so you can
-tell them from real values.
-*/
+/* Creates and initializes a meta_parameters structure, guessing at
+   conceivable values.  These bogus values always end in "989", so you
+   can tell them from real values.  */
 meta_parameters *raw_init(void)
 {
-/*Create new structure.*/
-	meta_parameters *meta = (meta_parameters *)MALLOC(sizeof(meta_parameters));
-	meta->general         = (meta_general *)MALLOC(sizeof(meta_general));
-	meta->sar             = NULL;
-	meta->optical         = NULL;  /* Not yet in use */
-	meta->thermal         = NULL;  /* Not yet in use */
-	meta->projection      = NULL;
-	meta->stats           = NULL;
-	meta->state_vectors   = NULL;
-	meta->geo  = NULL; /* DEPRECATED */
-	meta->ifm  = NULL; /* DEPRECATED */
-	meta->info = NULL; /* DEPRECATED */
+  meta_parameters *meta = MALLOC(sizeof(meta_parameters));
+  meta->general         = MALLOC(sizeof(meta_general));
+  meta->sar             = MALLOC(sizeof(meta_sar));
+  meta->optical         = NULL;  /* Not yet in use */
+  meta->thermal         = NULL;  /* Not yet in use */
+  meta->projection      = MALLOC(sizeof(meta_projection));
+  meta->stats           = NULL;  /* Not yet in use */
+  meta->state_vectors   = MALLOC(sizeof(meta_state_vectors));
+  meta->state_vectors->vecs = NULL;
 
-/*Fill with ludicrous values.*/
-	meta->general->sensor[0] = '\0';
-	meta->general->mode[0] = '\0';
-	meta->general->processor[0] = '\0';
-	meta->general->data_type[0] = '\0';
-	meta->general->system[0] = '\0';
-	meta->general->orbit -1;
-	meta->general->frame = -1;
-	meta->general->orbit_direction = '\0';
-	meta->general->line_count = -1;
-	meta->general->sample_count = -1;
-	meta->general->start_line = -1;
-	meta->general->start_sample = -1;
-	meta->general->xPix = NAN;
-	meta->general->yPix = NAN;
-	meta->general->center_latitude = NAN;
-	meta->general->center_longitude = NAN;
-	meta->general->re_major = NAN;
-	meta->general->re_minor = NAN;
-	meta->general->bit_error_rate = NAN;
-	meta->general->missing_lines = -1;
-	
-	return meta;
-}
+  /* Fill with ludicrous values.  */
+  meta->general->sensor[0] = '\0';
+  meta->general->mode[0] = '\0';
+  meta->general->processor[0] = '\0';
+  meta->general->data_type[0] = '\0';
+  meta->general->system[0] = '\0';
+  meta->general->orbit = -1;
+  meta->general->frame = -1;
+  meta->general->orbit_direction = '\0';
+  meta->general->line_count = -1;
+  meta->general->sample_count = -1;
+  meta->general->start_line = -1;
+  meta->general->start_sample = -1;
+  meta->general->xPix = NAN;
+  meta->general->yPix = NAN;
+  meta->general->center_latitude = NAN;
+  meta->general->center_longitude = NAN;
+  meta->general->re_major = NAN;
+  meta->general->re_minor = NAN;
+  meta->general->bit_error_rate = NAN;
+  meta->general->missing_lines = -1;
 
-/*raw_init_old:
-	Creates and initializes a meta_parameters
-structure, guessing at conceivable values.
-These bogus values always end in "989", so
-you can tell them from real values.
-*/
-meta_parameters *raw_init_old(void)
-{
-	
-/*Create new structure.*/
-#define newStruct(type) (type *)MALLOC(sizeof(type))
-	meta_parameters *sar=newStruct(meta_parameters);
-	sar->geo=newStruct(geo_parameters);
-	sar->ifm=newStruct(ifm_parameters);
-	sar->stVec=NULL;
-	sar->info=NULL;
+  meta->geo  = NULL; /* DEPRECATED */
+  meta->ifm  = NULL; /* DEPRECATED */
+  meta->info = NULL; /* DEPRECATED */
 
-/*Guess at conceivable values.*/
-	sar->geo->type='G';
-	sar->geo->proj=NULL;
-	sar->geo->lookDir='R';
-	sar->geo->deskew=0;
-	sar->geo->xPix=sar->geo->yPix=10.989;
-	sar->geo->rngPixTime=5.989e-8;
-	sar->geo->azPixTime=7.989e-4;
-	sar->geo->timeShift=sar->geo->slantShift=0;
-	sar->geo->slantFirst=800000.989;
-	sar->geo->wavelen=0.056989;
-	sar->geo->dopRange[0]=
-	sar->geo->dopRange[1]=
-	sar->geo->dopRange[2]=0.0;
-	sar->geo->dopAz[0]=
-	sar->geo->dopAz[1]=
-	sar->geo->dopAz[2]=0.0;
-	
-	sar->ifm->er=6360000.989;
-	sar->ifm->ht=sar->ifm->er+700000.0;
-	sar->ifm->nLooks=5;
-	sar->ifm->orig_nLines=25989;
-	sar->ifm->orig_nSamples=5989;
-	sar->ifm->lookCenter=19.989;
+/* Initialize deprecated structure elements: Creates and initializes a
+   meta_parameters structure, guessing at conceivable values.  These
+   bogus values always end in "989", so you can tell them from real
+   values.  */
 
-	return sar;
+  meta->geo = MALLOC(sizeof(geo_parameters));
+  meta->ifm = MALLOC(sizeof(ifm_parameters));
+  meta->stVec=NULL;
+  meta->info=NULL;
+  
+  /* Guess at conceivable values for deprecated elements.  */
+  meta->geo->type = 'G';
+  meta->geo->proj = NULL;
+  meta->geo->lookDir = 'R';
+  meta->geo->deskew = 0;
+  meta->geo->xPix = meta->geo->yPix = 10.989;
+  meta->geo->rngPixTime = 5.989e-8;
+  meta->geo->azPixTime = 7.989e-4;
+  meta->geo->timeShift = meta->geo->slantShift = 0;
+  meta->geo->slantFirst = 800000.989;
+  meta->geo->wavelen = 0.056989;
+  meta->geo->dopRange[0] = NAN;
+  meta->geo->dopRange[1] = NAN;
+  meta->geo->dopRange[2] = NAN;
+  meta->geo->dopAz[0] = NAN;
+  meta->geo->dopAz[1] = NAN;
+  meta->geo->dopAz[2] = NAN;
+  
+  meta->ifm->er = 6360000.989;
+  meta->ifm->ht = meta->ifm->er+700000.0;
+  meta->ifm->nLooks = 5;
+  meta->ifm->orig_nLines = 25989;
+  meta->ifm->orig_nSamples = 5989;
+  meta->ifm->lookCenter = 19.989;
+  
+  return meta;
 }
 
 /*meta_init_old: 
@@ -138,26 +125,33 @@ meta_parameters *meta_init_old(const char *fName)
 }
 meta_parameters *meta_init(const char *fName)
 {
-	return meta_init_old(fName);
+  meta_parameters *meta = raw_init(); /* Allocate empty metadata structure.  */
+
+  
+  return meta_init_old(fName);
 }
 
-/*meta_free:
-	Disposes of a given metadata parameters record.
-*/
-void meta_free_old(meta_parameters *meta)
-{
-	if (meta->geo->proj!=NULL)
-		free((void *)meta->geo->proj);
-	free((void *)meta->geo);
-	free((void *)meta->ifm);
-	if (meta->stVec!=NULL)
-		free((void *)meta->stVec);
-	meta->geo=NULL;
-	meta->ifm=NULL;
-	meta->stVec=NULL;
-	free((void *)meta);
-}
+/*  Disposes of a given metadata parameters record.  */
 void meta_free(meta_parameters *meta)
 {
-	meta_free_old(meta);
+  free(meta->general);
+  free(meta->sar);
+  free(meta->projection);
+  /* If any state vectors were allocated, free them.  */
+  if ( meta->state_vectors->num ) 
+    free(meta->state_vectors->vecs);
+  free(meta->state_vectors);
+
+  /* Dispose of deprecated structure elements, if they were ever
+     allocated.  */
+  if (meta->geo->proj!=NULL)
+    free(meta->geo->proj);
+  free(meta->geo);
+  free(meta->ifm);
+  if (meta->stVec!=NULL)
+    free(meta->stVec);
+  meta->geo=NULL;
+  meta->ifm=NULL;
+  meta->stVec=NULL;
+  free(meta);
 }
