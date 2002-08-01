5d2fed250d870556e1f431007f3bfdee29d52740
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-01 01:29:27 +0000

    New file containing unit tests for this library.

---
+++ src_lib/asf_meta/unit_tests.c
@@ -0,0 +1,41 @@
+/* Unit tests for the asf_meta library.  Not complete by any means,
+   but hopefully new code will use them at least.  */
+
+/* Note that these tests have wired in the values from the test meta
+   file.  */
+
+#include <asf_meta.h>
+#include <check.h>		/* For unit check functions.  */
+
+/* Over-safe micron for test floating point comparisons.  */
+#define UNIT_TESTS_MICRON 0.00001
+#define UNIT_TESTS_FLOAT_COMPARE(a, b) (abs(a - b) < UNIT_TESTS_MICRON ? 1 : 0)
+
+/* Test part of meta_read that parses new files.  */
+START_TEST(test_meta_read_new_format)
+{
+  meta_parameters *meta;
+  
+  meta_read(meta, "test_file.meta");
+  
+  /* Check a random fields to make sure things are working.  */
+  fail_unless(meta->general->orbit == 123, 
+	      "orbit field from general block not read correctly");
+  
+  /* Check a not-so-random field: things from projection params block
+     are currently partly holdover from deprecated code and use a
+     union.  */
+  fail_unless(meta->projection->type == 'A' 
+	      && meta->projection.param.atct.alpha1 == 0.6,
+	      "alpha1 field from param->atct block not read correctly");
+
+  /* Another not-so-random field check: state vector blocks currently
+     use wierd field names in the data file and map strangely into a
+     dynamicly allocated internal structure, lots of possibility for
+     error.  */
+  fail_unless( UNIT_TESTS_FLOAT_COMPARE(meta->state_vectors.vecs[1].pos.y, 
+					22333),
+	       "Y position element of second state vector not read correctly");
+
+  meta_free(meta);
+}
