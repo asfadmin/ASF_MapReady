20b7b6fd7eeb69ac6c741120433f230b60117f76
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-06 20:39:33 +0000

    Added a define for maximum file name length allowed in parser.

--- src_lib/asf_meta/lex_yacc.h
+++ src_lib/asf_meta/lex_yacc.h
@@ -8,4 +8,7 @@
 #define MAX_ERROR_STRING 1000
 #define MAX_SYMBOL_STRING 1000
 
+/* Maximum length of a file name to be handles by the parser.  */
+#define MAX_FILE_NAME 256
+
 #endif /* not __LEX_YACC__ */
