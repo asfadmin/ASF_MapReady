97c34690b57d4413931cf4cfda8d290bc20950fc
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-07 21:50:52 +0000

    First cut at a test for meta_write (which because I couldn't figure a better reasonably quick way, also requires a meta_read call).

--- src_lib/asf_meta/unit_tests.c
+++ src_lib/asf_meta/unit_tests.c
@@ -27,19 +27,40 @@ START_TEST(test_meta_read_new_format)
   fail_unless(meta->projection->type == 'A' 
 	      && meta->projection->param.atct.alpha1 == 0.6,
 	      "alpha1 field from param->atct block not read correctly");
-
+  
   /* Another not-so-random field check: state vector blocks currently
      use wierd field names in the data file and map strangely into a
      dynamicly allocated internal structure, lots of possibility for
      error.  */
-  fail_unless( UNIT_TESTS_FLOAT_COMPARE(meta->state_vectors
-					      ->vecs[1].vec.pos.y, 
-					22333),
-	       "Y position element of second state vector not read correctly");
+  fail_unless(UNIT_TESTS_FLOAT_COMPARE(meta->state_vectors
+				             ->vecs[1].vec.pos.y, 
+				       22333),
+	      "Y position element of second state vector not read correctly");
 
   meta_free(meta);
 }
 END_TEST
+
+/* Test routine which writes new-style meta files.  */
+START_TEST(test_meta_read_write_new_format)
+{
+  /* Get something to write.  */
+  meta_parameters *meta_in = meta_read("test_file_new_style.meta");
+  meta_parameters *meta_reread;	/* For re-read structure.  */
+  
+  meta_write(meta, "test_output_file.meta");
+  meta_read(meta_reread, "test_output_file.meta");
+  
+  /* Test a few random or not-so-random fields for equality.  */
+  fail_unless(!strcmp(meta->general->sensor, meta_reread->general->sensor),
+	      "sensor elements from original and written-then-reread metadata structures don't match");
+  fail_unless(meta->projection->type == meta_reread->projection->type,
+	      "projection->type fields from original and written-then-reread metadata structures not equal");
+  fail_unless(UNIT_TESTS_FLOAT_COMPARE(meta->projection->param.atct.rlocal,
+				       meta_reread->projection
+				                    ->param.atct.rlocal));
+}
+ENT_TEST
 
 /* Machinery for running the 'check' tests.  */
 Suite *asf_meta_suite(void)
