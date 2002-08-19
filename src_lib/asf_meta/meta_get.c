f5b830c3b8b54d2db9fcff078229002aab4a01af
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-19 22:44:03 +0000

    Updated all old structure references to new ones, including calls to meta_get_(earth_radius or sat_height) instead of meta->ifm->(er or ht)

--- src_lib/asf_meta/meta_get.c
+++ src_lib/asf_meta/meta_get.c
@@ -21,119 +21,131 @@ PROGRAM HISTORY:
 #endif
 
 /*General Calls:*/
-/* Convert a line, sample pair to a time, slant-range pair.
-These only use the geo_parameters structure, and only work 
-for SR and GR images.  They apply the time and slant range 
-correction fudge factors. Returns seconds and meters.
-*/
-double meta_get_time(meta_parameters *sar,double yLine, double xSample)
+/************************************************************
+ * meta_get_time:
+ * Convert a line, sample pair to a time, slant-range pair.
+ * These only use the geo_parameters structure, and only work 
+ * for SR and GR images.  They apply the time and slant range 
+ * correction fudge factors. Returns seconds and meters.*/
+double meta_get_time(meta_parameters *meta,double yLine, double xSample)
 {
-	if (sar->geo->type=='S'||
-		sar->geo->type=='G')/*Slant or ground range-- easy.*/
-		return yLine*sar->geo->azPixTime+sar->geo->timeShift;
-	else if (sar->geo->type=='P')
+    /*Slant or ground range -- easy.*/
+	if (meta->sar->image_type=='S' || meta->sar->image_type=='G')
+		return yLine*meta->sar->azimuth_time_per_pixel+meta->sar->time_shift;
+    /*Map projected -- not as easy.*/
+	else if (meta->sar->image_type=='P')
 	{
 		double time,slant;
-		meta_get_timeSlantDop(sar,yLine,xSample,&time,&slant,NULL);
-		return time+sar->geo->timeShift;
+		meta_get_timeSlantDop(meta,yLine,xSample,&time,&slant,NULL);
+		return time + meta->sar->time_shift;
 	}
 	else /*Unknown projection type.*/
 	{
 		printf("Error!  Unknown projection type '%c' passed to meta_get_slant!\n",
-			sar->geo->type);
+			meta->sar->image_type);
 		exit(1);
 	}
 	return 0.0;/*<- for whining compilers.*/
 }
-double meta_get_slant(meta_parameters *sar,double yLine, double xSample)
+double meta_get_slant(meta_parameters *meta,double yLine, double xSample)
 {
-	if (sar->geo->type=='S')/*Slant range is easy.*/
-		return sar->geo->slantFirst+xSample*sar->geo->xPix+sar->geo->slantShift;
-	else if (sar->geo->type=='G')/*Ground range is tougher.*/
+	if (meta->sar->image_type=='S')/*Slant range is easy.*/
+		return meta->sar->slant_range_first_pixel
+			+ xSample * meta->general->x_pixel_size
+			+ meta->sar->slant_shift;
+	else if (meta->sar->image_type=='G')/*Ground range is tougher.*/
 	{/*We figure out the ground angle, phi, for this pixel, then
 	use that and the law of cosines to find the slant range.*/
-		double ht=sar->ifm->ht,er=sar->ifm->er;
-		double minPhi=acos((SQR(ht)+SQR(er)-SQR(sar->geo->slantFirst))/
-			(2.0*ht*er));
-		double phi=minPhi+xSample*(sar->geo->xPix/sar->ifm->er);
-		double slantRng=sqrt(SQR(ht)+SQR(er)-2.0*ht*er*cos(phi));
-		return slantRng+sar->geo->slantShift;
+		double er = meta_get_earth_radius(meta,yLine,xSample);
+		double ht = meta_get_sat_height(meta,yLine,xSample);
+		double minPhi = acos((SQR(ht)+SQR(er)
+			- SQR(meta->sar->slant_range_first_pixel)) / (2.0*ht*er));
+		double phi = minPhi+xSample*(meta->general->x_pixel_size / er);
+		double slantRng = sqrt(SQR(ht)+SQR(er)-2.0*ht*er*cos(phi));
+		return slantRng + meta->sar->slant_shift;
 	} 
-	else if (sar->geo->type=='P')/*Map projected images are tougher.*/
+	else if (meta->sar->image_type=='P')/*Map projected images are tougher.*/
 	{
 		double time,slant;
-		meta_get_timeSlantDop(sar,yLine,xSample,&time,&slant,NULL);
-		return slant+sar->geo->slantShift;
+		meta_get_timeSlantDop(meta,yLine,xSample,&time,&slant,NULL);
+		return slant+meta->sar->slant_shift;
 	}
 	else /*Unknown projection type.*/
 	{
 		printf("Error!  Unknown projection type '%c' passed to meta_get_slant!\n",
-			sar->geo->type);
+			meta->sar->image_type);
 		exit(1);
 	}
 	return 0.0;/*<- for whining compilers.*/
 }
 
-/*Converts a line, sample pair to the doppler value
-at that location. Returns Hz.  Only works for SR & GR.
-*/
-double meta_get_dop(meta_parameters *sar,double yLine, double xSample)
+/*******************************************************
+ * meta_get_dop:
+ * Converts a line, sample pair to the doppler value at
+ * that location. Returns Hz. Only works for SR & GR. */
+double meta_get_dop(meta_parameters *meta,double yLine, double xSample)
 {
-	return sar->geo->dopRange[0]+
-	       sar->geo->dopRange[1]*xSample+
-	       sar->geo->dopRange[2]*xSample*xSample+
-	       sar->geo->dopAz[1]*yLine+
-	       sar->geo->dopAz[2]*yLine*yLine;
+	return meta->sar->range_doppler_coefficients[0]+
+	       meta->sar->range_doppler_coefficients[1]*xSample+
+	       meta->sar->range_doppler_coefficients[2]*xSample*xSample+
+	       meta->sar->azimuth_doppler_coefficients[1]*yLine+
+	       meta->sar->azimuth_doppler_coefficients[2]*yLine*yLine;
 }
 
 
-/*Return fixed-earth state vector for the given time.
-Steps through state vector list; then interpolates the
-right pair.*/
-stateVector meta_get_stVec(meta_parameters *sar,double time)
+/**********************************************************
+ * meta_get_stVec:
+ * Return fixed-earth state vector for the given time.
+ * Steps through state vector list; then interpolates the
+ * right pair.*/
+stateVector meta_get_stVec(meta_parameters *meta,double time)
 {
 	int stVecNo;
 	stateVector ret;
-	if (sar->stVec==NULL)
+	if (meta->state_vectors==NULL)
 	{
 		printf("Error! Requested a state vector, but"
 			"no state vectors exist in the file!\n");
 		exit(1);
 	}
-	if (sar->stVec->num<2)
+	if (meta->state_vectors->vector_count<2)
 	{
 		printf("Error! Only %d state vector exist in file!\n",
-			sar->stVec->num);
+			meta->state_vectors->vector_count);
 		exit(1);
 	}
 	stVecNo=0;
-	while (stVecNo<sar->stVec->num-2
-		&& sar->stVec->vecs[stVecNo+1].time<time)
+	while (stVecNo < meta->state_vectors->vector_count - 2
+		&& meta->state_vectors->vecs[stVecNo+1].time<time)
 		stVecNo++;
 	
-	interp_stVec(&sar->stVec->vecs[stVecNo].vec,
-			sar->stVec->vecs[stVecNo].time,
-			&sar->stVec->vecs[stVecNo+1].vec,
-			sar->stVec->vecs[stVecNo+1].time,
+	interp_stVec(&meta->state_vectors->vecs[stVecNo].vec,
+			 meta->state_vectors->vecs[stVecNo].time,
+			&meta->state_vectors->vecs[stVecNo+1].vec,
+			 meta->state_vectors->vecs[stVecNo+1].time,
 			&ret,time);
 	return ret;
 }
-/*Return the incidence angle: this is the angle measured
-	by the target between straight up and the satellite.
-	Returns radians.*/
-double meta_incid(meta_parameters *sar,double y,double x)
+/**********************************************************
+ * meta_incid:  Returns the incidence angle
+ * This is the angle measured by the target between straight
+ * up and the satellite. Returns radians.*/
+double meta_incid(meta_parameters *meta,double y,double x)
 {
-	double sr=meta_get_slant(sar,y,x);
-	return PI-acos((SQR(sr) + SQR(sar->ifm->er) - 
-		SQR(sar->ifm->ht) ) / (2.0*sr*sar->ifm->er));
+	double sr = meta_get_slant(meta,y,x);
+	double er = meta_get_earth_radius(meta,y,x);
+	double ht = meta_get_sat_height(meta,y,x);
+	return PI-acos((SQR(sr) + SQR(er) - SQR(ht)) / (2.0*sr*er));
 }
 
-/*Return the look angle: this is the angle measured
-	by the satellite between earth's center and the target point x.
-	Returns radians*/
-double meta_look(meta_parameters *sar,double y,double x)
+/**********************************************************
+ * meta_look: Return the look angle
+ * This is the angle measured by the satellite between
+ * earth's center and the target point x. Returns radians*/
+double meta_look(meta_parameters *meta,double y,double x)
 {
-	double sr=meta_get_slant(sar,y,x);
-	return acos((SQR(sr) + SQR(sar->ifm->ht) - 
-		SQR(sar->ifm->er) ) / (2.0*sr*sar->ifm->ht));
+	double sr = meta_get_slant(meta,y,x);
+	double er = meta_get_earth_radius(meta,y,x);
+	double ht = meta_get_sat_height(meta,y,x);
+	return acos((SQR(sr) + SQR(ht) - SQR(er)) / (2.0*sr*ht));
 }
