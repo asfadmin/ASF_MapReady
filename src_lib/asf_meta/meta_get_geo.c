5ad3a0c80386d9635671e4d4b1a361027f9dbd6f
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-23 18:07:06 +0000

    Fixed meta_parameters-> to meta-> in meta_get_original_line_sample() in order to compile

--- src_lib/asf_meta/meta_get_geo.c
+++ src_lib/asf_meta/meta_get_geo.c
@@ -20,21 +20,25 @@ PROGRAM HISTORY:
 
 /*Geolocation Calls:*/
 
-/* Converts a given line and sample to that for the non-multilooked,
-   non-windowed, equivalent image.  This function shows how
-   gracelessly our metadata represents the different types of images
-   we have to cope with.  */
+/*******************************************************************
+ * meta_get_original_line_sample:
+ * Converts a given line and sample to that for the non-multilooked,
+ * non-windowed, equivalent image.  This function shows how
+ * gracelessly our metadata represents the different types of images
+ * we have to cope with.  */
 void meta_get_original_line_sample(meta_parameters *meta, int line, 
 				   int sample, int *original_line, 
 				   int *original_sample)
 {
-  *original_line = line * meta_parameters->general->y_pixel_size 
-                   + meta_parameters->general->start_line;
-  *original_sample = sample * meta_parameters->general->x_pixel_size
-                     + meta_parameters->general->start_sample;
+  *original_line = line * meta->general->y_pixel_size
+			+ meta->general->start_line;
+  *original_sample = sample * meta->general->x_pixel_size 
+			+ meta->general->start_sample;
 }
 
-/* DEPRECATED.  You probably want meta_get_original_line_sample.  */
+/*******************************************************************
+ * meta_get_orig:
+ * DEPRECATED.  You probably want meta_get_original_line_sample.  */
 void meta_get_orig(void *fake_ddr, int y, int x,int *yOrig,int *xOrig)
 {
 	struct DDR *ddr=(struct DDR *)fake_ddr;
@@ -42,9 +46,10 @@ void meta_get_orig(void *fake_ddr, int y, int x,int *yOrig,int *xOrig)
 	*yOrig=(y)*ddr->line_inc + (ddr->master_line-1);
 }
 
-/* Converts given line and sample to geodetic
-latitude and longitude. Works with all image types.
-*/
+/*******************************************************************
+ * meta_get_latLon:
+ * Converts given line and sample to geodetic latitude and longitude.
+ * Works with all image types.*/
 void meta_get_latLon(meta_parameters *meta,
 	double yLine, double xSample,double elev,double *lat,double *lon)
 {
@@ -70,47 +75,50 @@ void meta_get_latLon(meta_parameters *meta,
 	}
 }
 
-/*Converts the given time, slant range, doppler, and elevation
-off earth's surface into a latitude and longitude.*/
-void meta_timeSlantDop2latLon(meta_parameters *sar,
+/*******************************************************************
+ * meta_timeSlantDop2latLon:
+ * Converts the given time, slant range, doppler, and elevation off
+ * earth's surface into a latitude and longitude.*/
+void meta_timeSlantDop2latLon(meta_parameters *meta,
 	double time, double slant,double dop,double elev,
 	double *lat,double *lon)
 {
 	double ignored;
 	stateVector stVec;
-	stVec=meta_get_stVec(sar,time);
+	stVec=meta_get_stVec(meta,time);
 	fixed2gei(&stVec,0.0);/*Subtract Earth's spin.*/
 		
-	getLatLongMeta(stVec,sar,slant,dop,elev,
+	getLatLongMeta(stVec,meta,slant,dop,elev,
 		lat,lon,&ignored);
 }
 
-/* Converts a given line and sample in image into time,
-slant-range, and doppler.  Works with all image types.
-*/  
-void meta_get_timeSlantDop(meta_parameters *sar,
+/*******************************************************************
+ * meta_get_timeSlantDop:
+ * Converts a given line and sample in image into time, slant-range,
+ * and doppler.  Works with all image types.*/  
+void meta_get_timeSlantDop(meta_parameters *meta,
 	double yLine,double xSample,double *time,double *slant,double *dop)
 {
-	if (sar->geo->type=='S'||sar->geo->type=='G')
+	if (meta->sar->image_type=='S'||meta->sar->image_type=='G')
 	{ /*Slant or ground range.  These are easy.*/
-		*slant=meta_get_slant(sar,yLine,xSample);
-		*time=meta_get_time(sar,yLine,xSample);
+		*slant = meta_get_slant(meta,yLine,xSample);
+		*time  = meta_get_time(meta,yLine,xSample);
 		if (dop != NULL)
 		{
-			if (sar->geo->deskew==1)
+			if (meta->sar->deskewed == 1)
 				*dop=0.0;
 			else
-				*dop=meta_get_dop(sar,yLine,xSample);
+				*dop=meta_get_dop(meta,yLine,xSample);
 		}
-	} else  if (sar->geo->type=='P') /*Map-projected image.  These are more difficult.*/
+	} else  if (meta->sar->image_type=='P') /*Map-projected image.  These are more difficult.*/
 	{
 		double lat,lon;
-		meta_get_latLon(sar,yLine,xSample,0.0,&lat,&lon);
-		latLon2timeSlant(sar,lat,lon,time,slant,dop);
+		meta_get_latLon(meta,yLine,xSample,0.0,&lat,&lon);
+		latLon2timeSlant(meta,lat,lon,time,slant,dop);
 	} else
 	{	/*Bogus image type.*/
-		printf("Error! Invalid image type '%c' passed to meta_get_latLon!\n",
-			sar->geo->type);
+		printf("Error! Invalid image type '%c' passed to meta_get_timeSlantDop!\n",
+			meta->sar->image_type);
 		exit(1);
 	}
 }
