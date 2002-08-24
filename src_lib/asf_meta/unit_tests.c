d1645cb4b53094e911b7bb49a954dbf6ffd632f4
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-24 00:08:43 +0000

    Test meta_create

--- src_lib/asf_meta/unit_tests.c
+++ src_lib/asf_meta/unit_tests.c
@@ -73,6 +73,40 @@ START_TEST(test_meta_read_write_new_format)
 }
 END_TEST
 
+/* Test part of ceos_init and aisp_init from meta_create.  */
+START_TEST(test_meta_create_new_format)
+{
+  char err_msg[256];
+  meta_parameters *meta;
+  
+  meta = meta_create("./CEOS/E12259029000X008");
+  
+  /* Check random fields to make sure things are working.  */
+  sprintf(err_msg,"meta->general->orbit = %d; not read correctly, should be 22590",meta->general->orbit);
+   fail_unless(meta->general->orbit == 22590,err_msg);
+  sprintf(err_msg,"meta->general->orbit_direction = '%c'; not read correctly, should be 'D'",meta->general->orbit_direction);
+   fail_unless(meta->general->orbit_direction == 'D',err_msg);
+  sprintf(err_msg,"meta->sar->prf = %lf; not read correctly, should be 1679.9023438",meta->sar->prf);
+   fail_unless(meta->sar->prf == 1679.9023438,err_msg);
+
+  /* Check a not-so-random field: things from projection params block
+     are currently partly holdover from deprecated code and use a
+     union.  */
+/*  fail_unless((meta->projection->type=='A') && (meta->projection->param.atct.alpha1==0.6),
+	      "alpha1 field from param->atct block not read correctly");
+  */
+  /* Another not-so-random field check: state vector blocks currently
+     use wierd field names in the data file and map strangely into a
+     dynamicly allocated internal structure, lots of possibility for
+     error.  */
+/*  fail_unless(UNIT_TESTS_FLOAT_COMPARE(meta->state_vectors->vecs[1].vec.pos.y, 22333),
+	      "Y position element of second state vector not read correctly");
+*/
+  meta_write(meta, "test_output_of_meta_create.meta");
+  meta_free(meta);
+}
+END_TEST
+
 /* Machinery for running the 'check' tests.  */
 Suite *asf_meta_suite(void)
 {
@@ -83,6 +117,7 @@ Suite *asf_meta_suite(void)
   
   tcase_add_test(tc_core, test_meta_read_new_format);
   tcase_add_test(tc_core, test_meta_read_write_new_format);
+  tcase_add_test(tc_core, test_meta_create_new_format);
 
   return s;
 }
