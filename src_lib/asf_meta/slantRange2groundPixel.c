0c5b4968b4bba65be89e401d3042fe8cfc4bbd80
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-08-27 18:48:59 +0000

    Updated function to use new meta structures

--- src_lib/asf_meta/slantRange2groundPixel.c
+++ src_lib/asf_meta/slantRange2groundPixel.c
@@ -1,54 +1,34 @@
-/****************************************************************
-FUNCTION NAME:
-
-SYNTAX:
-
-PARAMETERS:
-    NAME:	TYPE:		PURPOSE:
-    --------------------------------------------------------
-
-DESCRIPTION:
-
-RETURN VALUE:
-
-SPECIAL CONSIDERATIONS:
-
-PROGRAM HISTORY:
-
-****************************************************************/
 #include "asf.h"
 #include "asf_meta.h"
 #include <math.h>
 
-/* Calculate the ground range pixel from the total slant range
- ------------------------------------------------------------*/
-int slantRange2groundPixel(meta_parameters *sar,double slant)
-  {
+/*******************************************************
+ * slantRange2groundPixel:
+ * Calculate the ground range pixel from the total slant
+ * range*/
+int slantRange2groundPixel(meta_parameters *meta,double slant)
+{
 	double a,x,y,Gr0, GrX;
         double Rsc, Re;
 	int    groundPixel;
 
-	Rsc = sar->ifm->ht;
-	Re  = sar->ifm->er;
+	Rsc = meta_get_sat_height(meta, meta->general->line_count/2, 0);
+	Re  = meta_get_earth_radius(meta, meta->general->line_count/2, 0);
 
 	a = (Rsc-Re)/Re;
 	x = 1+a;
 
-	y = sar->geo->slantFirst / Re;
+	y = meta->sar->slant_range_first_pixel / Re;
 
 	Gr0 = Re * acos((1.0 + x*x - y*y) / (2.0 * x));
 
 	y = slant/Re;
 	GrX = Re * acos((1.0 + x*x - y*y) / (2.0 * x));
 
-	groundPixel = (int) ((GrX-Gr0)/sar->geo->proj->perX)+0.5;
+	groundPixel = (int) ((GrX-Gr0)/meta->projection->perX)+0.5;
 	printf("Slant %f :: ",slant);
-	printf("GP = (%f-%f) / %f = %i\n",GrX,Gr0,sar->geo->proj->perX,groundPixel);
-
-
-
-
+	printf("GP = (%f-%f) / %f = %i\n",GrX,Gr0,meta->projection->perX,groundPixel);
 
 	return(groundPixel);
-  }
+}
 
