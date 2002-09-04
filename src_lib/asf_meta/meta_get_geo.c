2c8c814d8926fd7f47f7f319a121c60f951b71c6
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-09-04 17:55:57 +0000

    Took meta_get_lineSamp and company from meta_get_geo2, some debugging on meta_get_original_line_sample (you know that is a fairly ostentacious name), but its still broken... not that it ever worked

--- src_lib/asf_meta/meta_get_geo.c
+++ src_lib/asf_meta/meta_get_geo.c
@@ -25,14 +25,14 @@ PROGRAM HISTORY:
  * Converts a given line and sample to that for the non-multilooked,
  * non-windowed, equivalent image.  This function shows how
  * gracelessly our metadata represents the different types of images
- * we have to cope with.  */
-void meta_get_original_line_sample(meta_parameters *meta, int line, 
-				   int sample, int *original_line, 
-				   int *original_sample)
+ * we have to cope with.
+ * NOT READY FOR PRODUCTION CODE!!!  */
+void meta_get_original_line_sample(meta_parameters *meta,
+				int line, int sample,
+				int *original_line, int *original_sample)
 {
-  *original_line = line * meta->general->y_pixel_size
-			+ meta->general->start_line;
-  *original_sample = sample * meta->general->x_pixel_size 
+  *original_line   = line * meta->sar->look_count + meta->general->start_line;
+  *original_sample = sample * (meta->sar->look_count/5 + 0.999999) /* to avoid 0 */
 			+ meta->general->start_sample;
 }
 
@@ -121,4 +121,59 @@ void meta_get_timeSlantDop(meta_parameters *meta,
 			meta->sar->image_type);
 		exit(1);
 	}
+}
+
+/******************************************************************
+ * Internal utilities for meta_get_lineSamp() */
+typedef struct {
+	double lat;
+	double lon;
+} lat_lon;
+double get_distance(lat_lon one,lat_lon two)
+{
+	double dlat=one.lat-two.lat;
+	double dlon=(one.lon-two.lon)*cos(one.lat*PI/180.0);
+	return dlat*dlat+dlon*dlon;
+}
+double get_error(meta_parameters *meta,
+	lat_lon target,double elev, double xSamp,double yLine)
+{
+	lat_lon new;
+	meta_get_latLon(meta,yLine,xSamp,elev,&new.lat,&new.lon);
+	return get_distance(new,target);
+}
+/******************************************************************
+ * meta_get_lineSamp:
+ * Converts given latitude and longitude back to the original line
+ * and sample.*/
+void meta_get_lineSamp(meta_parameters *meta,
+	double lat,double lon,double elev,double *yLine,double *xSamp)
+{
+#define DELTA 0.1 /*Number of pixels along which to 
+  perform finite difference approximation of the derivative.*/
+	double x=0.0,y=0.0;
+	double x_old=1000,y_old=1000;
+	int iter=0;
+	lat_lon target;
+
+	target.lat = lat;
+	target.lon = lon;
+	while (fabs(x-x_old)+fabs(y-y_old)>DELTA)
+	{
+		double cur_err = get_error(meta,target,elev,x,y);
+		double del_x   = (get_error(meta,target,elev,x+DELTA,y)-cur_err)/DELTA;
+		double del_y   = (get_error(meta,target,elev,x,y+DELTA)-cur_err)/DELTA;
+		double rad     = fabs(del_x) + fabs(del_y);
+
+		/*printf("   x=%6.1f; y=%6.1f, err=%.6f\n",x,y,cur_err);
+		*/
+		x_old=x;y_old=y;
+		x=x-(fabs(del_x)/rad)*cur_err/del_x;
+		y=y-(fabs(del_y)/rad)*cur_err/del_y;
+		iter++;
+	}
+	/*printf("  %d iterations\n",iter);*/
+ 
+	*yLine=y-DELTA/2;
+	*xSamp=x-DELTA/2;
 }
