5d1e1c3d87fd75be0ac29cccf130b2c98f6c1920
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2002-09-11 22:16:53 +0000

    Removed include ddr.h because it was causing problems, changed some comments

--- include/asf_meta.h
+++ include/asf_meta.h
@@ -24,7 +24,6 @@
 #define __ASF_META_H__
 
 #include "geolocate.h"		/* For stateVector.  */
-#include "ddr.h"
 
 /* There are some different versions of the metadata files around.
    This token defines the current version, which this header is
@@ -65,19 +64,20 @@ typedef struct {
   char processor[FIELD_STRING_MAX]; /* Name and version of SAR processor.  */
   char data_type[FIELD_STRING_MAX]; /* Type of samples (e.g. "REAL*4").    */
 /*  Possible string values for date_type:                                  *
- *                   ???       = unknown                                   *
  *                   BYTE      = 1 byte                = unsigned char     *
  *                   INTEGER*2 = 2 byte integer        = short int         *
  *                   INTEGER*4 = 4 byte integer        = int               *
  *                   REAL*4    = 4 byte floating point = float             *
- *                   REAL*8    = 8 byte floating point = double            */
+ *                   REAL*8    = 8 byte floating point = double            *
+ *                   ???       = unknown                                   */
   char system[FIELD_STRING_MAX];    /* System of samples (e.g. "ieee-std") */
-/*  Possible string values for system (inheritted from sysdef.h):          *
- *                   "ieee-std"    = IEEE standard (ref 754)               *
- *                   "ieee-lil"    = IEEE standard (ref 754) little-endian *
- *                   "ibm-mvs"     = IBM MVS                               *
- *                   "cray-unicos" = Cray Y-MP Unicos                      *
- *                   "other-msc"   = Misc. Other systems not covered       */
+/*  Possible string values for system:                                     *
+ *    OUR NAME      DDR EQUIVALENT      WHAT IT IS                         *
+ *    big_ieee        ieee-std       IEEE standard (ref 754)               *
+ *    lil_ieee        ieee-lil       IEEE standard (ref 754) little-endian *
+ * we don't support   ibm-mvs        IBM MVS                               *
+ *   cray_float       cray-unicos    Cray Y-MP Unicos                      *
+ *      ???           other-msc      Misc. Other systems not covered       */
   int orbit;                 /* Orbit number of satellite.                 */
   int frame;                 /* Frame for this image or -1 if inapplicable.*/
   int band_number;           /* Band number; first band is 0               */
