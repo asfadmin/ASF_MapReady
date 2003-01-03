c849db4454c0de266f1e8c6e9f6ad0be5339cb36
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-01-03 19:18:41 +0000

    Added meta structure to CEOS_FILE struct for phasing out of DDRs.

--- include/ceos_io.h
+++ include/ceos_io.h
@@ -4,7 +4,7 @@
 #ifndef _CEOS_IO_H_
 #define _CEOS_IO_H_
 
-#include "ddr.h"
+#include "asf_meta.h"
 
 /*Ceos2ddr converts a CEOS file into
 a DDR.  If headerBytes and lineBytes are not
@@ -14,14 +14,18 @@ void ceos2ddr(char *ceosName,struct DDR *ddrOut,
 
 
 typedef struct {
-	struct DDR ddr;/*This DDR is created by ceos2ddr*/
+/************************************************************************/
+/*FIXME: Nuke ddr struct as soon as it is not referenced in client code */
+/**/	struct DDR ddr;         /* This DDR is created by ceos2ddr      */
+/************************************************************************/
+	meta_parameters *meta;  /*Meta struct acquired from meta_create()*/
 	int headerBytes;
 	int lineBytes;
 	FILE *f_in;
 	char name[1024];/*Name of image data file.*/
 } CEOS_FILE;
 
-
+/* in ceos_io.c */
 CEOS_FILE *fopenCeos(char *fName);
 void readCeosLine(int *dest,int y,CEOS_FILE *in);
 void closeCeos(CEOS_FILE *in);
