ea69cd5280e3d7da1dc67864176e901b25dd328e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-01-04 01:22:59 +0000

    Added meta structure to CEOS_FILE struct & necessary code to load it.

--- src_lib/asf_meta/ceos_io.c
+++ src_lib/asf_meta/ceos_io.c
@@ -7,12 +7,30 @@
 #include "ceos.h"
 #include "ceos_io.h"
 
+/* in ceos2ddr.c */
+int firstRecordLen(char *ceosIn);
+
+
 CEOS_FILE *fopenCeos(char *fName)
 {
 	CEOS_FILE *c=(CEOS_FILE *)MALLOC(sizeof(CEOS_FILE));
-	
+/** For when ddr struct goes bye bye,
+ ** and thus call to ceos2ddr does as well
+ *	struct IOF_VFDR iof;    Imagery Options File, from CEOS
+ *	char dummy[255];
+ *	get_ifiledr(fName, &iof);
+ *	c->lineBytes=iof.reclen;
+ *	if (set_era(fName,dummy,0)) new data header size
+ *		c->headerBytes = firstRecordLen(fName) + 192;
+ *	else old data header size
+ *		c->headerBytes = firstRecordLen(fName) + 12;
+**/
+
 /* Determine format and size of input file */
 	ceos2ddr(fName,&c->ddr,&c->headerBytes,&c->lineBytes);
+
+/* Get meta structs from CEOS */
+	c->meta = meta_create(fName);
 
 /* Determine name of, then open input file.*/
 	set_era(fName,c->name,0);
@@ -48,5 +66,6 @@ void closeCeos(CEOS_FILE *in)
 {
 	FCLOSE(in->f_in);
 	in->f_in=NULL;
+	meta_free(in->meta);
 	FREE((void *)in);
 }
