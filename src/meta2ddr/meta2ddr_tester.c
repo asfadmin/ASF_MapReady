189018581de0cb712e959f69f9faff88b3dbcf1b
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-02-24 20:13:59 +0000

    Commenting, and directory cleaning after the tests.

--- src/meta2ddr/meta2ddr_tester.c
+++ src/meta2ddr/meta2ddr_tester.c
@@ -20,6 +20,18 @@
 #define UNIT_TESTS_MICRON 0.00001
 #define UNIT_TESTS_FLOAT_COMPARE(a, b) (abs(a - b) < UNIT_TESTS_MICRON ? 1 : 0)
 
+void diff_clean(char *diff_name)
+{
+  FILE *fp;
+  if ((fp=fopen(diff_name,"r")) == NULL)
+    return;
+  fgetc(fp);/*Read a byte, to set the EOF flag.*/
+  if (feof(fp)) {
+    fclose(fp);
+    remove(diff_name);
+  }
+}
+
 /**********************************************
  * Test making las -> meta.                  */
 START_TEST(test_ddr2meta)
@@ -27,14 +39,17 @@ START_TEST(test_ddr2meta)
   char cmd[256];
   printf("\n");
 
-  sprintf(cmd,"ddr2meta %s/old_style %s/ddr2meta_out",
-          _TEST_DATA_DIR,_TEST_DATA_DIR);
+/* Run program for testing */
+  sprintf(cmd,"ddr2meta %s/old_style %s/ddr2meta_out",_TEST_DATA_DIR,_TEST_DATA_DIR);
   printf("%s\n",cmd);  
   fail_unless( !system(cmd), "test_ddr2meta: meta2ddr returned non-zero exit code.");
 
-  sprintf(cmd,"diff %s/ddr2meta_out.meta %s/ddr2meta_good.meta > %s/diff_ddr2meta.txt",
-          _TEST_DATA_DIR,_TEST_DATA_DIR,_TEST_DATA_DIR);
+/* Diff the out put */
+  sprintf(cmd,"diff %s/ddr2meta_out.meta %s/ddr2meta_good.meta > diff_ddr2meta_meta",
+          _TEST_DATA_DIR,_TEST_DATA_DIR);
   fail_unless( !system(cmd), "test_ddr2meta: diff between out and good .meta file.");
+/* Clean up */
+  diff_clean ("diff_ddr2meta_meta");
 }
 END_TEST
 
@@ -45,22 +60,31 @@ START_TEST(test_meta2ddr)
   char cmd[256];
   printf("\n");
 
-  sprintf(cmd,"meta2ddr %s/new_style %s/meta2ddr_out",
-          _TEST_DATA_DIR,_TEST_DATA_DIR);
+/* Run program for testing */
+  sprintf(cmd,"meta2ddr %s/new_style %s/meta2ddr_out",_TEST_DATA_DIR,_TEST_DATA_DIR);
   printf("%s\n",cmd);  
   fail_unless( !system(cmd), "test_meta2ddr: meta2ddr returned non-zero exit code.");
 
-  sprintf(cmd,"diff %s/meta2ddr_out.meta %s/meta2ddr_good.meta > %s/diff_meta2ddr_meta.txt",
-          _TEST_DATA_DIR,_TEST_DATA_DIR,_TEST_DATA_DIR);
-  fail_unless( !system(cmd), "test_meta2ddr: diff between out and good .meta file");
+/* Prepare easily comparable text version of DDRs */
+  sprintf(cmd,"dspddr %s/meta2ddr_out.ddr | grep -v -i IMAGE > meta2ddr_out.txt",
+          _TEST_DATA_DIR);
+  system(cmd);
+  sprintf(cmd,"dspddr %s/meta2ddr_good.ddr | grep -v -i IMAGE > meta2ddr_good.txt",
+          _TEST_DATA_DIR);
+  system(cmd);
 
-  sprintf(cmd,"dspddr %s/meta2ddr_out.ddr > %s/meta2ddr_out.txt",_TEST_DATA_DIR,_TEST_DATA_DIR);
-  system(cmd);
-  sprintf(cmd,"dspddr %s/meta2ddr_good.ddr > %s/meta2ddr_good.txt",_TEST_DATA_DIR,_TEST_DATA_DIR);
-  system(cmd);
-  sprintf(cmd,"diff %s/meta2ddr_out.txt %s/meta2ddr_good.txt > %s/diff_meta2ddr_ddr.txt",
-          _TEST_DATA_DIR,_TEST_DATA_DIR,_TEST_DATA_DIR);
-  fail_unless( !system(cmd), "test_meta2ddr: diff between out and good .ddr file");
+/* Diff metas & DDRs */
+  sprintf(cmd,"diff %s/meta2ddr_out.meta %s/meta2ddr_good.meta > diff_meta2ddr_meta",
+          _TEST_DATA_DIR,_TEST_DATA_DIR);
+  fail_unless( !system(cmd), "test_meta2ddr: diff between out and good .meta file");
+  fail_unless( !system("diff meta2ddr_out.txt meta2ddr_good.txt > diff_meta2ddr_ddr"),
+               "test_meta2ddr: diff between out and good .ddr file");
+
+/* Clean up */
+  remove ("meta2ddr_good.txt");
+  remove ("meta2ddr_out.txt");
+  diff_clean ("diff_meta2ddr_meta");
+  diff_clean ("diff_meta2ddr_ddr");
 }
 END_TEST
 
