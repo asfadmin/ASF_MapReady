a9fdce8be20aee643b354a8e708d38e9578dddf7
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-04-02 19:39:09 +0000

    Added TRUE & FALSE defines. Added get/put_double_line prototypes.

--- include/asf.h
+++ include/asf.h
@@ -21,6 +21,13 @@
 #define D2R (PI/180.0)
 #define R2D (180.0/PI)
 
+#ifndef FALSE
+# define FALSE (0)
+#endif
+#ifndef TRUE
+# define TRUE (!FALSE)
+#endif
+
 /* The maximum allowable length in characters (not including trailing
    null character )of result strings from the appendExt function.  */
 #define MAX_APPENDEXT_RESULT_STRING_LENGTH 255
@@ -43,41 +50,27 @@ void StopWatch(void);
 void StopWatchLog(FILE *fLog);
 
 /*****************************************
-FileUtil:
-	A collection of file I/O utilities.
-Actually implemented in asf.a/fileUtil.c
-
-extExists returns whether the given
-file basename and extension exist and
-are readable.
-
-fileExists returns whether the given file
-name exists and is readable.
-
-findExt returns a pointer to the beginning
-(the period) of the given name's first
-extension, or NULL if none exists.
-
-appendExt allocates its return string
-on the heap, so it must be free'd, or
-memory will leak.  It can take a NULL extension,
-whereupon it just allocates a copy of the given
-string, and returns it.
-
-fopenImage first tries to open the given image 
-name, then appends ".img" and tries again.
-It returns a pointer to the opened file.
-*/
-
+ * FileUtil:
+ * A collection of file I/O utilities. Implemented in asf.a/fileUtil.c */
 int extExists(const char *name,const char *newExt);
 int fileExists(const char *name);
 char *findExt(char *name);
 char *appendExt(const char *name,const char *newExt);
+void create_name(char *out,const char *in,const char *newExt);
 FILE *fopenImage(const char *name,const char *accessType);
+
+
+/*****************************************
+ * ioLine:
+ * Grab a a line of any data type and fill a buffer of _type_ data. Puts data in
+ * correct endian order. Implemented in asf.a/ioLine.c */
 int get_float_line(FILE *file, meta_parameters *meta, int line_number,
 		float *dest);
 int put_float_line(FILE *file, meta_parameters *meta, int line_number,
 		const float *source);
-void create_name(char *out,const char *in,const char *newExt);
+int get_double_line(FILE *file, meta_parameters *meta, int line_number,
+		double *dest);
+int put_double_line(FILE *file, meta_parameters *meta, int line_number,
+		const double *source);
 
 #endif
