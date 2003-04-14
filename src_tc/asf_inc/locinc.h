2d604a6df9fb9bea0c7e99920e71f422429399e5
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-04-14 17:17:02 +0000

    Removing the erroneous src_tc/asf_inc directory's contents. It is not used as make_support/system_rules sets the include directory to ../../include rather than ../asf_inc as it used to. Not only that, terrcorr passed autotest with flying colors (after a little bit of library/header fixing)!

--- src_tc/asf_inc/locinc.h
+++
@@ -1,7 +0,0 @@
-#include "buf.h"
-#include "fdesc.h"
-#include "datasize.h"
-
-lasErr FUNCTION allocate_buffer(struct FDESC *fd,struct GDESC *gd);
-lasErr FUNCTION check_window(struct FDESC *fd,struct DDR *ddr);
-lasErr FUNCTION process_it(struct FDESC *fd, int cline, int cband,struct GDESC * gd);
