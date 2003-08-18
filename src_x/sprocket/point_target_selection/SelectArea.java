522408cbd64862abadb8290e4a726c98e9da2ada
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-08-18 19:19:33 +0000

    Use ActionListener interface instead of depricated action method.

--- src_x/sprocket/point_target_selection/SelectArea.java
+++ src_x/sprocket/point_target_selection/SelectArea.java
@@ -1,99 +1,89 @@
 import java.awt.* ;
+import java.awt.event.*;
 import java.io.* ;
 import java.util.* ;
 
-public class SelectArea extends Dialog {
-	protected String area;
-	protected Vector area_list;
-	protected java.awt.List area_list_wid;
-	protected TextField info_area;
-	protected Button cancel, ok;
-	protected Area selected_item;
+public class SelectArea extends Dialog implements ActionListener {
+   protected String area;
+   protected Vector area_list;
+   protected java.awt.List area_list_wid;
+   protected TextField info_area;
+   protected Button cancelButton, okButton;
+   protected Area selected_item;
 
-	public Area getSelectedItem()
-	  {
-	    return selected_item;
-          }
+   public Area getSelectedItem() {
+      return selected_item;
+   }
 
-	public SelectArea (String title, Frame f)
-	  {
+   public SelectArea (String title, Frame f) {
+      super(f, title, true);
+         /* Structure window */
+      Panel top, bottom, button_row;
+      top = new Panel(new BorderLayout());
+      bottom = new Panel(new BorderLayout());
+      button_row = new Panel();
+      top.add(bottom, BorderLayout.SOUTH);
 
-		super(f, title, true);
-			/* Structure window */
-		Panel top, bottom, button_row;
-		top = new Panel(new BorderLayout());
-		bottom = new Panel(new BorderLayout());
-		button_row = new Panel();
-		top.add(bottom, BorderLayout.SOUTH);
-	
-		top.add(new Label ("Please select the region"), BorderLayout.NORTH);
+      top.add(new Label ("Please select the region"), BorderLayout.NORTH);
 
-			/* Obtain the possible areas */
-		Spawner S = new Spawner ( "list_sites.sql");
-             	AreaReader r = new AreaReader ( S.In);
-             	area_list= r.getElements();
-			
-			/* Display the Area's names */
-		area_list_wid = new java.awt.List ( area_list.size() );
-		for ( int a = 0; a < area_list.size(); a++)
-		  area_list_wid.add(((Area)area_list.elementAt(a)).cs_name);
-		top.add(area_list_wid, BorderLayout.CENTER);
+      /* Obtain the possible areas */
+      Spawner S = new Spawner ( "list_sites.sql");
+      AreaReader r = new AreaReader ( S.In);
+      area_list= r.getElements();
+         
+      /* Display the Area's names */
+      area_list_wid = new java.awt.List ( area_list.size() );
+      for (int a = 0; a < area_list.size(); a++)
+         { area_list_wid.add(((Area)area_list.elementAt(a)).cs_name); }
+      top.add(area_list_wid, BorderLayout.CENTER);
 
-			/* Create a info window */
-		info_area = new TextField();
-		info_area.setEditable(false);
-	
-		bottom.add(info_area, BorderLayout.CENTER);
-		cancel = new Button ( "Cancel");
-		ok = new Button("Ok");
-		button_row.add(ok);
-		button_row.add(cancel);
-		bottom.add(button_row, BorderLayout.SOUTH);
-		add(top);
-	  }
+      /* Create a info window */
+      info_area = new TextField();
+      info_area.setEditable(false);
+   
+      bottom.add(info_area, BorderLayout.CENTER);
+      cancelButton = new Button ( "Cancel");
+      okButton = new Button("Ok");
+      button_row.add(okButton);
+      button_row.add(cancelButton);
+      bottom.add(button_row, BorderLayout.SOUTH);
+      add(top);
+      cancelButton.addActionListener(this);
+      okButton.addActionListener(this);
+   }
 
-	public boolean action ( Event e, Object arg )
-	  {
-	   //System.err.println("Event = " + e.target);
-           if ( e.target == cancel )
-	      {
-	        this.hide();
-		this.dispose();
-		return true;
-	      }
-	   if ( e.target == ok )
-	     {
-		//preform some ok stuff here
-                int item = area_list_wid.getSelectedIndex();
-                selected_item = (Area)area_list.elementAt(item);
+   public void actionPerformed (ActionEvent actionEvt) {
+      if (actionEvt.getSource() == cancelButton) {
+         setVisible(false);
+         this.dispose();
+         return;
+      }
+      if (actionEvt.getSource() == okButton) {
+         int item = area_list_wid.getSelectedIndex();
+         selected_item = (Area)area_list.elementAt(item);
+         if (selected_item != null) {
+            setVisible(false);
+            this.dispose();
+            return;
+         }
+      }
+      if (actionEvt.getSource() == area_list_wid) {
+         System.err.println("List selected.");
+         int item = area_list_wid.getSelectedIndex();
+         selected_item = (Area)area_list.elementAt(item);
+         info_area.setText(selected_item.cs_desc);
+         return;
+      }
+   }
 
-		if ( selected_item != null)
-		  {
-		     	this.hide();
-                	this.dispose();
-                	return true;
-		  }
-		return true;
-	     }
-	   if (e.target == area_list_wid)
-	     {
-		System.err.println("List selected.");
-		int item = area_list_wid.getSelectedIndex();
-		selected_item = (Area)area_list.elementAt(item);
-		info_area.setText( selected_item.cs_desc  );
-		return true;	
-	     }
-	   return false;
-	  }
 
-	public static void main ( String [] args)
-	 {
-	/*
-	   SelectArea f = new SelectArea (" Foo! ");
-	   f.resize(200,300);
-	   f.setLocation(200,200);
-	   f.show();
-	*/
-	 }
-	}
+   public static void main ( String [] args) {
+   /*
+      SelectArea f = new SelectArea (" Foo! ");
+      f.resize(200,300);
+      f.setLocation(200,200);
+      f.show();
+   */
+   }
+}
 
