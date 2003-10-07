e2995c44e4e4072e10660fd2b0eaa337719435a0
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-10-07 18:13:42 +0000

    Removed function ceos2ddr, and filled the ddr struct using meta2ddr. Added function firstRecordlen which used to be in ceos2ddr.c.

--- src_lib/asf_meta/ceos_io.c
+++ src_lib/asf_meta/ceos_io.c
@@ -3,38 +3,45 @@
 */
 #include "asf.h"
 #include "asf_meta.h"
-#include "ddr.h"
+#include "asf_endian.h"
 #include "ceos.h"
 #include "ceos_io.h"
 
-/* in ceos2ddr.c */
-int firstRecordLen(char *ceosIn);
+int firstRecordLen(char *ceosName)
+{
+	FILE *f;
+	struct HEADER h;
+
+	f=FOPEN(ceosName,"rb");    /*Open file.*/
+	FREAD(&h,1,12,f);          /*Read first CEOS header.*/
+	FCLOSE(f);                 /*Close file*/
+	return bigInt32(h.recsiz); /*put recsiz in proper endian format & return*/
+}
 
 
 CEOS_FILE *fopenCeos(char *fName)
 {
-	CEOS_FILE *c=(CEOS_FILE *)MALLOC(sizeof(CEOS_FILE));
-/** For when ddr struct goes bye bye,
- ** and thus call to ceos2ddr does as well
- *	struct IOF_VFDR iof;    Imagery Options File, from CEOS
- *	char dummy[255];
- *	get_ifiledr(fName, &iof);
- *	c->lineBytes=iof.reclen;
- *	if (set_era(fName,dummy,0)) new data header size
- *		c->headerBytes = firstRecordLen(fName) + 192;
- *	else old data header size
- *		c->headerBytes = firstRecordLen(fName) + 12;
-**/
+	struct IOF_VFDR iof;    /*Imagery Options File, from CEOS*/
+	CEOS_FILE *c = (CEOS_FILE *)MALLOC(sizeof(CEOS_FILE));
 
-/* Determine format and size of input file */
-	ceos2ddr(fName,&c->ddr,&c->headerBytes,&c->lineBytes);
+	get_ifiledr(fName, &iof);
+	c->lineBytes = iof.reclen;
+
+/* Determine data header size. Set c->name (filename) */
+	if (set_era(fName,c->name,0)) /*new data header size*/
+		c->headerBytes = firstRecordLen(c->name) + 192;
+	else /*old data header size*/
+		c->headerBytes = firstRecordLen(c->name) + 12;
 
 /* Get meta structs from CEOS */
 	c->meta = meta_create(fName);
 
-/* Determine name of, then open input file.*/
-	set_era(fName,c->name,0);
+/* Fill DDR struct for backwards compatibility */
+	meta2ddr(c->meta, &c->ddr);
+
+/* Open input file.*/
 	c->f_in=FOPEN(c->name,"rb");
+
 	return c;
 }
 
