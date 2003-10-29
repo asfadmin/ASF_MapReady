679cac0572a78a4b29eb754bc57baa053edb19d2
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-10-29 01:45:23 +0000

    Obliterate use of DDR/meta0.9 and use meta version 1.1.

--- src_geo/geocode/geoLatLon.c
+++ src_geo/geocode/geoLatLon.c
@@ -1,94 +1,74 @@
+/*******************************************************************************
+NAME: geoLatLon
+
+SYNOPSIS: geoLatLon(double eleva, char *metaName, char *out_tps)
+
+DESCRIPTION:
+    creates a 9x9 grid over the given input image, finds the latitude and
+    longitude for each grid point, and writes the lat,lon and x,y values to
+    its output file, which is later read by projectGeo.
+
+ARGUMENTS:
+   eleva      Set average terrain height to given meters.
+   metaName   image metadata name (.meta, .ddr, or .L)
+   out_tps    ASCII tie point file, used by projectGeo. This has the format:
+                   <lat> <lon> <input X> <input Y>
+
+FUNCTION HISTORY:
+    VERS:   DATE:  AUTHOR:	PURPOSE:
+    ---------------------------------------------------------------
+    1.0    03/98   O. Lawlor    Original Development
+    2.0    ?       R. Gens      Convert from program to function
+    2.5    08/03   P. Denny     Obliterate DDR and use new metadata
+
+HARDWARE/SOFTWARE LIMITATIONS: none
+
+BUGS: none known
+
+*******************************************************************************/
+
 #include "asf.h"
-#include "proj.h"
-#include "ddr.h"
 #include "asf_meta.h"
 
-int geoLatLon(double eleva, char *in_meta, char *in_ddr, char *out_tps)
-{
-	int x,y,k;			/* Loop counters */
-	FILE *out; 			/*Output tie point file */
-	meta_parameters *meta=NULL;
-	struct DDR ddr;
-	inverse_transform ddr_trans;/*Geocoding projection to lat,lon function pointer*/
-	
-
-	/* Print the user specified average elevation of the output area 
-	-----------------------------------------------------------*/
-	printf("   Input average output elevation = %f\n\n",eleva);
-	if (logflag) {
-	  sprintf(logbuf, "   Input average output elevation = %f\n\n",eleva);
-	  printLog(logbuf);
-	}
-	
-	out=FOPEN(out_tps,"w");/*Open ascii output file*/
-	
-	/* Initialize metadata
-	-----------*/
-	c_getddr(in_ddr,&ddr);
-
-/*Try to read from SAR metadata if at all possible*/
-	if (extExists(in_meta,".meta")||
-		extExists(in_meta,".L")||
-		extExists(in_meta,".ldr"))
-	{
-/*		printf("Reading .meta metadata...\n");*/
-		meta=meta_init(in_meta);
-	}
-	else if (ddr.valid[DDPCV]!=0)
-	/*Read projection information from DDR*/
-	{
-		inverse_transform trans[100];
-		int error=0;
-/*		printf("Initializing LAS Projection from DDR, since no .meta found...\n");*/
-		inv_init(ddr.proj_code,ddr.zone_code,ddr.proj_coef,
-			ddr.datum_code,NULL,NULL,&error,trans);
-		if (error!=0) {
-		  sprintf(errbuf, "   ERROR: %d initializing LAS projection!\n",error);
-		  printErr(errbuf);
-		}
-		ddr_trans=trans[ddr.proj_code];
-	} else { 
-	/*No valid metadata found-- bail!*/
-		sprintf(errbuf, "   ERROR: No valid metadata found for file '%s'!\n"
-			"geoLatLon exiting...\n",in_meta);
-		printErr(errbuf);
-	}
-	
-	/* Create grid over image
-	-----------*/
-	
 #define REZ_X 8
 #define REZ_Y 8
-	for(y=0; y<=REZ_Y; y++)
-		for(x=0; x<=REZ_X; x++,k++)
-		{
-			double lat,lon;	/* Output latitude and longitude  */
-			int img_x,img_y; /*Coordinates in DDR image*/
-			
-			img_y=y*ddr.nl/REZ_Y;
-			img_x=x*ddr.ns/REZ_X;
-			if (meta!=NULL)
-			{/*Is a Metadata image*/
-				int meta_x,meta_y; /*Coordinates in original image*/
-				/*Figure out the coordinates in the original image*/
-				meta_get_orig((void *)&ddr,img_y,img_x,&meta_y,&meta_x);
-				/*Convert these coordinates to lat/lon*/
-				meta_get_latLon(meta,meta_y,meta_x,eleva,&lat,&lon);
-			} else {/*Is just a LAS image--use ddr_trans*/
-				double projX,projY;
-				projX=ddr.upleft[1]+((double)img_x)/ddr.ns*(ddr.upright[1]-ddr.upleft[1]);
-				projY=ddr.upleft[0]+((double)img_y)/ddr.nl*(ddr.loleft[0]-ddr.upleft[0]);
-				ddr_trans(projX,projY,&lon,&lat);
-				lon*=R2D;
-				lat*=R2D;
-			}
-			
-			/*Print out the lat & lon information to output file*/
-			fprintf(out,"%.12f %.12f %d %d\n",lat,lon,img_x,img_y);
-		}
-	
-	FCLOSE(out);
-	
-/*	printf("geoLatLon complete\n");*/
-	return(0);
+
+void geoLatLon(double eleva, char *metaName, char *out_tps)
+{
+   int x,y;               /* Loop counters              */
+   FILE *out;             /* Output tie point file      */
+   char message[256];     /* Blather to user & log file */
+   meta_parameters *meta; /* Metadata file struct       */
+
+/* Print the user specified average elevation of the output area */
+   sprintf(message,"   Input average output elevation = %f\n\n",eleva);
+   printf("%s",message);
+   if (logflag) { printLog(message); }
+   
+   out = FOPEN(out_tps,"w");    /* Open ascii output file */
+   meta = meta_read(metaName);  /* Read meta file into meta struct */
+
+/* Create grid over image */
+   for (y=0; y<=REZ_Y; y++) {
+      for (x=0; x<=REZ_X; x++) {
+         double lat,lon;           /* Output latitude and longitude */
+         int cur_line,cur_samp;    /* Coordinates in current image  */
+         int orig_samp,orig_line;  /* Coordinates in original image */
+         
+         cur_line = y * meta->general->line_count / REZ_Y;
+         cur_samp = x * meta->general->sample_count / REZ_X;
+
+         /* Figure out the coordinates in the original image */
+         meta_get_original_line_sample(meta, cur_line, cur_samp,
+                                       &orig_line, &orig_samp);
+
+         /* Convert these coordinates to lat/lon */
+         meta_get_latLon(meta, orig_line, orig_samp, eleva, &lat, &lon);
+
+         /* Print out the lat & lon information to output file */
+         fprintf(out,"%.12f %.12f %d %d\n", lat, lon, cur_samp, cur_line);
+      }
+   }
+
+   FCLOSE(out);
 }
