186760988eb09f8b2cbd113c86d9a08f98b46092
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-15 23:44:45 +0000

    Removed some globals. changed use of DDR to meta.

--- src_ifm/reskew_dem/deskew.h
+++ src_ifm/reskew_dem/deskew.h
@@ -1,24 +1,23 @@
 #include "asf.h"
 #include "asf_meta.h"
 
-extern float badDEMht;
-extern int maxBreakLen;
-
-/*Defs:
-	Ground Range: distance measured along
-curve of earth, at sea level (0 elevation).
-	Phi: smallest angle made between line connecting
-point on earth's surface with earth's center and line connecting
-satellite and earth's center.
-	Slant Range: straight-line distance between
-satellite and point on earth's surface.
-*/
+/* DEFINITIONS:
+ * Ground Range: Distance measured along curve of earth, at sea level
+ *                 (0 elevation).
+ * Phi:          Smallest angle made between line connecting point on earth's
+ *                 surface with earth's center and line connecting satellite and
+ * 	        earth's center.
+ * Slant Range:  Straight-line distance between satellite and point on earth's
+ *                 surface.
+ */
 
 #define phi2grX(phi) (((phi)-minPhi)*phiMul)
 #define grX2phi(gr) (minPhi+(gr)/phiMul)
 
-extern double minPhi,maxPhi,phiMul,grPixelSize;
-extern meta_parameters *meta;
+extern float badDEMht;
+extern int maxBreakLen;
+
+extern double grPixelSize;
 extern int gr_ns,sr_ns;
 
 /*Array[gr_ns] indexed by ground range pixel.*/
@@ -30,16 +29,14 @@ extern double *heightShiftSR;
 extern double *groundSR;/*Ground range pixel #*/
 extern double *slantRangeSqr,*slantRange,*heightShift;
 extern double *incidAng,*sinIncidAng,*cosIncidAng;
-extern struct DDR ddrDEM,outddr,inddr;
 
-double calc_ranges(const struct DDR *);
+double calc_ranges(meta_parameters *meta);
 
 void geo_compensate(float *srDEM,float *in,float *out,int ns);
 void radio_compensate(float *grDEM, float *grDEMprev,float *inout,int ns);
 void dem_sr2gr(float *inBuf,float *outBuf);
+void dem_gr2sr(float *grDEM, float *srDEM,float *amp);
 void dem_interp_col(float *buf,int ns,int nl);
-
-void set_ddr_corners(char *ceos,struct DDR *outddr);
 
 float sr2gr(float srX,float height);
 float gr2sr(float grX,float height);
