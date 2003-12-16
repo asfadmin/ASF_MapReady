7b2735dfa48f391adf5d7e09396a34c1b2c04a6e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-16 03:20:38 +0000

    Updated command line parsing & usage.

--- src_ifm/aisp/spectra.c
+++ src_ifm/aisp/spectra.c
@@ -1,25 +1,25 @@
 /******************************************************************************
 NAME: spectra
 
-    SYNOPSIS: spectra <signal data> [ startX startY ]
+SYNOPSIS: spectra [ startX startY ] <signal data>
 
-    DESCRIPTION:
+DESCRIPTION:
 
-	Spectra computes range and azimuth spectra of the input
-complex signal data file.  The spectra are written to the output
-files "spectra_range", and "spectra_az", which are 1024-line long
-ASCII files containing the (arbitrary-units) power of the signal
-data at freqencies between 0 Hz and the sampling frequency.
-In range, this is several tens of megahertz (18.96 MHz for ERS);
-in azimuth, this is about a thousand hertz (about 1700 Hz for ERS).
+    Spectra computes range and azimuth spectra of the input complex signal data
+    file.  The spectra are written to the output files "spectra_range", and
+    "spectra_az", which are 1024-line long ASCII files containing the
+    (arbitrary-units) power of the signal data at freqencies between 0 Hz and
+    the sampling frequency. In range, this is several tens of megahertz (18.96
+    MHz for ERS); in azimuth, this is about a thousand hertz (about 1700 Hz for
+    ERS).
 
-	Spectra is useful in checking signal data quality/validity,
-but most people have no reason to use it.
+    Spectra is useful in checking signal data quality/validity, but most people
+    have no reason to use it.
 
-	Spectra's output files have the format:
-<FFT bin #> <power>
-<FFT bin #> <power>
-...
+    Spectra's output files have the format:
+      <FFT bin #> <power>
+      <FFT bin #> <power>
+      ...
 
 EXTERNAL ASSOCIATES:
     NAME:               USAGE:
@@ -33,6 +33,8 @@ PROGRAM HISTORY:
     VERS:   DATE:  AUTHOR:      PURPOSE:
     ---------------------------------------------------------------
     1.0	    6/98   O. Lawlor
+    1.1     ?/??   ?. ??????
+    1.3    12/03   P. Denny     Update command line order & parsing
 
 HARDWARE/SOFTWARE LIMITATIONS:
 
@@ -78,87 +80,112 @@ BUGS:
 
 #include <math.h>
 #include "asf.h"
-
 #include "aisp_defs.h"
 #include "read_signal.h"
 
-#define Version 1.1
+#undef VERSION
+#define VERSION 1.3
+
+#define FFT_LENGTH 1024
+#define NUM_ARGS 1
 
 int main(int argc,char **argv)
 {
-#define fftLen 1024
-
 	int startX=0,startY=0;
 	int x,y,i;
 	getRec *r;
 	complexFloat *inBuf;
-	complexFloat fftBuf[fftLen];
-	float outBuf[fftLen];
+	complexFloat fftBuf[FFT_LENGTH];
+	float outBuf[FFT_LENGTH];
 	FILE *outF;
-	if ((argc!=2 && argc!=4)||(argv[1][0]=='-'))
-		{printf("\nUSAGE: spectra <raw> [start X start Y]\n"
-		"\n"
-		"    <raw>:	APD raw complex signal data file.\n"
-		"    start X:	sample (Default is 0)\n"
-		"    start Y:	line (Default is 0)\n\n"
-		"spectra computes the range and azimuth\n"
-		"power spectra of a complex signal data file.\n"
-		"These spectra are written to files 'spectra_range'\n"
-		"and 'spectra_az' and are both %d points long.\n\n"
-		"Version %.2f, ASF SAR TOOLS\n\n",fftLen, Version);exit(1);}
 	
-	r=fillOutGetRec(argv[1]);
-	if (argc>3)
-	{
-		startX=atoi(argv[2]);
-		startY=atoi(argv[3]);
+/* Parse command line arguments */
+	while (currArg < (argc-NUM_ARGS)) {
+	   char *key = argv[currArg++];
+	   if (strmatch(key,"-x")) {
+	      CHECK_ARG(1);
+	      startX = atoi(GET_ARG(1));
+	   }
+	   else if (strmatch(key,"-y")) {
+	      CHECK_ARG(1);
+	      startY = atoi(GET_ARG(1));
+	   }
+	   else {printf( "\n**Invalid option:  %s\n",argv[currArg-1]); usage(argv[0]);}
 	}
+	if ((argc-currArg) < NUM_ARGS) {printf("Insufficient arguments.\n"); usage(argv[0]);}
+
+	r = fillOutGetRec(argv[currArg]);
+
+	inBuf=(complexFloat *)MALLOC(sizeof(complexFloat)*FFT_LENGTH*FFT_LENGTH);
+	cfft1d(FFT_LENGTH,NULL,0);
 	
-	inBuf=(complexFloat *)MALLOC(sizeof(complexFloat)*fftLen*fftLen);
-	cfft1d(fftLen,NULL,0);
-	
-/*Read a fftLen x fftLen block of input complex data.*/
-	for (y=0;y<fftLen;y++)
-		getSignalLine(r,startY+y,&inBuf[y*fftLen],startX,fftLen);
+/*Read a FFT_LENGTH x FFT_LENGTH block of input complex data.*/
+	for (y=0;y<FFT_LENGTH;y++)
+		getSignalLine(r,startY+y,&inBuf[y*FFT_LENGTH],startX,FFT_LENGTH);
 		
 /*Compute range spectra.*/
-	for (i=0;i<fftLen;i++) outBuf[i]=0;
+	for (i=0;i<FFT_LENGTH;i++) outBuf[i]=0;
 	outF=FOPEN("input_data","w");
-	for (y=0;y<fftLen;y++)
+	for (y=0;y<FFT_LENGTH;y++)
 	{
-		for (x=0;x<fftLen;x++)
+		for (x=0;x<FFT_LENGTH;x++)
 		{
-			fftBuf[x]=inBuf[y*fftLen+x];
+			fftBuf[x]=inBuf[y*FFT_LENGTH+x];
 			fprintf(outF,"%f %f\n",fftBuf[x].real,fftBuf[x].imag);
 		}
-		cfft1d(fftLen,fftBuf,-1);
-		for (i=0;i<fftLen;i++) 
+		cfft1d(FFT_LENGTH,fftBuf,-1);
+		for (i=0;i<FFT_LENGTH;i++) 
 			outBuf[i] += fftBuf[i].real * fftBuf[i].real
                                      + fftBuf[i].imag * fftBuf[i].imag;
 	}
 	outF=FOPEN("spectra_range","w");
-	for (i=0;i<fftLen;i++) 
+	for (i=0;i<FFT_LENGTH;i++) 
 		fprintf(outF,"%d %f\n",i,outBuf[i]);
 	FCLOSE(outF);
 
 
 /*Compute azimuth spectra.*/
-	for (i=0;i<fftLen;i++) outBuf[i]=0;
-	for (x=0;x<fftLen;x++)
+	for (i=0;i<FFT_LENGTH;i++) outBuf[i]=0;
+	for (x=0;x<FFT_LENGTH;x++)
 	{
-		for (y=0;y<fftLen;y++)
-			fftBuf[y]=inBuf[y*fftLen+x];
-		cfft1d(fftLen,fftBuf,-1);
-		for (i=0;i<fftLen;i++) 
+		for (y=0;y<FFT_LENGTH;y++)
+			fftBuf[y]=inBuf[y*FFT_LENGTH+x];
+		cfft1d(FFT_LENGTH,fftBuf,-1);
+		for (i=0;i<FFT_LENGTH;i++) 
 			outBuf[i] += fftBuf[i].real * fftBuf[i].real
                                      + fftBuf[i].imag * fftBuf[i].imag;
 	}
 	outF=FOPEN("spectra_az","w");
 	printf("Writing the average azimuthal spectra\n");
-	for (i=0;i<fftLen;i++) 
-		fprintf(outF,"%d %f\n",i,outBuf[i]/fftLen);
+	for (i=0;i<FFT_LENGTH;i++) 
+		fprintf(outF,"%d %f\n",i,outBuf[i]/FFT_LENGTH);
 	FCLOSE(outF);
 	
 	return 0;
 }
+
+
+void usage (char *name)
+{
+ printf("\n"
+	"USAGE:\n"
+	"   %s [-x <startX>]  [-y <startY>] <raw>\n",name);
+ printf("\n"
+	"REQUIRED ARGUMENT:\n"
+	"   <raw>   Raw complex signal data file.\n");
+ printf("\n"
+	"OPTIONAL ARGUMENTS:\n"
+	"   -x   sample <startX> (Default is 0)\n"
+	"   -y   line <startY> (Default is 0)\n");
+ printf("\n"
+	"DESCRIPTION:\n"
+	"   Computes the range and azimuth power spectra of a complex signal data file.\n"
+	"   These spectra are written to files 'spectra_range' and 'spectra_az' and are\n"
+	"   both %d points long.\n", FFT_LENGTH);
+ printf("\n"
+	"Version %.2f, ASF InSAR Tools\n"
+	"\n", VERSION);
+ exit(EXIT_FAILURE);
+}
+
 
