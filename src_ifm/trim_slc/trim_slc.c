f21f26781d9e33eba54158b6fcf94a7c5590758e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-17 00:30:01 +0000

    Update commandline order and parsing procedure. Use new meta instead of the ddr/meta combo. Make code a little more readable.

--- src_ifm/trim_slc/trim_slc.c
+++ src_ifm/trim_slc/trim_slc.c
@@ -30,6 +30,8 @@ PROGRAM HISTORY:
     1.3     4/00   Mark Ayers   Added .meta file creation to trim_slc
     1.4     6/00   D. Koster    Modified to handle files > 2GB
     1.5     7/01   R. Gens	Added a flag for Focus processor
+    1.75   12/03   P. Denny     Update command line order / parsing
+                                 Update to meta1.1 (remove DDR use).
 
 HARDWARE/SOFTWARE LIMITATIONS: 
 
@@ -76,204 +78,181 @@ BUGS: none known
 #include "las.h"
 #include "asf_meta.h"
 
-float version=1.5;
-
-int create_good_ddr(char *fname,int nl,int ns,int ml,int ms,double azpixsiz,double rnpixsiz,int data_type);
-
-/* Remove the first extention from a filename, and return the extention.*/
-char * strip_extention(char *filename)
-{
-/*We play "Hunt the first extention." by searching for periods starting at the end.*/
-	int i=strlen(filename)-1;
-	while ((i>0)&&(filename[i]!='.')) 
-		i--;
-	if (i!=0)
-	{
-	/*We have an extention, so first we remove it from the filename...*/
-		filename[i]=0;
-	/*And now we return it.*/
-		return &filename[i+1];
-	} else return "";
-}
-
-void usage(char *name);
-
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
+#define NUM_ARGS 4
+
+#define VERSION 1.75
+
+
 int main(int argc, char *argv[])
 {
-  int era;
-  int in_bytes_per_line,line_header,swflag;
-  struct IOF_VFDR vfdr;
-  struct VFDRECV asf_facdr;
-  int pixelSize=8;
-  int x,y,startX,startY,endX,endY,inMaxX,inMaxY,outMaxX,outMaxY;
-  int lastReadY,firstReadX,numInX;
-  FILE *in,*out;
-  float *outbuffer;
-  short *inbuffer;
-  meta_parameters *meta;
- 
-/*Process CLA's*/
-  char infileData[256],*outfileData,infile[256],*outfileDDR;
-  if (argc!=7 && argc!=5) usage(argv[0]);
-   /*Compute filenames*/
-  
-  era=set_era(argv[1],infileData,0);
-  era=set_era(infileData,infile,1);
+   int era;
+   int in_bytes_per_line,line_header;
+   struct IOF_VFDR vfdr;
+   struct VFDRECV asf_facdr;
+   int pixelSize=8;
+   int x,y,startX,startY,endX,endY,inMaxX,inMaxY,outMaxX,outMaxY;
+   int lastReadY,firstReadX,numInX;
+   FILE *in,*out;
+   float *outbuffer;
+   short *inbuffer;
+   meta_parameters *meta;
+   char infile[256],infileData[256],outfileData[256],outfileMeta[256];
+   int widthFlag=FALSE, heightFlag=FALSE;
+   int widthTemp, heightTemp;
 
-  get_ifiledr(infile,&vfdr);
-  get_asf_facdr(infile,&asf_facdr);
-  
+/* Parse command line arguments */
+   while (currArg < (argc-NUM_ARGS)) {
+      char *key = argv[currArg++];
+      if (strmatch(key,"-width")) {
+         CHECK_ARG(1);
+         widthTemp = atoi(GET_ARG(1));
+         widthFlag = TRUE;
+      }
+      else if (strmatch(key,"-height")) {
+         CHECK_ARG(1);
+         heightTemp = atoi(GET_ARG(1));
+         heightFlag = TRUE;
+     }
+      else {
+         printf( "\n**Invalid option:  %s\n",argv[currArg-1]);
+         usage(argv[0]);
+      }
+   }
+   if ((argc-currArg) < NUM_ARGS) {
+      printf("Insufficient arguments.\n");
+      usage(argv[0]);
+   }
+   /* Name infile extention based on whether CEOS is pre or post RADARSAT */
+   era = set_era(argv[currArg],infileData,0);
+   era = set_era(infileData,infile,1);
 
-  outfileData=appendExt(argv[2],".cpx");
-  outfileDDR=appendExt(argv[2],".ddr");
-  printf("trim_slc: input SLC: '%s'. output Float-Complex: '%s'.\n",infileData,outfileData);
-  in=fopenImage(infileData,"rb");
-  out=fopenImage(outfileData,"wb");
+   create_name(outfileData,argv[currArg+1],".cpx");
+   create_name(outfileMeta,argv[currArg+1],".meta");
+   startY = atoi(argv[currArg+2]);
+   startX = atoi(argv[currArg+3]);
 
-  swflag=strncmp("FOCUS",vfdr.software_id,5);
+/* Get metadata */
+   meta = meta_create(infileData);
+   get_ifiledr(infile,&vfdr);
+   get_asf_facdr(infile,&asf_facdr);
 
-  if (swflag!=0) {
-    meta=meta_create(infileData);
-    meta_write(meta,outfileData);
-  } 
+   line_header = (era==0) ? 12 : vfdr.predata;
+   in_bytes_per_line = vfdr.reclen;
 
-  inMaxX=asf_facdr.apixels;
-  inMaxY=asf_facdr.alines;
-  
-  if (era==0) /*pre-RADARSAT data-- vfdr invalid, 12 byte header per line always.*/
-  	line_header=12;
-  else line_header=vfdr.predata; /*post-RADARSAT data-- get header size from vfdr.*/
-  in_bytes_per_line=vfdr.reclen;
-  
-  if (0!=strncmp(vfdr.formcode,"CI*4",4)) {printf("Fatal Error: This program only works with\n\
-  Single Int Complex input data ('COMPLEX INTEGER*4').  \n\
-This data file is ('%s').\n",vfdr.formatid);exit(1);}
-  
-  startX=atoi(argv[4]);
-  startY=atoi(argv[3]);
-  if (argc==7)
-  {
-  	endX=startX+atoi(argv[6]);
-  	endY=startY+atoi(argv[5]);
-  } else
-  {
-  	endX=inMaxX-startX;
-  	endY=inMaxY-startY;
-  }
-  outMaxX=endX-startX;
-  outMaxY=endY-startY;
+/* Calculate trimmed dimensions */
+   inMaxX = asf_facdr.apixels;
+   inMaxY = asf_facdr.alines;
+   endY = (heightFlag) ? startY+heightTemp : inMaxY-startY;
+   endX = (widthFlag) ? startX+widthTemp : inMaxX-startX;
+   outMaxX = endX-startX;
+   outMaxY = endY-startY;
 
-  printf("\tInput image size: Lines=%i, Samples=%i\n",inMaxY,inMaxX);
-  printf("\tOutput image size: Lines=%i, Samples=%i\n",outMaxY,outMaxX);
-  
-/*Write out our outDDR*/
-  create_good_ddr(outfileDDR,outMaxY,outMaxX,startY+1,startX+1,asf_facdr.azpixspc,asf_facdr.rapixspc,4);
-    
-/*If everything's OK, then allocate an input and output buffer big enough for */
- /*one line of input and output data, respectively.*/
-  outbuffer= (float *)MALLOC(2*sizeof(float)*outMaxX);
-  inbuffer= (short *)MALLOC(2*sizeof(short)*inMaxX);
-  
+/* Update metadata */
+   meta->general->data_type = COMPLEX_REAL32;
+   meta->general->line_count = outMaxY;
+   meta->general->sample_count = outMaxX;
+   meta->general->start_line = startY;
+   meta->general->start_sample = startX;
+
+/* Open the files */
+   in=fopenImage(infileData,"rb");
+   out=fopenImage(outfileData,"wb");
+
+/* If everything's OK, then allocate an input and output buffer big enough for
+ * one line of input and output data, respectively.*/
+   outbuffer= (float *)MALLOC(2*sizeof(float)*outMaxX);
+   inbuffer= (short *)MALLOC(2*sizeof(short)*inMaxX);
+
+/* Let the user know what's goin' down */
+   printf("trim_slc: input SLC: '%s'. output Float-Complex: '%s'.\n",infileData,outfileData);
+   if (0!=strncmp(vfdr.formcode,"CI*4",4)) {
+      printf("Fatal Error: This program only works with\n"
+             "  Single Int Complex input data ('COMPLEX INTEGER*4').  \n"
+             "  This data file is ('%s').\n",vfdr.formatid);
+      exit(EXIT_FAILURE);
+   }
+   printf("        Input image size: Lines=%i, Samples=%i\n",inMaxY,inMaxX);
+   printf("        Output image size: Lines=%i, Samples=%i\n",outMaxY,outMaxX);
+
+
 /*If necessary, fill the top of the output with zeros, by loading up a buffer and writing.*/
-  for (x=0;x<outMaxX*2;x++)
-  	outbuffer[x]=0.0;
-  for (y=0;y<-startY && y<outMaxY;y++)
-  {
-  	FWRITE(outbuffer,pixelSize,outMaxX,out);
-  	if (0==y%100) printf("\tWriting output image line %i...\n",y);
-  }
+   for (x=0;x<outMaxX*2;x++)
+  	 outbuffer[x]=0.0;
+   for (y=0;y<-startY && y<outMaxY;y++)
+   {
+      FWRITE(outbuffer,pixelSize,outMaxX,out);
+      if (0==y%100) printf("        Writing output image line %i...\n",y);
+   }
 
 /*Do some calculations on where we should read.*/
-  firstReadX=MAX(0,-startX);
-  numInX=MIN(MIN(outMaxX,inMaxX-(firstReadX+startX)),outMaxX-firstReadX);
-  lastReadY=MIN(outMaxY,inMaxY-startY);
-  
-  for (;y<lastReadY;y++)
-  {
-  	const int SLCPixSize=4;
-  	int inputY=y+startY,
-  		inputX=firstReadX+startX;
-	FSEEK64(in,(inputY+1)*in_bytes_per_line+line_header+SLCPixSize*inputX,0);
-  	FREAD(inbuffer,SLCPixSize,numInX,in);
-  	for (x=0;x<numInX;x++)
-  	{	
-  		outbuffer[2*(x+firstReadX)]=inbuffer[x*2];
-  		outbuffer[2*(x+firstReadX)+1]=inbuffer[x*2+1];
-  	}
-  	FWRITE(outbuffer,pixelSize,outMaxX,out);
-  	if (0==y%100) printf("\tWriting output image line %i...\n",y);
-  }
+   firstReadX=MAX(0,-startX);
+   numInX=MIN(MIN(outMaxX,inMaxX-(firstReadX+startX)),outMaxX-firstReadX);
+   lastReadY=MIN(outMaxY,inMaxY-startY);
+
+   for (;y<lastReadY;y++) {
+       const int SLCPixSize=4;
+       int inputY = y+startY;
+       int inputX = firstReadX+startX;
+
+       FSEEK64(in,(inputY+1)*in_bytes_per_line+line_header+SLCPixSize*inputX,0);
+       FREAD(inbuffer,SLCPixSize,numInX,in);
+       for (x=0;x<numInX;x++) {
+           outbuffer[2*(x+firstReadX)]=inbuffer[x*2];
+  	   outbuffer[2*(x+firstReadX)+1]=inbuffer[x*2+1];
+       }
+       FWRITE(outbuffer,pixelSize,outMaxX,out);
+       if (0==y%100) printf("    Writing output image line %i...\n",y);
+   }
 /*Reset buffer to zeros and fill remaining pixels.*/
-  for (x=0;x<outMaxX*2;x++)
-  	outbuffer[x]=0.0;
-  for (;y<outMaxY;y++)
-  {
-  	FWRITE(outbuffer,pixelSize,outMaxX,out);
-  	if (0==y%100) printf("\tWriting output image line %i...\n",y);
-  }
-  
-  printf("trim_slc completed sucessfully!\n");
-/*Now close up the files, 'cause we're done.*/
-  FCLOSE(in);
-  FCLOSE(out);
-  return(0);
+   for (x=0;x<outMaxX*2;x++)
+  	 outbuffer[x]=0.0;
+   for (;y<outMaxY;y++)
+   {
+  	 FWRITE(outbuffer,pixelSize,outMaxX,out);
+  	 if (0==y%100) printf("    Writing output image line %i...\n",y);
+   }
+
+/* now that it worked, write the metadata file */
+   meta_write(meta,outfileMeta);
+
+   printf("trim_slc completed sucessfully!\n");
+
+/*Now free memory and close up the files, 'cause we're done.*/
+   meta_free(meta);
+   FREE(inbuffer);
+   FREE(outbuffer);
+   FCLOSE(in);
+   FCLOSE(out);
+
+   exit(EXIT_SUCCESS);
 }
 
-int create_good_ddr(char *fname,int nl,int ns,int ml,int ms,double azpixsiz,double rnpixsiz,int data_type)
-{
-	struct DDR ddr;
-	int stati;
-
-        c_intddr(&ddr);
-
-	ddr.valid[4] = VALID;		/* Projection Units    */
-	ddr.valid[5] = VALID;		/* Projection Distance */
-	ddr.valid[7] = VALID;		/* Increment	       */
-        ddr.nl = nl;
-        ddr.ns = ns;
-        ddr.nbands = 1;
-        ddr.dtype = data_type;
-        ddr.line_inc = 1.0;
-        ddr.sample_inc = 1.0;
-        ddr.master_line = ml;
-        ddr.master_sample = ms;
-        ddr.pdist_x = rnpixsiz;
-        ddr.pdist_y = azpixsiz;
-        strcpy(ddr.proj_units,"METERS");
-
-        stati = c_putddr(fname, &ddr);
-        if (stati != E_SUCC)
-          {
-            printf("Error returned from putddr\n");
-            exit(1);
-          }
-
-	return(0);
-}
 
 void usage(char *name)
 {
-  printf("\n"); 
-  printf("Usage:\n");
-  printf("  %s <inSLC> <outCPX> <top line> <left samp> [<trim ht> "
-	 "<trim wth>]\n\n",name); 
-  printf("\t<inSLC>:  Single-Look-Complex image files (.D and .L or .dat"
-	 " and .ldr)"
- 	 "\n\t<outCPX>: Output file of <inSLC> in float complex format.\n"
-	 "\t<top line>: and <left samp>:\n\t\t"
-	 "  These are the top left coordinates.  Line and sample.\n"
-	 "\t<trim ht> and <trim wth>:\n\t\t"
-	 "  The new height and width (to be trimmed or enlarged");
-  printf("\n\n");
-  printf("Trim_slc: lets you trim/add any # of samples to a Single\n"
-	"Int Complex image file, while creating a DDR, a .meta, and "
-	"converting to \nfloat complex format.\n");
-  printf("\n");
-  printf("Version %4.2f, ASF SAR TOOLS\n\n",version);
-  exit(1);
- 
+ printf("\n"
+	"USAGE:\n"
+	"   %s [-width <width>] [-height <height>]\n"
+	"            <inSLC> <outCPX> <top_line> <left_samp>\n",name); 
+ printf("\n"
+	"REQUIRED ARGUMENTS:\n"
+	"   inSLC      Single-Look-Complex image files (.D and .L or .dat and .ldr)\n"
+ 	"   outCPX     Output file of <inSLC> in float complex format.\n"
+	"   top_line   Upper coordinate (line)\n"
+	"   left_samp  Left coordinate (sample)\n");
+ printf("\n"
+	"OPTIONAL ARGUMENTS:\n"
+	"   -width    Trim/enlarge to new <width>\n"
+	"   -height   Trim/enlarge to new <height>\n");
+ printf("\n"
+	"DESCRIPTION:\n"
+	"   Allows you to trim/add any number of samples to a Single Int Complex image\n"
+	"   file, while creating a .meta, and converting to float complex format.\n");
+ printf("\n"
+	"Version %4.2f, ASF InSAR Tools\n"
+	"\n",VERSION);
+ exit(EXIT_FAILURE);
 }
-
