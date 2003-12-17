da99a2fc6421b5452f1a7f65c0ffb0dcd54c01aa
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-17 00:57:34 +0000

    Remove DDR parameter in init_offset() prototype.

--- src_ifm/refine_offset/offset.h
+++ src_ifm/refine_offset/offset.h
@@ -1,4 +1,4 @@
-void *init_offset(char *ceos,meta_parameters *i,struct DDR *ddr);
+void *init_offset(char *ceos,meta_parameters *i);
 
 void add_offset(void *l,
 	double x,double y,double lat,double lon,double elev);
