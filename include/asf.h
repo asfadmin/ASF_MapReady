a759a48ca42bd7c30326f3298814223bb4a4d5ab
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-24 21:13:56 +0000

    Comment spelling fix.

--- include/asf.h
+++ include/asf.h
@@ -39,7 +39,7 @@ void bail(const char *message, ...)/* ; is coming, don't worry.  */
 /* The GNU C compiler can give us some special help if compiling with
    -Wformat or a warning option that implies it.  */
 #ifdef __GNUC__
-     /* Function attribute format says function is variadic al la
+     /* Function attribute format says function is variadic a la
         printf, with argument 1 its format spec and argument 2 its
         first optional argument corresponding to the spec.  Function
         attribute noreturn says function doesn't return.  */
