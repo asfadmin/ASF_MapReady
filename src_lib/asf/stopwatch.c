66107025f072b77ab4ab1df440f8000b37957c88
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-24 23:13:36 +0000

    Made startTime static, added check that a Start* routine had been run in Stop* routines, reformatted.

--- src_lib/asf/stopwatch.c
+++ src_lib/asf/stopwatch.c
@@ -29,44 +29,47 @@ PROGRAM HISTORY:
 
 /*#ifdef __MWERKS__ */
 
-	clock_t startTime;
+/* Start time to be set and remembered by the Start* routines.  We
+   initialize this to a sentinel value meaning which is to be
+   interpreted as meaning "Start* hasn't been run yet".  */
+static const clock_t start_time_unset_sentinel = (clock_t) -1;
+static clock_t startTime = start_time_unset_sentinel;
 
-	void StartWatch(void)
-	{
-	  startTime=clock();
-	  assert (startTime != (clock_t) -1);
-	}
+void StartWatch(void) 
+{
+  startTime = clock ();
+  assert (startTime != (clock_t) -1);
+}
 
-	void StopWatch(void)
-	{
-	  clock_t stopTime=clock();
-	  assert (stopTime != (clock_t) -1);
-	  float elapsed = stopTime-startTime;
-	  elapsed /= CLOCKS_PER_SEC;
-	  printf("Total CPU time = %f seconds.\n\n",elapsed);
-	}
+void StopWatch(void)
+{
+  /* The stopwatch must already have been started.  */
+  assert (startTime != start_time_unset_sentinel);
+  clock_t stopTime=clock();
+  assert (stopTime != (clock_t) -1);
+  float elapsed = stopTime - startTime;
+  elapsed /= CLOCKS_PER_SEC;
+  printf ("Total CPU time = %f seconds.\n\n",elapsed);
+}
 
-	void StartWatchLog(FILE *fLog)
-	{
-	  time_t t;
-	  char *c;
+void StartWatchLog(FILE *fLog)
+{
+  startTime = clock ();
+  assert (startTime != (clock_t) -1);
+  time_t t = time (NULL);
+  char *c = asctime (localtime (&t));
+  fprintf (fLog, "Stopwatch started on date: %s", c);
+}
 
-	  startTime=clock();
-	  assert (startTime != (clock_t) -1);
-
-	  t = time(NULL);
-	  c = asctime(localtime(&t));
-	  fprintf(fLog, "Stopwatch started on date: %s", c);
-	}
-
-	void StopWatchLog(FILE *fLog)
-	{
-	  clock_t stopTime=clock();
-	  assert (stopTime != (clock_t) -1);
-	  float elapsed = stopTime-startTime;
-	  elapsed /= CLOCKS_PER_SEC;
-	  fprintf(fLog, "Total CPU time = %f seconds.\n\n",elapsed);
-	}
+void StopWatchLog(FILE *fLog)
+{
+  assert (startTime != start_time_unset_sentinel);
+  clock_t stopTime = clock ();
+  assert (stopTime != (clock_t) -1);
+  float elapsed = stopTime - startTime;
+  elapsed /= CLOCKS_PER_SEC;
+  fprintf (fLog, "Total CPU time = %f seconds.\n\n", elapsed);
+}
 
 /*
 #else
