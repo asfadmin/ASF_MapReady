a9adf9b3f3300117ca27b852bb39b2954461c200
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2003-12-24 23:15:24 +0000

    Removed commented out code, header comments.

--- src_lib/asf/stopwatch.c
+++ src_lib/asf/stopwatch.c
@@ -1,33 +1,9 @@
-/****************************************************************
-FUNCTION NAME: StartWatch()  & StopWatch()
+/* Functions for timing programs.  */
 
-SYNTAX: void StartWatch();
-	void StopWatch();
-	void StartWatch(FILE *fLog);
-	void StopWatch(FILE *fLog);
-
-PARAMETERS:
-    NAME:	TYPE:		PURPOSE:
-    --------------------------------------------------------
-
-DESCRIPTION:
-    Keeps track of when a program starts and stops. Stopwatch will print out
-    the elapsed CPU time.
-
-RETURN VALUE:
-    None.
-
-SPECIAL CONSIDERATIONS:
-
-PROGRAM HISTORY:
-     1.0 - Mike Shindle    4/96  Original Creation
-****************************************************************/
 #include <assert.h>
 #include <time.h>
 
 #include "asf.h"
-
-/*#ifdef __MWERKS__ */
 
 /* Start time to be set and remembered by the Start* routines.  We
    initialize this to a sentinel value meaning which is to be
@@ -70,33 +46,5 @@ void StopWatchLog(FILE *fLog)
   elapsed /= CLOCKS_PER_SEC;
   fprintf (fLog, "Total CPU time = %f seconds.\n\n", elapsed);
 }
-
-/*
-#else
-#include <sys/time.h>
-
-	/ this half for running under old SunOS /
-	struct timeval tp1;
-
-	void StartWatch()
-	{
-	  struct timezone tzp1;
-
-	  gettimeofday(&tp1,&tzp1);
-	}
-
-	void StopWatch()
-	{
-	  long elapsed;
-	  struct timeval tp2;
-	  struct timezone tzp2;
-
-	  gettimeofday(&tp2,&tzp2);
-	  elapsed = tp2.tv_sec - tp1.tv_sec;
-	  printf("Total wall clock time = %li seconds.\n\n",elapsed);
-	}
-
-#endif
-*/
 
 
