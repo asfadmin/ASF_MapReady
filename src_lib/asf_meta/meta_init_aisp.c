9092e6f6e4127818bfb99d6d6490638004f98a07
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-01-28 03:35:14 +0000

    Change use of appendExt to create_name (fixes a memory leak). Set new meta->sar block values earth_radius and satellite height.

--- src_lib/asf_meta/meta_init_aisp.c
+++ src_lib/asf_meta/meta_init_aisp.c
@@ -25,20 +25,15 @@ void aisp_init(const char *fName,meta_parameters *meta)
 {
 	struct AISP_PARAMS g;
 /*Check to see if the AISP input file exists.*/
-	char *inputFileName;
+	char inputFileName[256];
 	if (!extExists(fName,".in"))
 		return;/*If the AISP input file does not exist, just bail.*/
 
 /*If the AISP input file exists, read parameters from it.*/
-	inputFileName = appendExt(fName,".in");
+	create_name(inputFileName,fName,".in");
 	read_params(inputFileName,&g);
-	free(inputFileName);
 
 /*Now sift these AISP globals into SAR parameters.*/
-/* DEPRICATED FIELDS
- *	meta->ifm->er = g.re;
- *	meta->ifm->ht = g.re+g.ht;
- */
  	meta->sar->look_count              = g.nlooks;
 	meta->sar->deskewed                = g.deskew;
 	meta->sar->range_time_per_pixel    = 1.0/g.fs;
@@ -48,6 +43,8 @@ void aisp_init(const char *fName,meta_parameters *meta)
 	meta->sar->slant_range_first_pixel = g.r00;
 	meta->sar->wavelength              = g.wavl;
 	meta->sar->prf                     = g.prf;
+	meta->sar->earth_radius            = g.re;
+	meta->sar->satellite_height        = g.re+g.ht;
 	meta->sar->range_doppler_coefficients[0] = g.fd;
 	meta->sar->range_doppler_coefficients[1] = g.fdd;
 	meta->sar->range_doppler_coefficients[2] = g.fddd;
