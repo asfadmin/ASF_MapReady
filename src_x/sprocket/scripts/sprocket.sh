d7cede84be15208921ff75104884ab5ba23d521d
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-03-08 23:10:20 +0000

    Dynamic figuring of the CLASSPATH variable. Uses perl for the regular expression matching. If there was a .zsh suffix, that was removed from the usage.

--- src_x/sprocket/scripts/sprocket.sh
+++ src_x/sprocket/scripts/sprocket.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-export CLASSPATH=/home/pdenny/asf_tools/java
+# To dynamically figure the path to all our Java classes we use perl to parse
+# up the absolute path to this script ($0), and lop off everything after
+# 'asf_tools'. We can then append the /java directory to our newly found path
+# to asf_tools, and blamo, we've got a classpath. Very ugly, yes, I agree.
+# example:
+#    $0 = /usr/local/asf_tools/bin/linux/script_name
+#    perl returns /usr/local/asf_tools
+#    this script appends /java
+#    and BLAMO, our classpath is /usr/local/asf_tools/java
+perl_command='perl -p -e '"'"'s/((\/|\w)+asf_tools)+(\/|\w)*/$1/;'"'"
+CLASSPATH_TEMP=`echo $0 | eval $perl_command`
+CLASSPATH="${CLASSPATH_TEMP}/java"
+export CLASSPATH
+
+# Call pvs (actually sprocket, but it was called pvs to begin with)
 java pvs
--- src_x/sprocket/scripts/target_selector.sh
+++ src_x/sprocket/scripts/target_selector.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-export CLASSPATH=/home/pdenny/asf_tools/java
+# To dynamically figure the path to all our Java classes we use perl to parse
+# up the absolute path to this script ($0), and lop off everything after
+# 'asf_tools'. We can then append the /java directory to our newly found path
+# to asf_tools, and blamo, we've got a classpath. Very ugly, yes, I agree.
+# example:
+#    $0 = /usr/local/asf_tools/bin/linux/script_name
+#    perl returns /usr/local/asf_tools
+#    this script appends /java
+#    and BLAMO, our classpath is /usr/local/asf_tools/java
+perl_command='perl -p -e '"'"'s/((\/|\w)+asf_tools)+(\/|\w)*/$1/;'"'"
+CLASSPATH_TEMP=`echo $0 | eval $perl_command`
+CLASSPATH="${CLASSPATH_TEMP}/java"
+export CLASSPATH
 
 # Check for no args
 if [ $# -ne "1" ]
 then
-   echo  "Usage: target_selector.zsh  <image root>"
+   echo ""
+   echo "Usage: target_selector <image root>"
+   echo ""
    exit 1
 fi
 
