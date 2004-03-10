3a0c056c6b8421dca522925ae9d5f75dc6cb8fb9
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-03-10 02:00:59 +0000

    Added a data file extention constant (would have been data_file_ext, but all the constants are extentions).

--- src_x/sprocket/java_util/Constants.java
+++ src_x/sprocket/java_util/Constants.java
@@ -1,5 +1,6 @@
 
 public class Constants {
+   public static String data_file = ".img";
    public static String metafile = ".metadata";
    public static String ground_target_file = ".ground_target";
    public static String ground_target_file_located = ".ground_target_final";
