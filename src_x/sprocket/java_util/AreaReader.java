412b7bb631aeb3cbe3798ef0ac69d3f8bb961ed3
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-03-18 22:26:31 +0000

    Whitespace reformatting for easier reading & removed unused variable.

--- src_x/sprocket/java_util/AreaReader.java
+++ src_x/sprocket/java_util/AreaReader.java
@@ -3,34 +3,31 @@ import java.awt.* ;
 import java.io.* ;
 
 public class AreaReader {
-     private Vector v;
-     public AreaReader ( InputStream in)
-       {
-	
-	 try { 
- 	   Reader r = new BufferedReader(new InputStreamReader(in));
-           StreamTokenizer st = new StreamTokenizer(r);
-	   Area a;
+	private Vector v;
 
- 	   v = new Vector();
+	public AreaReader ( InputStream in)
+	{
+	   try { 
+		Reader r = new BufferedReader(new InputStreamReader(in));
+		StreamTokenizer st = new StreamTokenizer(r);
 
-	   st.resetSyntax(); 
-	   st.quoteChar('\'');
-	   st.whitespaceChars('-','-');
-	   st.whitespaceChars(' ',' ');
+		v = new Vector();
 
-	   while ( st.ttype != st.TT_EOF )
-		v.add ( new Area (st) );
-	  }
-	 catch ( IOException e)
-	     {
+		st.resetSyntax(); 
+		st.quoteChar('\'');
+		st.whitespaceChars('-','-');
+		st.whitespaceChars(' ',' ');
+
+		while ( st.ttype != st.TT_EOF )
+			v.add ( new Area (st) );
+	   }
+	   catch ( IOException e) {
 		System.err.println( "An Error occured:" + e );
-	     }
-       }
-
-     public Vector getElements()
-        {
-	  return ( new Vector ( v ));
+	   }
 	}
-         
-  }
+
+	public Vector getElements()
+	{
+		return ( new Vector ( v ));
+	}
+}
