9b4694b7eb32d6cb6395bb9ccf3a7fa97a9af920
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-03-19 00:43:58 +0000

    Whitespace changes

--- src_x/sprocket/java_util/TargetReader.java
+++ src_x/sprocket/java_util/TargetReader.java
@@ -3,30 +3,29 @@ import java.awt.* ;
 import java.io.* ;
 
 public class TargetReader {
-     private Vector v;
-     public TargetReader ( InputStream in)
-       {
-	
-	 try { 
-	   Reader r = new BufferedReader(new InputStreamReader(in));
-           StreamTokenizer st = new StreamTokenizer(r);
- 	   v = new Vector();
-	   st.resetSyntax(); 
-	   st.quoteChar('\'');
-	   st.whitespaceChars('-','-');
-	   st.whitespaceChars(' ',' ');
-	   while ( st.ttype != st.TT_EOF )
-		v.add ( new Target (st) );
-	   v.remove(v.size()-1);
-	  }
-	 catch ( IOException e)
-	     {
-		System.err.println( "An Error occured:" + e );
-	     }
-       }
+   private Vector v;
 
-     public Vector getElements()
-        {
-	  return ( new Vector ( v ));
-	}
-  }
+   public TargetReader (InputStream in)
+   {
+      try {
+         Reader r = new BufferedReader(new InputStreamReader(in));
+         StreamTokenizer st = new StreamTokenizer(r);
+         v = new Vector();
+         st.resetSyntax(); 
+         st.quoteChar('\'');
+         st.whitespaceChars('-','-');
+         st.whitespaceChars(' ',' ');
+         while (st.ttype != st.TT_EOF)
+            v.add (new Target (st) ;
+         v.remove(v.size()-1);
+      }
+      catch (IOException e) {
+         System.err.println("An Error occured:" + e);
+      }
+   }
+
+   public Vector getElements()
+   {
+      return (new Vector(v));
+   }
+}
