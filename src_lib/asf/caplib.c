8d1ae4fa2e30e42646f9cf17c7dab371d7e85142
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-04-06 20:23:50 +0000

    Allowed FSEEK64, FTELL64, and FOPEN to use their plain old 32 bit calls if the operating system is windows. Cygwin says that it supports the 64 bit stuff but compilation chokes when this library is included. Also reformatted some of the error reporting (for both terminal and log file).

--- src_lib/asf/caplib.c
+++ src_lib/asf/caplib.c
@@ -1,58 +1,40 @@
+/**************************** Fail-Free Library *******************************
+These are convenience replacements for the standard library routines.  They
+differ from the standard routines ONLY in that they will not fail-- if they
+encounter an error, they will print an informative error message to stderr and
+exit.  This way, programs can be written more easily, quickly, and cleanly,
+and users get better error messages.
+
+Note that to minimize impact on readability and portability, the return types,
+error codes, and ALL functionality of the library routines will be passed
+through.  For example, MALLOC'ing zero bytes is OK, because malloc'ing zero
+bytes is OK.
+
+The only difference is that malloc might return NULL, and MALLOC will never
+return NULL (because it will exit before that).
+
+This way, we can have maximum trust in these routines, and use them as much as
+possible.
+******************************************************************************/
 #include "caplib.h"
 #include "log.h"
 
-/*************** Fail-Free Library *************
-	These are convenience replacements for
-the standard library routines.  They differ from
-the standard routines ONLY in that they will not
-fail-- if they encounter an error, they will
-print an informative error message to stderr and
-exit.  This way, programs can be written more
-easily, quickly, and cleanly, and users get
-better error messages.
-
-	Note that to minimize impact on readability
-and portability, the return types, error codes,
-and ALL functionality of the library routines 
-will be passed through.  For example, 
-MALLOC'ing zero bytes is OK, because malloc'ing 
-zero bytes is OK.
-
-	The only difference is that malloc might
-return NULL, and MALLOC will never return NULL
-(because it will exit before that).
-
-	This way, we can have maximum trust in these
-routines, and use them as much as possible.
-************************************************/
 void programmer_error(char *mess)
 {
-	fprintf(stderr,"********* ERROR!  Internal program error *******\n"
+	char error_message[1024];
+	sprintf(error_message,
+		"********* ERROR!  Internal program error *******\n"
 		"* \n"
 		"*     The following programmer error occured:\n"
 		"* {%s}"
 		"*     This isn't supposed to make sense to users, but\n"
 		"* you as a user aren't supposed to see this.\n"
-		"* Please contact the Advanced Product Development\n"
-		"* on the web at http://www.asf.alaska.edu/apd\n"
-		"* via email at apd@asf.alaska.edu or\n"
-		"* talk to Rudi Gens at (907)474-7621.\n"
+		"* Please contact ASF via email at uso@asf.alaska.edu.\n"
 		"**     Program terminating... internal program error.\n",
 		mess);
-
+	fprintf(stderr,error_message);
 	if (fLog!=NULL)
-	  fprintf(fLog,"********* ERROR!  Internal program error *******\n"
-		"* \n"
-		"*     The following programmer error occured:\n"
-		"* {%s}"
-		"*     This isn't supposed to make sense to users, but\n"
-		"* you as a user aren't supposed to see this.\n"
-		"* Please contact the Advanced Product Development\n"
-		"* on the web at http://www.asf.alaska.edu/apd\n"
-		"* via email at apd@asf.alaska.edu or\n"
-		"* talk to Rudi Gens at (907)474-7621.\n"
-		"**     Program terminating... internal program error.\n",
-		mess);
+	  fprintf(fLog,error_message);
 
 	exit(199);
 }
@@ -87,25 +69,16 @@ void bail(const char *mess, ...)
 size_t bytes_mallocked=0;
 void *MALLOC(size_t size)
 {
-	void *ret=malloc(size);
-	bytes_mallocked+=size;
+	void *ret = malloc(size);
+	char error_message[1024];
+
+	bytes_mallocked += size;
 	if (ret==NULL)
 	{
 #ifdef ENOMEM
 		if (errno==ENOMEM) /*There will never be enough memory.*/
 		{
-			fprintf(stderr,
-			"*****  ERROR!  Out of Memory *******\n"
-			"* \n"
-			"*    This program needs more memory than your\n"
-			"* system has.  It was asking for %i bytes of memory.\n"
-			"*    You might ask your system administrator to\n"
-			"* increase the size of your swap partition, or add\n"
-			"* more real memory to your system.\n"
-			"**    Program terminating... Out of memory.\n",(int)size);
-
-			if (fLog!=NULL)
-			  fprintf(fLog,
+			sprintf(error_message,
 				"*****  ERROR!  Out of Memory *******\n"
 				"* \n"
 				"*    This program needs more memory than your\n"
@@ -113,7 +86,11 @@ void *MALLOC(size_t size)
 				"*    You might ask your system administrator to\n"
 				"* increase the size of your swap partition, or add\n"
 				"* more real memory to your system.\n"
-				"**    Program terminating... Out of memory.\n",(int)size);
+				"**    Program terminating... Out of memory.\n",
+				(int)size);
+			fprintf(stderr,error_message);
+			if (fLog!=NULL)
+			  fprintf(fLog,error_message);
 
 			exit(200);
 		}
@@ -125,7 +102,7 @@ void *MALLOC(size_t size)
 			ret=malloc(size);/*... try again.*/
 			if (ret==NULL)
 			{ /*If the call failed again, we just bail.*/
-				fprintf(stderr,
+				sprintf(error_message,
 					"*****  ERROR!  Out of Memory *******\n"
 					"* \n"
 					"*    This program needs more memory than your\n"
@@ -133,18 +110,11 @@ void *MALLOC(size_t size)
 					"* asking for %i bytes of memory.\n"
 					"*    You might try running the program again\n"
 					"* later, when there are fewer people on.\n"
-					"**    Program terminating... Out of memory.\n",(int)size);
-
+					"**    Program terminating... Out of memory.\n",
+					(int)size);
+				fprintf(stderr,error_message);
 				if (fLog!=NULL)
-				  fprintf(fLog,
-					"*****  ERROR!  Out of Memory *******\n"
-					"* \n"
-					"*    This program needs more memory than your\n"
-					"* system has free at this moment.  It was \n"
-					"* asking for %i bytes of memory.\n"
-					"*    You might try running the program again\n"
-					"* later, when there are fewer people on.\n"
-					"**    Program terminating... Out of memory.\n",(int)size);
+				  fprintf(fLog,error_message);
 
 			exit(201);
 			}
@@ -152,7 +122,7 @@ void *MALLOC(size_t size)
 		}
 #endif
 	 /*An unknown memory error might cause us to get here.*/
-		fprintf(stderr,
+	 	sprintf(error_message,
 			"*****  ERROR!  Out of Memory *******\n"
 			"* \n"
 			"*    This program needs more memory than your\n"
@@ -160,18 +130,11 @@ void *MALLOC(size_t size)
 			"*    You might ask your system administrator to\n"
 			"* increase the size of your swap partition, or add\n"
 			"* more real memory to your system.\n"
-			"**    Program terminating... Out of memory.\n",(int)size);
-
+			"**    Program terminating... Out of memory.\n",
+			(int)size);
+		fprintf(stderr,error_message);
 		if (fLog!=NULL)
-		  fprintf(fLog,
-			"*****  ERROR!  Out of Memory *******\n"
-			"* \n"
-			"*    This program needs more memory than your\n"
-			"* system has.  It was asking for %i bytes of memory.\n"
-			"*    You might ask your system administrator to\n"
-			"* increase the size of your swap partition, or add\n"
-			"* more real memory to your system.\n"
-			"**    Program terminating... Out of memory.\n",(int)size);
+		  fprintf(fLog,error_message);
 
 		exit(202);
 	}
@@ -186,43 +149,49 @@ void FREE(void *ptr)
 
 FILE *FOPEN(const char *file,const char *mode)
 {
+#if defined(win32)
+	/* Although there is a man page for fopen64 on windows Cygwin
+	 * it seems that the 64 bit functionality is not yet in place */
+	FILE *ret=fopen(file,mode);
+#else
 	FILE *ret=fopen64(file,mode);
+#endif
+	char error_message[1024];
+
 	if (ret==NULL)
 	{
-		fprintf(stderr,
+		sprintf(error_message,
 			"*****  ERROR!  Cannot Open File! *******\n"
 			"* \n"
 			"*    This program tried to open the file named\n"
 			"* '%s' ",file);
-		if (fLog!=NULL)
-		  fprintf(fLog,
-			"*****  ERROR!  Cannot Open File! *******\n"
-			"* \n"
-			"*    This program tried to open the file named\n"
-			"* '%s' ",file);
+		fprintf(stderr,error_message);
+		if (fLog!=NULL)  fprintf(fLog,error_message);
 
 		if (mode[0]=='r') {
-			fprintf(stderr,"for reading.\n");
-			if (fLog!=NULL) fprintf(fLog,"for reading.\n");
+			sprintf(error_message,"for reading.\n");
+			fprintf(stderr,error_message);
+			if (fLog!=NULL) fprintf(fLog,error_message);
 		}
 
 		else if (mode[0]=='w'||mode[0]=='a') {
-			fprintf(stderr,"for writing.\n");
-			if (fLog!=NULL) fprintf(fLog,"for writing.\n");
+			sprintf(error_message,"for writing.\n");
+			fprintf(stderr,error_message);
+			if (fLog!=NULL) fprintf(fLog,error_message);
 		}
 
 		else {
-			fprintf(stderr,"in the unknown way '%s'.\n",mode);
-			if (fLog!=NULL) fprintf(fLog,"in the unknown way '%s'.\n",mode);
+			sprintf(error_message,"in the unknown way '%s'.\n",mode);
+			fprintf(stderr,error_message);
+			if (fLog!=NULL) fprintf(fLog,error_message);
 		}
 
-		fprintf(stderr,"* This file does not exist or cannot be opened.\n"
-				"* Please check the file name and try again.\n"
-				"**    Program terminating... Cannot open file.\n");
-		if (fLog!=NULL)
-		  fprintf(fLog,"* This file does not exist or cannot be opened.\n"
-				"* Please check the file name and try again.\n"
-				"**    Program terminating... Cannot open file.\n");
+		sprintf(error_message,
+			"* This file does not exist or cannot be opened.\n"
+			"* Please check the file name and try again.\n"
+			"**    Program terminating... Cannot open file.\n");
+		fprintf(stderr,error_message);
+		if (fLog!=NULL) fprintf(fLog,error_message);
 
 		exit(203);
 	}
@@ -233,6 +202,8 @@ FILE *FOPEN(const char *file,const char *mode)
 size_t FREAD(void *ptr,size_t size,size_t nitems,FILE *stream)
 {
 	size_t ret;
+	char error_message[1024];
+
 	if (ptr==NULL)
 		programmer_error("NULL data buffer passed to FREAD.\n");
 	if (stream==NULL)
@@ -242,45 +213,35 @@ size_t FREAD(void *ptr,size_t size,size_t nitems,FILE *stream)
 	{
 		if (0!=feof(stream))
 		{
-			fprintf(stderr,
- 				"*****  ERROR!  Read past end of file! *******\n"
-				"* \n"
-				"*    This program tried to read %i bytes past\n"
-				"* the end of file stream 0x%x.  You might want to\n"
-				"* check any image-size related parameters you passed\n"
-				"* to the program.\n"
-				"**    Program terminating... Attempted read past end of file.\n",(int)size*nitems,(int)stream);
-
-			if (fLog!=NULL)
-			  fprintf(fLog,
+			sprintf(error_message,
 				"*****  ERROR!  Read past end of file! *******\n"
 				"* \n"
 				"*    This program tried to read %i bytes past\n"
 				"* the end of file stream 0x%x.  You might want to\n"
 				"* check any image-size related parameters you passed\n"
 				"* to the program.\n"
-				"**    Program terminating... Attempted read past end of file.\n",(int)size*nitems,(int)stream);
-
+				"**    Program terminating... Attempted read past end of file.\n",
+				(int)size*nitems,(int)stream);
+			fprintf(stderr,error_message);
+			if (fLog!=NULL)  fprintf(fLog,error_message);
 			exit(204);
 		}
-		fprintf(stderr,
+		
+		sprintf(error_message,
 			"*****  ERROR!  Error reading file! *******\n"
 			"* \n"
 			"*    When reading the file stream 0x%x, this program\n"
-			"*  encountered the following error:\n",(int)stream);
-		if (fLog!=NULL)
-		  fprintf(fLog,
-			"*****  ERROR!  Error reading file! *******\n"
-			"* \n"
-			"*    When reading the file stream 0x%x, this program\n"
-			"*  encountered the following error:\n",(int)stream);
+			"*  encountered the following error:\n",
+			(int)stream);
+		fprintf(stderr,error_message);
+		if (fLog!=NULL) fprintf(fLog,error_message);
 
 		perror(NULL);
-		fprintf(stderr,"* Note that this was NOT a read-past end of file error.\n"
+		sprintf(error_message,
+			"* Note that this was NOT a read-past end of file error.\n"
 			"**   Program terminating... Error encounter while reading.\n");
-		if (fLog!=NULL)
-		  fprintf(fLog,"* Note that this was NOT a read-past end of file error.\n"
-			"**   Program terminating... Error encounter while reading.\n");
+		fprintf(stderr,error_message);
+		if (fLog!=NULL) fprintf(fLog,error_message);
 		
 		exit(205);
 	}
@@ -289,6 +250,8 @@ size_t FREAD(void *ptr,size_t size,size_t nitems,FILE *stream)
 size_t FWRITE(const void *ptr,size_t size,size_t nitems,FILE *stream)
 {
 	size_t ret;
+	char error_message[1024];
+	
 	if (ptr==NULL)
 		programmer_error("NULL data buffer passed to FWRITE.\n");
 	if (stream==NULL)
@@ -296,32 +259,26 @@ size_t FWRITE(const void *ptr,size_t size,size_t nitems,FILE *stream)
 	ret=fwrite(ptr,size,nitems,stream);
 	if (ret!=nitems)
 	{
-		fprintf(stderr,"*******  ERROR writing file! *******\n"
+		sprintf(error_message,
+			"*******  ERROR writing file! *******\n"
 			"*\n"
 			"*   This program tried to write %i bytes to\n"
-			"* the stream 0x%x.  This writing failed, because:\n",(int) size*nitems,(int)stream);
-		if (fLog!=NULL)
-		  fprintf(fLog,"*******  ERROR writing file! *******\n"
-			"*\n"
-			"*   This program tried to write %i bytes to\n"
-			"* the stream 0x%x.  This writing failed, because:\n",(int) size*nitems,(int)stream);
+			"* the stream 0x%x.  This writing failed, because:\n",
+			(int) size*nitems,(int)stream);
+		fprintf(stderr,error_message);
+		if (fLog!=NULL) fprintf(fLog,error_message);
 
 		perror(NULL);
-		fprintf(stderr,
+		sprintf(error_message,
 			"* The most common cause of file write errors\n"
 			"* is running out of disk space.  Try using the\n"
 			"*'df -k' command to check the amount of free space\n"
 			"* on your current volume.  Many of ASF's utilities\n"
 			"* require very large amounts of disk space.\n"
 			"**   Program terminating... error during file write.\n");
+		fprintf(stderr,error_message);
 		if (fLog!=NULL)
-		  fprintf(fLog,
-			"* The most common cause of file write errors\n"
-			"* is running out of disk space.  Try using the\n"
-			"*'df -k' command to check the amount of free space\n"
-			"* on your current volume.  Many of ASF's utilities\n"
-			"* require very large amounts of disk space.\n"
-			"**   Program terminating... error during file write.\n");
+		  fprintf(fLog,error_message);
 
 		exit(206);
 	}
@@ -347,13 +304,18 @@ int FSEEK64(FILE *stream,long long offset,int ptrname)
 
 #if defined(irix)
 	ret=fseek64(stream,offset,ptrname);
+#elif defined(win32)
+	/* Although there is a man page for fseeko64 on Windows cygwin
+	 * it appears that 64 bit ops are not yet supported there */
+	ret=fseek(stream,(long)offset,ptrname);
 #else
 	ret=fseeko64(stream,offset,ptrname);
 #endif
 	if (ret==-1)
 	{
 		fprintf(stderr, "The file offset passed is: %lli\n", offset);
-		if (fLog!=NULL) fprintf(fLog, "The file offset passed is: %lli\n", offset);
+		if (fLog!=NULL)
+		   fprintf(fLog, "The file offset passed is: %lli\n", offset);
 		programmer_error("Stream passed to FSEEK64 is not seekable.\n");
 	}
 	return ret;
@@ -366,6 +328,10 @@ long long FTELL64(FILE *stream)
 		programmer_error("NULL file pointer passed to FTELL64.\n");
 #if defined(irix)
 	ret=(long long)ftell64(stream);
+#elif defined(win32)
+	 /* Although there is a man page for ftello64 on Windows cygwin,
+	  * it appears that 64 bit ops are nont yet supported there */
+	ret=(long long)ftell(stream);
 #else
 	ret=ftello64(stream);
 #endif
