a6f81d9cde798235b2f5b75cb863e6386295a780
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-04-30 03:03:41 +0000

    Added prototypes for some new functions in the asf.a library. Couple to fileUtil that deal with filename parsing. Also some from a whole new file that deals with CEOS file naming.

--- include/asf.h
+++ include/asf.h
@@ -31,6 +31,12 @@
 # define TRUE (!FALSE)
 #endif
 
+#if defined(win32)
+#define DIR_SEPARATOR '\\'
+#else
+#define DIR_SEPARATOR '/'
+#endif
+
 /* Print an error with printf-style formatting codes and args, then die.  */
 void bail(const char *message, ...)/* ; is coming, don't worry.  */
 /* The GNU C compiler can give us some special help if compiling with
@@ -79,26 +85,78 @@ char *findExt(char *name);
    with newExt.  If no dot extension exists originally, the new
    extension is appended.  */
 char *appendExt(const char *name,const char *newExt);
-/* Ruturn true iff file name exists and is readable.  */
+/* Return true iff file name exists and is readable.  */
 int fileExists(const char *name);
 /* Return true iff fileExists(appendExt(name, newExt)) would return
    true, but without the memory leak fileExists(appendExt(name,
    newExt)) would produce. */
 int extExists(const char *name,const char *newExt);
+
 /* Creates name out by clipping off the rightmost dot extension, if
    any, of in, and then appending newExt.  out must point to existing
    memory large enough to store the result.  */
 void create_name(char *out,const char *in,const char *newExt);
 
+/* Takes a string and fills one pre-allocated array with any path prior to
+   the file name, and fills another pre-allocated array with the file name */
+void split_dir_and_file(const char *inString, char *dirName, char *fileName);
+
+#define PREPENDED_EXTENSION -1
+#define APPENDED_EXTENSION   1
+/* Fills 'extension' with the file's extension and returns TRUE if there is
+   indeed an extension. Otherwise it returns FALSE and fills 'extension' with
+   an empty string. If 'side' is PREPENDED_EXTENSION, then it looks for the
+   extension at the front of the file name. If 'side' is APPENDED_EXTENSION, it
+   looks for the extension at the end of the file name. Otherwise it returns
+   FALSE and fills 'extension' with an empty string. The extension separator
+   is a '.' It assumes 'fileName' is only the file name (no path included) */
+int split_base_and_ext(char *fileName,int side,char *baseName,char *extension);
+
+/* first tries to open the given image name, then appends ".img" and tries again
+   It returns a pointer to the opened file.*/
 FILE *fopenImage(const char *name,const char *accessType);
 
-/* Size of line chunk to read or write.  */
-#define CHUNK_OF_LINES 256
+
+/*****************************************
+ * get_ceos_names: Functions that match CEOS files with each other using their
+ * extensions and testing to see if the files actually exist*/
+
+/* Returns TRUE if the file exists and has an accepted ASF CEOS leader file
+   extension  */
+int has_ceos_leader_extension(const char *ceosName);
+
+/* Returns TRUE if the file exists and has an accepted ASF CEOS data file
+   extension  */
+int has_ceos_data_extension(const char *ceosName);
+
+/* Checks to see if the file extension of each ceos file name is an accepted
+   one, and if it has the correct pairing (ie a .D has a matching .L) */
+int has_legit_ceos_extensions(const char *dataName, const char *metaName);
+
+/* Enum for pairs of ceos extensions */
+typedef enum {
+   ASF_CEOS_D_L_PAIR=1,     /* .D   data and .L   meta */
+   ASF_CEOS_RAW_LDR_PAIR,   /* .RAW data and .LDR meta */
+   FOCUS_CEOS_DAT_LEA_PAIR  /* DAT. data and LEA. meta */
+} ceos_file_pairs_t;
+
+/* Given the name of a file (potentially with path in front of it), determine if
+   it is one of a CEOS file pair (depending on our accepted CEOS file
+   extensions). If so populate dataName & metaName with the appropriate names
+   and return the appropriate ENUM ceos_file_pairs_t value. Otherwise exit the
+   program with a failure.  */
+ceos_file_pairs_t get_ceos_names(const char *ceosName, char *dataName,
+                                 char *metaName);
+
 
 /*****************************************
  * ioLine: Grab any data type and fill a buffer of _type_ data.
  * Assumes that the datae file contains data in big endian order and
  * returns data in host byte order. Implemented in asf.a/ioLine.c */
+
+/* Size of line chunk to read or write.  */
+#define CHUNK_OF_LINES 256
+
 int get_float_line(FILE *file, meta_parameters *meta, int line_number,
 		float *dest);
 int get_float_lines(FILE *file, meta_parameters *meta, int line_number,
