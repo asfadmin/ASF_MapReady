c754ee03be1326674785b57fb411904794b64f26
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-05-04 22:39:15 +0000

    Undo the casting of unsigned byte arrays to the bitfield using structure ERS_raw_aux. Now we use bitwise ops to get the data out of the raw_aux.

--- src/import2asf/decoder_ERS.c
+++ src/import2asf/decoder_ERS.c
@@ -14,43 +14,43 @@ This file ingests VEXCEL Level-0 products from the ERS satellite.
  * ERS_readNextPulse:
  * Fetches the next echo from the signal data, and unpacks it into iqBuf. Skips
  * over any blank lines. Updates nFrames with number of frames read. Currently
- * the 'inName' and 'outName' function parameters only exist so as to match this
- * function up with the readPulseFunc function pointer   */
+ * the 'inName' and 'outName' function parameters only exist so as to match
+ * this function up with the readPulseFunc function pointer   */
 int nLines=0;
 void ERS_readNextPulse(bin_state *s,iqType *iqBuf, char *inName, char *outName)
 {
-	int i;
-	iqType *iqCurr=iqBuf;
-	ERS_frame f;
-	
-	nLines++;
-	
-/*Seek to next auxiliary data record.*/
-	while (ERS_readNextFrame(s,&f)->is_aux!=1) {}
-	
-	ERS_auxAGC_window(s,&f.aux);
-	
-/*Unpack the 20 leftover bytes (16 samples) in auxilary frame.*/
-	iqCurr=ERS_unpackBytes(&f.data[ERS_datPerAux],ERS_datPerFrame-ERS_datPerAux,iqCurr);
-	
-/*Unpack each 250 bytes (200 samples) in framesPerLine remaining echo frames.*/
-	for (i=1;i<ERS_framesPerLine;i++)
-	{
-		ERS_readNextFrame(s,&f);
-		if (f.is_echo==0)
-		  {
-		     if (!quietflag) printf("   Error! Expected echo frame; got '%d' frame! Assuming bit error\n",f.type);
-		     /* exit(1); */
-		  }
-		iqCurr=ERS_unpackBytes(f.data,ERS_datPerFrame,iqCurr);
-	}
-	
-	/*Occasionally print out the auxiliary data record
-	if (nLines%1000==0)
-	{
-		ERS_auxPrint(&f.aux,stdout);
-	}
-*/
+  int ii;
+  iqType *iqCurr=iqBuf;
+  ERS_frame f;
+
+  nLines++;
+
+  /*Seek to next auxiliary data record.*/
+  while (ERS_readNextFrame(s,&f)->is_aux!=1) {}
+
+  ERS_auxAGC_window(s,&f.aux);
+
+  /*Unpack the 20 leftover bytes (16 samples) in auxilary frame.*/
+  iqCurr = ERS_unpackBytes(&f.data[ERS_datPerAux],
+                           ERS_datPerFrame-ERS_datPerAux,
+                           iqCurr);
+
+  /*Unpack each 250 bytes (200 samples) in framesPerLine remaining echo frames.*/
+  for (ii=1; ii<ERS_framesPerLine; ii++) {
+    ERS_readNextFrame(s,&f);
+    if (f.is_echo==0) {
+      if (!quietflag)
+        printf("   Error! Expected echo frame; got '%d' frame! Assuming bit error\n",
+               f.type);
+    }
+    iqCurr=ERS_unpackBytes(f.data,ERS_datPerFrame,iqCurr);
+  }
+
+  /* Occasionally print out the auxiliary data record */
+/*  if (nLines%1000==0) {
+ *    ERS_auxPrint(&f.aux,stdout);
+ *  }
+ */
 }
 
 
@@ -59,9 +59,9 @@ void ERS_readNextPulse(bin_state *s,iqType *iqBuf, char *inName, char *outName)
  * Raw satellite initialization routine.  */
 void ERS_init(bin_state *s)
 {
-	strcpy(s->satName,"ERS-?");/*Filled out in ERS_auxUpdate*/
-	CONF_ERS_fields(s);
-	s->bytesPerFrame=ERS_bytesPerFrame;
+  strcpy(s->satName,"ERS-?");/*Filled out in ERS_auxUpdate*/
+  CONF_ERS_fields(s);
+  s->bytesPerFrame=ERS_bytesPerFrame;
 }
 
 /*********************************
@@ -69,19 +69,19 @@ void ERS_init(bin_state *s)
  * Decoder initialization routine.  */
 bin_state *ERS_decoder_init(char *inN,char *outN,readPulseFunc *reader)
 {
-	bin_state *s=new_bin_state();
-	ERS_frame f;
-	printf("   Initializing ERS decoder...\n");
-	*reader=ERS_readNextPulse;
-	
-	ERS_init(s);
-	
-	openBinary(s,inN);
-	while (ERS_readNextFrame(s,&f)->is_aux!=1) {}
-	ERS_auxUpdate(&f.aux,s);
-	seekFrame(s,0);
-	
-	return s;
+  bin_state *s=new_bin_state();
+  ERS_frame f;
+  printf("   Initializing ERS decoder...\n");
+  *reader=ERS_readNextPulse;
+
+  ERS_init(s);
+
+  openBinary(s,inN);
+  while (ERS_readNextFrame(s,&f)->is_aux!=1) {}
+  ERS_auxUpdate(&f.aux,s);
+  seekFrame(s,0);
+
+  return s;
 }
 
 
@@ -92,14 +92,14 @@ bin_state *ERS_decoder_init(char *inN,char *outN,readPulseFunc *reader)
 void ERS_readNextCeosPulse(bin_state *s,iqType *iqBuf, char *inName,
                            char *outName)
 {
-	int i;
-	signalType *sig=NULL;
-	ERS_aux aux;
-	sig=getNextCeosLine(s->binary, s, inName, outName);
-	for (i=0;i<2*s->nSamp;i++)
-		iqBuf[i]=sig[220+i];
-	ERS_decodeAux((ERS_raw_aux *)sig,&aux);
-	ERS_auxAGC_window(s,&aux);
+  int i;
+  signalType *sig=NULL;
+  ERS_aux aux;
+  sig=getNextCeosLine(s->binary, s, inName, outName);
+  for (i=0;i<2*s->nSamp;i++)
+    iqBuf[i]=sig[220+i];
+  ERS_decodeAux(sig,&aux);
+  ERS_auxAGC_window(s,&aux);
 }
 
 /***********************
@@ -108,23 +108,23 @@ void ERS_readNextCeosPulse(bin_state *s,iqType *iqBuf, char *inName,
 bin_state *ERS_ceos_decoder_init(char *inName, char *outName,
                                  readPulseFunc *reader)
 {
-	bin_state *s=new_bin_state();
-	signalType *sig=NULL;
-	ERS_aux aux;
-	printf("   Initializing ERS decoder...\n");
-	*reader=ERS_readNextCeosPulse;
-	
-	ERS_init(s);
-	
-	s->binary=openCeos(inName, outName, s);
-	sig=getNextCeosLine(s->binary, s, inName, outName);
-	ERS_decodeAux((ERS_raw_aux *)sig,&aux);
-        ERS_auxUpdate(&aux,s);
+  bin_state *s=new_bin_state();
+  signalType *sig=NULL;
+  ERS_aux aux;
+  printf("   Initializing ERS decoder...\n");
+  *reader=ERS_readNextCeosPulse;
 
-	ERS_auxAGC_window(s,&aux);
-	FSEEK64(s->binary,0,0);
-	
-	return s;
+  ERS_init(s);
+
+  s->binary=openCeos(inName, outName, s);
+  sig=getNextCeosLine(s->binary, s, inName, outName);
+  ERS_decodeAux(sig,&aux);
+  ERS_auxUpdate(&aux,s);
+
+  ERS_auxAGC_window(s,&aux);
+  FSEEK64(s->binary,0,0);
+
+  return s;
 }
 
 #endif
