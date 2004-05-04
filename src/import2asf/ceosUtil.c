188b57f8b24960729542b2535ae5da3a7fd82548
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-05-04 22:16:50 +0000

    Better printing to stdout & logfile. Whitespace changes.

--- src/import2asf/ceosUtil.c
+++ src/import2asf/ceosUtil.c
@@ -13,10 +13,10 @@ void createMeta_ceos(bin_state *s, struct dataset_sum_rec *dssr, char *inName,
  * file pointer  */
 FILE *openCeos(char *fName, char *outN, bin_state *s)
 {
-	FILE *ret=FOPEN(fName,"rb");
-	FSEEK64(ret,0,0);/*Seek to beginning of file*/
-	getNextCeosLine(ret, s, fName, outN);/*Skip over first line of file*/
-	return ret;
+  FILE *ret=FOPEN(fName,"rb");
+  FSEEK64(ret,0,0);/*Seek to beginning of file*/
+  getNextCeosLine(ret, s, fName, outN);/*Skip over first line of file*/
+  return ret;
 }
 
 /*********************************
@@ -25,32 +25,34 @@ FILE *openCeos(char *fName, char *outN, bin_state *s)
  * static buffer. */
 signalType *getNextCeosLine(FILE *f, bin_state *s, char *inN, char *outN)
 {
-	static int headerLen=12,totHeaderLen=192;
-	static signalType buffer[100000];/*Input Buffer*/
-	struct HEADER head;
-	int length;
-        struct dataset_sum_rec dssr;
+  static int headerLen=12,totHeaderLen=192;
+  static signalType buffer[100000];/*Input Buffer*/
+  struct HEADER head;
+  int length;
+  struct dataset_sum_rec dssr;
+  char message[256];
 
-	if (headerLen!=fread(&head,1,headerLen,f))
-	{
-		/* create metadata file */
-        	get_dssr(inN,&dssr);
-       		createMeta_ceos(s,&dssr,inN,outN);
-		/* write out AISP input parameter file */
-		if (fabs(dssr.crt_dopcen[0])<15000)
-        	  writeAISPparams(s,outN,dssr.crt_dopcen[0],dssr.crt_dopcen[1],dssr.crt_dopcen[2]);
-		else
-        	  writeAISPparams(s,outN,0,0,0);
-        	writeAISPformat(s,outN);
-		
-	        printf("   Wrote %i lines of raw signal data.\n\n",s->nLines);
-	        if (logflag) {
-	          sprintf(logbuf,"   Wrote %i lines of raw signal data.\n\n",s->nLines);
-	          printLog(logbuf);
-	        }
-		exit(EXIT_SUCCESS);
-	}
-	length=bigInt32(head.recsiz);
-	FREAD(&buffer,1,length-12,f);
-	return &buffer[totHeaderLen-headerLen];
+  if (headerLen!=fread(&head,1,headerLen,f)) {
+    /* create metadata file */
+    get_dssr(inN,&dssr);
+    createMeta_ceos(s,&dssr,inN,outN);
+
+    /* write out AISP input parameter file */
+    if (fabs(dssr.crt_dopcen[0])<15000)
+      writeAISPparams(s,outN,dssr.crt_dopcen[0],dssr.crt_dopcen[1],dssr.crt_dopcen[2]);
+    else
+      writeAISPparams(s,outN,0,0,0);
+
+    writeAISPformat(s,outN);
+
+    sprintf(message,"\n   Wrote %i lines of raw signal data.\n\n",s->nLines);
+    printf(message);
+    if (logflag) { printLog(message); }
+
+    exit(EXIT_SUCCESS);
+  }
+
+  length=bigInt32(head.recsiz);
+  FREAD(&buffer,1,length-12,f);
+  return &buffer[totHeaderLen-headerLen];
 }
