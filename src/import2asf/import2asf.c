ae8dcb35b6c2603bb531533f18dd97de7e2b1a07
rgens <rgens@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-05-07 20:25:38 +0000

    added ENVI import functionality.

--- src/import2asf/import2asf.c
+++ src/import2asf/import2asf.c
@@ -83,6 +83,7 @@ BUGS:
 #include "ceos_io.h"
 #include "dateUtil.h"
 #include "decoder.h"
+#include "envi.h"
 #include "esri.h"
 #include "lzFetch.h"
 #include <ctype.h>
@@ -143,11 +144,15 @@ int main(int argc, char *argv[])
   readPulseFunc readNextPulse;
   iqType *iqBuf;
   esri_header *esri=NULL;
+  envi_header *envi=NULL;
   FILE *fpIn=NULL, *fpOut=NULL, *fp;;
   char in_type[25]="", out_type[25]="";
   char inDataName[256], inMetaName[256], outName[288], prcPath[255];
   char outBaseName[256], type[255]="", tmp[255], imgTimeStr[20]="";
-  char line[255]="", key[25]="", value[25]="";
+  char line[255]="", key[25]="", value[25]="", bla[25];
+  char *map_info_ptr=NULL, *proj_info_ptr, proj_info[255]="", map_info[255]="";
+  double fTmp1, fTmp2;
+  int projection_key;
   int nl, ns=0, ii, kk, tableRes=MAX_tableRes, tablePix=0, headerBytes;
   int latConstraintFlag=FALSE, prcflag=FALSE, nTotal, nVec=1;
   long offset;
@@ -830,13 +835,161 @@ int main(int argc, char *argv[])
       FCLOSE(fLog);
     }
   }
+
   /* Ingest ENVI format data */
   else if (strncmp(type, "-ENVI", 5)==0) {
 
-	sprintf(tmp,"   Data format: ENVI\n");
-	printf(tmp);
-	if (logflag) printLog(tmp);
+    sprintf(tmp,"   Data format: ENVI\n");
+    printf(tmp);
+    if (logflag) printLog(tmp);
 
+    /* Handle output file name */
+    create_name(outName, outBaseName, "_amp.img");
+ 
+    /* Allocate memory for ESRI header structure */
+    envi = (envi_header *)MALLOC(sizeof(envi_header));
+    
+    /* Read .hdr and fill meta structures */ 
+    fp = FOPEN(inMetaName, "r");
+    while (NULL != fgets(line, 255, fp)) {
+      sscanf(line, "%s = %s", key, value);
+      if (strncmp(key, "samples", 6)==0) envi->samples = atoi(value);
+      else if (strncmp(key, "lines", 5)==0) envi->lines = atoi(value);
+      else if (strncmp(key, "bands", 5)==0) envi->bands = atoi(value);
+      else if (strncmp(key, "header", 6)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "offset", 6)==0)
+	  envi->header_offset = atoi(value);
+      }
+      /*** ignore file type for the moment ***/
+      else if (strncmp(key, "data", 4)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "type", 4)==0)
+	  envi->data_type = atoi(value);
+      }
+      else if (strncmp(key, "interleave", 10)==0) 
+	sprintf(envi->interleave, "%s", value);
+      else if (strncmp(key, "sensor", 6)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "type", 4)==0)
+	  sprintf(envi->sensor_type, "%s", value);
+      }
+      else if (strncmp(key, "byte", 4)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "order", 5)==0)
+	  envi->byte_order = atoi(value);
+      }
+      else if (strncmp(key, "map", 3)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "info", 4)==0) {
+	  map_info_ptr = strstr(line, ",");
+	  sprintf(map_info, "%s", map_info_ptr);
+	}
+      }
+      else if (strncmp(key, "projection", 10)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "info", 4)==0) {
+	  proj_info_ptr = strstr(line, ",");
+	  sprintf(proj_info, "%s", proj_info_ptr);      
+	  sscanf(value, "{%i,", &projection_key);
+	}
+      }
+      else if (strncmp(key, "wavelength", 10)==0) {
+	sscanf(line, "%s %s = %s", bla, key, value);
+	if (strncmp(key, "units", 5)==0)
+	  sprintf(envi->wavelength_units, "%s", value);
+      }
+      /*** ignore wavelength for the moment ***/
+      /*** ignore data ignore for the moment ***/
+      /*** ignore default stretch for the moment ***/
+    }
+    FCLOSE(fp);
+    
+    switch(projection_key)
+      {
+      case 3: 
+	sprintf(envi->projection, "UTM");
+	sscanf(map_info, ", %i %i %lf %lf %lf %lf %i %s",
+	       &envi->ref_pixel_x, &envi->ref_pixel_y, 
+	       &envi->pixel_easting, &envi->pixel_northing, 
+	       &envi->proj_dist_x, &envi->proj_dist_y,
+	       &envi->projection_zone, envi->hemisphere);
+	sscanf(proj_info, ", %lf, %lf, %lf, %lf, %s}",
+	       &envi->semimajor_axis, &envi->semiminor_axis, &envi->center_lat,
+	       &envi->center_lon, bla);
+	break;
+      case 4: 
+	sprintf(envi->projection, "Lambert Conformal Conic");
+	sscanf(map_info, ", %i, %i, %lf, %lf, %lf, %lf, %s}",
+	       &envi->ref_pixel_x, &envi->ref_pixel_y, 
+	       &envi->pixel_easting, &envi->pixel_northing, 
+	       &envi->proj_dist_x, &envi->proj_dist_y,
+	       envi->hemisphere);
+	sscanf(proj_info, ", %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %s}",
+	       &envi->semimajor_axis, &envi->semiminor_axis, &envi->center_lat,
+	       &envi->center_lon, &fTmp1, &fTmp2, &envi->standard_parallel1,
+	       &envi->standard_parallel2, bla);
+	break;
+      case 9: 
+	sprintf(envi->projection, "Albers Conical Equal Area");
+	sscanf(map_info, ", %i, %i, %lf, %lf, %lf, %lf, %s}",
+	       &envi->ref_pixel_x, &envi->ref_pixel_y, 
+	       &envi->pixel_easting, &envi->pixel_northing, 
+	       &envi->proj_dist_x, &envi->proj_dist_y,
+	       envi->hemisphere);
+	sscanf(proj_info, ", %lf, %lf, %lf, %lf, %lf %lf %lf %lf%s}",
+	       &envi->semimajor_axis, &envi->semiminor_axis, &envi->center_lat,
+	       &envi->center_lon, &fTmp1, &fTmp2, &envi->standard_parallel1,
+	       &envi->standard_parallel2, bla);
+	break;
+      case 11: 
+	sprintf(envi->projection, "Lambert Azimuthal Equal Area");
+	sscanf(map_info, ", %i, %i, %lf, %lf, %lf, %lf, %s}",
+	       &envi->ref_pixel_x, &envi->ref_pixel_y, 
+	       &envi->pixel_easting, &envi->pixel_northing, 
+	       &envi->proj_dist_x, &envi->proj_dist_y,
+	       envi->hemisphere);
+	sscanf(proj_info, ", %lf, %lf, %lf, %lf, %s}",
+	       &envi->semimajor_axis, &envi->semiminor_axis, &envi->center_lat,
+	       &envi->center_lon, bla);
+	break;
+      case 31: 
+	sprintf(envi->projection, "Polar Stereographic");
+	sscanf(map_info, ", %d, %d, %lf, %lf, %lf, %lf, %s}",
+	       &envi->ref_pixel_x, &envi->ref_pixel_y, 
+	       &envi->pixel_easting, &envi->pixel_northing, 
+	       &envi->proj_dist_x, &envi->proj_dist_y,
+	       envi->hemisphere);
+	sscanf(proj_info, ", %lf, %lf, %lf, %lf, %s}",
+	       &envi->semimajor_axis, &envi->semiminor_axis, &envi->center_lat,
+	       &envi->center_lon, bla);
+	break;
+      default:
+	sprintf(errbuf, "\n   ERROR: unsupported map projection\n\n");
+	printErr(errbuf);
+	break;
+      }
+    
+    /* Fill metadata structure with valid data */
+    meta = envi2meta(envi);
+    
+    /* Write metadata file */
+    meta_write(meta, outName);
+    
+    /* Write data file - currently no header, so just copying generic binary */
+    sprintf(tmp, "cp %s %s", inDataName, outName);
+    system(tmp);
+
+    /* Clean and report */
+    meta_free(meta);
+    sprintf(logbuf, "   Converted ENVI file (%s) to ASF internal file (%s)\n\n",
+	    inDataName, outName);
+    printf(logbuf);
+    if (logflag) {
+      fLog = FOPEN(logFile, "a");
+      printLog(logbuf);
+      FCLOSE(fLog);
+    }  
   }
 
   exit(EXIT_SUCCESS);
