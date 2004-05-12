edef7bc0b73fe64df10e2743a8362c9fb61260ca
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-05-12 23:32:09 +0000

    SProCKET style metadata header... needed for meta_write_sprocket in asf_meta.a

---
+++ include/metadata.h
@@ -0,0 +1,57 @@
+/* Metadata keys */
+#define CONVERTER_VERSION "converter_version"
+#define PROCESSING_INFO "system_version"
+#define PROCESSING_DATE "date_of_processing"
+#define PLATFORM "platform"
+#define BEAM_MODE "beam_mode"
+#define FREQUENCY "frequency"
+#define POLARIZATION "polarization"
+#define TRACK_ANGLE "track_angle"
+#define CLOCK_ANGLE "clock_angle"
+#define PROJECTION "projection"
+#define NUMBER_OF_PIXELS "number_of_pixels"
+#define NUMBER_OF_LINES  "number_of_lines"
+#define RNG_PIXEL_SPACING "pixel_size_range"
+#define AZ_PIXEL_SPACING "pixel_size_az"
+#define CENTER_GMT "center_gmt"
+#define SLANT_RANGE_TO_FIRST_PIXEL "slant_range_to_first_pixel"
+#define EARTH_RADIUS_AT_IMAGE_CENTER "earth_radius_at_center"
+#define EARTH_RADIUS_AT_IMAGE_NARIR "earth_radius_at_nadir"
+#define PLATFORM_ALITITUDE "platform_altitude"
+#define IMAGE_FORMAT "image_format"
+#define TOP_RIGHT_CORNER_LAT "far_early_lat"
+#define TOP_RIGHT_CORNER_LONG "far_early_long"
+#define TOP_LEFT_CORNER_LAT "near_early_lat"
+#define TOP_LEFT_CORNER_LONG "near_early_long"
+#define BOTTOM_RIGHT_CORNER_LAT "far_late_lat"
+#define BOTTOM_RIGHT_CORNER_LONG "far_late_long"
+#define BOTTOM_LEFT_CORNER_LAT "near_late_lat"
+#define BOTTOM_LEFT_CORNER_LONG "near_late_long"
+#define ELLIPS_MAJ_AXIS "ellipsoid_major_axis"
+#define ELLIPS_MIN_AXIS "ellipsoid_minor_axis"
+#define REVOLUTION "revolution"
+#define FLIGHT_DIRECTION "ascending/descending"
+#define PRF "PRF"
+#define DOPPLER_POLY_A0 "doppler_poly_a0"
+#define DOPPLER_POLY_A1 "doppler_poly_a1"
+#define DOPPLER_POLY_A2 "doppler_poly_a2"
+
+/* Vexcel "Unique" metadata keys */
+#define VEXCEL_BETA_OR_SIGMA    "vexcel_beta_or_sigma"
+#define VEXCEL_ANTENNA_PATTERN  "vexcel_antenna_pattern"
+
+
+/* File extentions */
+#define SIGMA_EXT       ".sigma0"
+#define DATA_EXT        ".img"
+#define LOOK_EXT        ".look"
+#define METADATA_EXT    ".metadata"
+#define COMPLEX_I_PLANE ".dataI"
+#define COMPLEX_Q_PLANE ".dataQ"
+
+
+/* Consistent values */
+#define COMPLEX_FORMAT       "complex"
+#define STANDARD_FORMAT      "detected"
+#define SIZE_OF_ASF_COMPLEX  (4)
+
