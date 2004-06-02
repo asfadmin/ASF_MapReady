c83201de70bfda3c61f489ce22689a8795cd06e6
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-06-02 19:54:21 +0000

    Added some comments.

--- src_lib/libgsl/find_gsl.sh
+++ src_lib/libgsl/find_gsl.sh
@@ -1,8 +1,17 @@
 #!/bin/sh 
 
+# This script looks in a hard coded list of directories for the GNU
+# Scientific library library and header files.  As soon as it finds
+# readable files lib/libgsl.a, lib/libgslcblas.a, and
+# include/gsl/gsl_math.h under a common root directory, it prints the
+# name of that root directory and exits.  If it doesn't find such
+# files under any of the hard coded root directories, it exits without
+# printing anything.
+
 # Exit immediately if a command exists with a non-zero status.
 set -e
 
+# Directories to look in.
 dirs="/usr/local /usr $HOME/local"
 
 for dir in $dirs; do
