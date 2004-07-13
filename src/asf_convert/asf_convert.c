37862cfc5bfe93e8e23869a658b2931dc9bc1271
gshort <gshort@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-07-13 22:38:58 +0000

    Removed all C++ style comments and replaced them with C-style comments (No more //, only /*...*/)

--- src/asf_convert/asf_convert.c
+++ src/asf_convert/asf_convert.c
@@ -122,8 +122,8 @@ void usage()
 	exit(EXIT_FAILURE);
 }
 
-//Default splash screen, the same for all the tools
-//This function should be called first in the "we're good enough" part of command line parsing
+/*Default splash screen, the same for all the tools
+This function should be called first in the "we're good enough" part of command line parsing*/
 void print_splash_screen(int argc, char* argv[])
 {
 	char temp1[255];
@@ -152,7 +152,7 @@ void config_usage()
 
 void print_error(char *msg)
 {
-	sprintf(errbuf, "\n   \033[31;1mERROR:\033[0m %s\n\n", msg);//I made "ERROR:" red...Yay! :D
+	sprintf(errbuf, "\n   \033[31;1mERROR:\033[0m %s\n\n", msg);/*I made "ERROR:" red...Yay! :D*/
 	printErr(errbuf);
 	exit(EXIT_FAILURE);
 }
@@ -164,9 +164,9 @@ void check_return(int ret, char *msg)
 		print_error(msg);
 }
 
-//Check to see if an option was supplied or not
-//If it was found, return its argument number
-//Otherwise, return FLAG_NOT_SET
+/*Check to see if an option was supplied or not
+If it was found, return its argument number
+Otherwise, return FLAG_NOT_SET*/
 int checkForOption(char* key, int argc, char* argv[])
 {
 	int ii = 0;
@@ -188,20 +188,20 @@ int main(int argc, char *argv[])
 	char proj[25], line[255], fileName[255], batchConfig[255];
 	char format_in[255], format_out[255];
 	char out_file[255], data_file[255], meta_file[255];
-//	int inFlag = FALSE, outFlag = FALSE, formatFlag = FALSE, configFlag = FALSE, configInitFlag = FALSE;
+/*	int inFlag = FALSE, outFlag = FALSE, formatFlag = FALSE, configFlag = FALSE, configInitFlag = FALSE;*/
 	int formatFlag, sizeFlag, configFlag, configInitFlag, quietFlag;
 	int ii, size_out;
 	file_format_t type_in, type_out;
 
-//*********************BEGIN COMMAND LINE PARSING STUFF*********************//
-	//Super-secret hidden options :)
+/**********************BEGIN COMMAND LINE PARSING STUFF**********************/
+	/*Super-secret hidden options :)*/
 	if(checkForOption("-batch", argc, argv) != FLAG_NOT_SET)
 	{
 		config_usage();
 		exit(0);
 	}
 
-	//Normal options
+	/*Normal options*/
 	formatFlag = checkForOption("-format", argc, argv);
 	sizeFlag = checkForOption("-size", argc, argv);
 	configFlag = checkForOption("-config", argc, argv);
@@ -210,118 +210,118 @@ int main(int argc, char *argv[])
 	quietFlag = checkForOption("-quiet", argc, argv);
 
 
-	if(configFlag != FLAG_NOT_SET && configInitFlag != FLAG_NOT_SET)//One or the other is fine, but not both
-		usage();//This exits with a failure
-	if((configFlag != FLAG_NOT_SET || configInitFlag != FLAG_NOT_SET) && formatFlag)//format is mutually exclusive with these
-		usage();//This exits with a failure
+	if(configFlag != FLAG_NOT_SET && configInitFlag != FLAG_NOT_SET)/*One or the other is fine, but not both*/
+		usage();/*This exits with a failure*/
+	if((configFlag != FLAG_NOT_SET || configInitFlag != FLAG_NOT_SET) && formatFlag)/*format is mutually exclusive with these*/
+		usage();/*This exits with a failure*/
 
-	//So, at this point, we know our options don't conflict...now we need
-	//to know how many arguments we ought to have.
-	int neededArgs = 1;//command
+	/*So, at this point, we know our options don't conflict...now we need
+	to know how many arguments we ought to have.*/
+	int neededArgs = 1;/*command*/
 	if(configFlag != FLAG_NOT_SET || configInitFlag != FLAG_NOT_SET)
-		neededArgs += 2;//option & parameter
+		neededArgs += 2;/*option & parameter*/
 	else
-		neededArgs += 3;//in_data_file, in_meta_file, out_file
+		neededArgs += 3;/*in_data_file, in_meta_file, out_file*/
 	if(logflag != FLAG_NOT_SET)
-		neededArgs += 2;//option & parameter
+		neededArgs += 2;/*option & parameter*/
 	if(formatFlag != FLAG_NOT_SET)
-		neededArgs += 2;//option & parameter
+		neededArgs += 2;/*option & parameter*/
 	if(sizeFlag != FLAG_NOT_SET)
-		neededArgs += 2;//option & parameter
+		neededArgs += 2;/*option & parameter*/
 	if(quietFlag != FLAG_NOT_SET)
-		neededArgs += 1;//option
+		neededArgs += 1;/*option*/
 
-	//make sure we've got enough arguments
+	/*make sure we've got enough arguments*/
 	if(argc != neededArgs)
-		usage();//This exits with a failure
+		usage();/*This exits with a failure*/
 
-	//We also need to make sure the last three options are close to what we expect
+	/*We also need to make sure the last three options are close to what we expect*/
 	if(argv[argc - 1][0] == '-' || argv[argc - 2][0] == '-' || argv[argc - 3][0] == '-')
-		usage();//This exits with a failure
+		usage();/*This exits with a failure*/
 
-	//Next we're going to make sure the options are specified according to the usage
-	//That is, -option <parameter> -option <parameter> and so on...if an option requires
-	//a parameter, we need to make sure it's followed by a parameter! Also make sure
-	//an option's parameters don't bleed into the command's required arguments
+	/*Next we're going to make sure the options are specified according to the usage
+	That is, -option <parameter> -option <parameter> and so on...if an option requires
+	a parameter, we need to make sure it's followed by a parameter! Also make sure
+	an option's parameters don't bleed into the command's required arguments*/
 	if(logflag != FLAG_NOT_SET)
 		if(argv[logflag + 1][0] == '-' || logflag >= argc - 4)
-			usage();//This exits with a failure
+			usage();/*This exits with a failure*/
 	if(formatFlag != FLAG_NOT_SET)
 		if(argv[formatFlag + 1][0] == '-' || formatFlag >= argc - 4)
-			usage();//This exits with a failure
+			usage();/*This exits with a failure*/
 	if(sizeFlag != FLAG_NOT_SET)
 		if(argv[sizeFlag + 1][0] == '-' || sizeFlag >= argc - 4)
-			usage();//This exits with a failure
+			usage();/*This exits with a failure*/
 
-	//We must be good enough at this point...start processing with assumptions that are
-	//*supposedly* guaranteed from above :)
+	/*We must be good enough at this point...start processing with assumptions that are
+	*supposedly* guaranteed from above :)*/
 	if(quietFlag == FLAG_NOT_SET)
-		print_splash_screen(argc, argv);//display splash screen if not quiet
+		print_splash_screen(argc, argv);/*display splash screen if not quiet*/
 	
 	
 	if(formatFlag != FLAG_NOT_SET)
 		strcpy(format_out, argv[formatFlag + 1]);
 	else
-		strcpy(format_out, "geotiff");//default behavior is geotiff
+		strcpy(format_out, "geotiff");/*default behavior is geotiff*/
 	if(sizeFlag != FLAG_NOT_SET)
 		size_out = atol(argv[sizeFlag + 1]);
 	if(logflag != FLAG_NOT_SET)
 		strcpy(logFile, argv[logflag + 1]);
 	else
-		sprintf(logFile, "tmp%i.log", (int)getpid());//default behavior: log to tmp<pid>.log
+		sprintf(logFile, "tmp%i.log", (int)getpid());/*default behavior: log to tmp<pid>.log*/
 		
 	if(configFlag != FLAG_NOT_SET)
 		strcpy(configFile, argv[configFlag + 1]);
 	else
-		sprintf(configFile, "tmp%i.config", (int)getpid());//default behavior: config to tmp<pid>.config
+		sprintf(configFile, "tmp%i.config", (int)getpid());/*default behavior: config to tmp<pid>.config*/
 		
 	if(configInitFlag != FLAG_NOT_SET)
 		strcpy(configFile, argv[configInitFlag + 1]);
 
-	//Get files from command line if we're not doing a config file
+	/*Get files from command line if we're not doing a config file*/
 	if(configFlag == FLAG_NOT_SET && configInitFlag == FLAG_NOT_SET)
 	{
-		strcpy(data_file, argv[argc - 3]);//The first of the last three arguments
-		strcpy(meta_file, argv[argc - 2]);//The second of the last three arguments
-		strcpy(out_file, argv[argc - 1]);//The third of the last three arguments
-		if(strchr(data_file, '.') != NULL)//Make sure the file has an extension
+		strcpy(data_file, argv[argc - 3]);/*The first of the last three arguments*/
+		strcpy(meta_file, argv[argc - 2]);/*The second of the last three arguments*/
+		strcpy(out_file, argv[argc - 1]);/*The third of the last three arguments*/
+		if(strchr(data_file, '.') != NULL)/*Make sure the file has an extension*/
 		{
-			if(!strcmp(".D", strrchr(data_file, '.')))//If the file ends in .D
-				strcpy(format_in, "CEOS");//It must be a CEOS image
-			else if(!strcmp(".img", strrchr(data_file, '.')))//If the file ends in .img
-				strcpy(format_in, "ASF");//It must be an ASF internal format file
+			if(!strcmp(".D", strrchr(data_file, '.')))/*If the file ends in .D*/
+				strcpy(format_in, "CEOS");/*It must be a CEOS image*/
+			else if(!strcmp(".img", strrchr(data_file, '.')))/*If the file ends in .img*/
+				strcpy(format_in, "ASF");/*It must be an ASF internal format file*/
 			else
-				print_error("Unrecognized input file format");//Otherwise, we have no idea
+				print_error("Unrecognized input file format");/*Otherwise, we have no idea*/
 		}
 		else
-			print_error("Unrecognized input file format");//If no extension, we're not sure what to do
+			print_error("Unrecognized input file format");/*If no extension, we're not sure what to do*/
 	}
-//**********************END COMMAND LINE PARSING STUFF**********************//
+/***********************END COMMAND LINE PARSING STUFF***********************/
 
 	/* If we're working from a config file, read configuration file */
-	if (configFlag != FLAG_NOT_SET)//configFlag has been set
+	if (configFlag != FLAG_NOT_SET)/*configFlag has been set*/
 	{
-		if (!fileExists(configFile))//Does the specific config file already exist? If not...
+		if (!fileExists(configFile))/*Does the specific config file already exist? If not...*/
 		{
 			check_return(init_config(configFile),
 			"basic configuration file could not be initialized");
 			exit(0);
     	}
-		else if (check_resample_flag(configFile) && configInitFlag != FLAG_NOT_SET)//config init has been set
+		else if (check_resample_flag(configFile) && configInitFlag != FLAG_NOT_SET)/*config init has been set*/
 		{
 			check_return(init_resample_config(configFile),
 				"extended resampling configuration file "
 				"could not be initialized");
 			exit(0);
 		}
-		else if (check_geocode_flag(configFile) && configInitFlag != FLAG_NOT_SET)//config init has been set
+		else if (check_geocode_flag(configFile) && configInitFlag != FLAG_NOT_SET)/*config init has been set*/
 		{
 			check_return(init_projection_config(configFile),
 				"extended geocoding configuration file "
 				"could not be initialized");
 			exit(0);
 		}
-		//If everything else succeeded, do this...
+		/*if everything else succeeded, do this...*/
 		cfg = read_config(configFile);
 		strcpy(format_in, uc(cfg->general->in_format));
 		strcpy(format_out, uc(cfg->general->out_format));
@@ -352,11 +352,11 @@ int main(int argc, char *argv[])
 	}
 
 	/* Handle data types */
-	//Input types:
+	/*Input types:*/
 	if (strncmp(uc(format_in),"CEOS",4) == 0) type_in = CEOS;
 	else if (strncmp(uc(format_in),"ASF",3) == 0) type_in = ASF;
 	else print_error("Unrecognized input format");
-	//Output types:
+	/*Output types:*/
 	if (strncmp(uc(format_out),"ASF",3) == 0) type_out = ASF;
 	else if (strncmp(uc(format_out),"CEOS",4) == 0) type_out = CEOS;
 	else if (strncmp(uc(format_out),"GEOTIFF",3) == 0) type_out = GEOTIFF;
@@ -371,8 +371,8 @@ int main(int argc, char *argv[])
 	/* Batch mode processing */
 	if (cfg->general->batch != 0)
 	{
-//		cfg->general->batch = 0;
-//		logflag = 0;
+/*		cfg->general->batch = 0;
+		logflag = 0;*/
 		fBatch = FOPEN(cfg->general->batchFile, "r");
 		while (fgets(line, 255, fBatch) != NULL)
 		{
@@ -413,7 +413,6 @@ int main(int argc, char *argv[])
 		(int)getpid());
 		strcat(command, temp);
 		check_return(system(command), "Importing data (asf_import)");
-//		exit(0);
 		
 		
 		/* Ingest CEOS image */
