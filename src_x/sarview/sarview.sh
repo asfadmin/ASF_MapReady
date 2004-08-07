6945e67f7ee89864da38f0aa7a8954241dedd86e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-08-07 01:08:19 +0000

    Add a bunch of stuff to set up the TCL_LIBRARY environment variable within the script so tcl knows where to look for it's initialization script.

--- src_x/sarview/sarview.sh
+++ src_x/sarview/sarview.sh
@@ -6,8 +6,40 @@
 # Bourne shell programs' $0 contain the absolute path to the 
 # script.  Sarview_bin needs to be called with the absolute path
 # so it can find its "support" directory.
-# Otherwise, this script does nothing
-# Orion Sky Lawlor, 5/21/1999, ASF-STEP
+
+
+osType=`uname`
+case "${osType}" in
+	IRIX*)
+		sys="irix" ;;
+	SunOS)
+		sys="solaris" ;;
+	Linux)
+		sys="linux" ;;
+	CYGWIN*)
+		sys="win32" ;;
+	*)
+		echo "Dur... SARview can't figure out where Tcl/Tk libraries are."
+		echo "Exiting..."
+		exit 1 ;;
+esac
+
+# Dynamically get the path to the tcl init script 
+# Take the absolute path to this scipt ($0), and work backwards to the tools
+# base directory, then append the /lib/system directory.
+# Example
+#    $0 = /usr/local/asf_tools/bin/linux/script_name
+#    dirname returns /usr/local/asf_tools/bin/linux
+#    dirname returns /usr/local/asf_tools/bin
+#    dirname returns /usr/local/asf_tools
+#    then append /java
+#    and BLAMO, our classpath is /usr/local/asf_tools/java
+tempDir=`dirname $0`
+tempDir=`dirname $tempDir`
+tempDir=`dirname $tempDir`
+TCL_LIBRARY=${tempDir}/lib/${sys}/tcl8.1
+export TCL_LIBRARY
+echo $TCL_LIBRARY
 
 echo "Running SARview..."
 $0_bin $1 &
