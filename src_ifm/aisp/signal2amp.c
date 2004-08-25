dd6fa3b3dfcb9eb7e484fad0836d498a1465a9b7
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-08-25 00:45:12 +0000

    Quick endian fix so as to work on big or little endian machines.

--- src_ifm/aisp/signal2amp.c
+++ src_ifm/aisp/signal2amp.c
@@ -1,14 +1,15 @@
+
 #include "asf.h"
-
-
-
 #include "aisp_defs.h"
 #include "read_signal.h"
+#include "asf_endian.h"
+
+#define mlX 8
+#define mlY 16
+
 
 main(int argc,char **argv)
 {
-#define mlX 8
-#define mlY 16
 	int x,outX,y,outY,mlLines,mlSamples;
 	getRec *r;
 	complexFloat *inBuf;
@@ -23,21 +24,22 @@ main(int argc,char **argv)
 	inBuf=(complexFloat *)MALLOC(sizeof(complexFloat)*r->nSamples);
 	outBuf=(float *)MALLOC(sizeof(float)*(mlSamples+10));
 	outF=fopenImage(argv[2],"wb");
-	for (outY=0;outY<mlLines;outY++)
-	{
-		for (y=0;y<mlY;y++)
-		{
+	for (outY=0;outY<mlLines;outY++) {
+		for (y=0;y<mlY;y++) {
 			getSignalLine(r,outY*mlY+y,inBuf,0,r->nSamples);
-			for (outX=0;outX<mlSamples;outX++)
-			{
+			for (outX=0;outX<mlSamples;outX++) {
 				outBuf[outX]=0;
-				for (x=0;x<mlY;x++)
-					outBuf[outX]+=Cabs(inBuf[outX*mlX+x]);
+				for (x=0;x<mlY;x++) {
+					outBuf[outX]+=Cabs(inBuf[outX*mlX+x]);		       
+				}
 			}
 		}
 		if (!quietflag && (outY%1024 == 0)) printf("Writing line %d\n",outY);
-		fwrite(outBuf,sizeof(float),mlSamples,outF);
+
+		for (x=0; x<mlSamples; x++) {
+			ieee_big32(outBuf[x]);
+		}
+		FWRITE(outBuf,sizeof(float),mlSamples,outF);
 	}
 	return 0;
 }
-
