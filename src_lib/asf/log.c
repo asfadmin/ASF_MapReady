484e2a9d41faecc8049fd71fc5ab920db3e1b60e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-08-25 23:35:18 +0000

    Added new printAndLog() function which prints to terminal if !quietflag and to the log if logflag.

--- src_lib/asf/log.c
+++ src_lib/asf/log.c
@@ -4,13 +4,24 @@ Log file utilities
 
 #include "log.h"
 
-FILE *fLog;
-int logflag;		/* flag for log file output */
-char logFile[256];	/* log file name */
-int quietflag;		/* flag for short or long output */
-char logbuf[4096];	/* buffer for log file output */
+FILE *fLog;         /* log file stream pointer */
+char logFile[256];  /* log file name */
+char logbuf[4096];  /* buffer for log file output */
+int logflag;        /* flag for log file output */
+int quietflag;      /* flag for short or long output */
 
 void printLog(char *msg)
 {
   fprintf(fLog, "%s", msg);
+}
+
+void printAndLog(char *format, ...)
+{
+  va_list ap;
+  va_start(ap, format);
+  if (!quietflag)
+    vprintf(format, ap);
+  if (logflag)
+    vfprintf(fLog, format, ap);
+  va_end(ap);
 }
