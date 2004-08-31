c6f819e1b5bcaf2ff4c61281eebcc3440c8c7d98
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-08-31 00:31:43 +0000

    Changed name of uc() to str2upper().

--- src/asf_convert/functions.h
+++ src/asf_convert/functions.h
@@ -9,11 +9,11 @@
 int exit_code;
 
 /* Function definitions*/
-char *uc(char *string);
+char *str2upper(char *string);
 int asf_import(char *inFile, char *metaFile, char *type, char *outFile);
 int projprm(char *projection, char *projkey, char *outFile, char *parameters);
-int geocode(char *inFile, char *projFile, char *projkey, float pix_size, 
-	    float height, char *outFile);
+int geocode(char *inFile, char *projFile, char *projkey, float pix_size,
+            float height, char *outFile);
 int corner_coords(char *inFile);
 int filter(char *options, char *inFile, char *outFile);
 int asf_export(int format, char *inFile, char *outFile);
