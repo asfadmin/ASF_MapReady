3698222b25c0a22fbfab1f39cfb4c16128cb1ba5
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-08-31 00:31:13 +0000

    Tabs to spaces

--- src/asf_convert/asf_convert.h
+++ src/asf_convert/asf_convert.h
@@ -3,88 +3,88 @@
 
 typedef struct
 {
-	char *in_data_name;		/* input data file name */
-	char *in_meta_name;		/* input metadata file name */
-	char *in_format;		/* input format: CEOS, ASF */
-	char *data_type;		/* data type: amplitude, power, sigma, gamma,
-								beta */
-	char *out_name;			/* output file name */
-	char *out_format;		/* output format: ASF, CEOS, GEOTIFF, ENVI, ESRI, JPEG,
-								PPM, PNG, LAS */
-	int resample;			/* resampling flag */
-	int browse;				/* browse image flag */ 
-	char *logFile;			/* log file name */
-	int geocoding;			/* geocoding flag */
-	int batch;				/* batch mode flag */
-	char *batchFile;		/* batch file name */
-	char *units;			/* units as defined in the default values file */
+    char *in_data_name;     /* input data file name */
+    char *in_meta_name;     /* input metadata file name */
+    char *in_format;        /* input format: CEOS, ASF */
+    char *data_type;        /* data type: amplitude, power, sigma, gamma,
+                                beta */
+    char *out_name;         /* output file name */
+    char *out_format;       /* output format: ASF, CEOS, GEOTIFF, ENVI, ESRI, JPEG,
+                                PPM, PNG, LAS */
+    int resample;           /* resampling flag */
+    int browse;             /* browse image flag */
+    char *logFile;          /* log file name */
+    int geocoding;          /* geocoding flag */
+    int batch;              /* batch mode flag */
+    char *batchFile;        /* batch file name */
+    char *units;            /* units as defined in the default values file */
 } s_general;
 
 typedef struct
 {
-	char *projection;		/* projection: POLARSTEREO, UTM, ALBERS, 
-								LAMBERT1, LAMBERT2 */
-	int background;			/* background fill color */
-	double pixel;			/* pixel size for geocoding */
-	double height;			/* average height of the data */
+    char *projection;       /* projection: POLARSTEREO, UTM, ALBERS,
+                                LAMBERT1, LAMBERT2 */
+    int background;         /* background fill color */
+    double pixel;           /* pixel size for geocoding */
+    double height;          /* average height of the data */
 } s_geocoding;
 
 typedef struct
 {
-	int kernel;				/* kernel size for reducing the output image size */
+    int kernel;             /* kernel size for reducing the output image size */
 } s_resampling;
 
 typedef struct
 {
-	int datum;				/* datum */
-	double center_lon;		/* center longitude */
-	double center_lat;		/* standard parallel */
-	char *units;			/* units */
+    int datum;              /* datum */
+    double center_lon;      /* center longitude */
+    double center_lat;      /* standard parallel */
+    char *units;            /* units */
 } s_azimuthal;
 
 typedef struct
 {
-	int datum;				/* datum */
-	int zone;				/* UTM zone number */
-	char *units;			/* units */
+    int datum;              /* datum */
+    int zone;               /* UTM zone number */
+    char *units;            /* units */
 } s_utm;
 
 typedef struct
 {
-	int datum;					/* datum */
-	double first_parallel;		/* first standard parallel */
-	double second_parallel;		/* second standard parallel */
-	double center_meridian;		/* center meridian */
-	double orig_latitude;		/* latitude of projection origin */
-	char *units;				/* units */
+    int datum;                  /* datum */
+    double first_parallel;      /* first standard parallel */
+    double second_parallel;     /* second standard parallel */
+    double center_meridian;     /* center meridian */
+    double orig_latitude;       /* latitude of projection origin */
+    char *units;                /* units */
 } s_conic;
 
 typedef struct
 {
-	char comment[255];			/* first line for comments */
-	s_general *general;			/* general processing details */
-	s_geocoding *geocoding;		/* geocoding parameters */
-	s_resampling *resampling;	/* resampling parameters */
-	s_azimuthal *polar;			/* parameters for Polarstereo coordinates */
-	s_utm *utm;					/* parameters for UTM coordinates */
-	s_conic *albers;			/* parameters for Albers coordinates */
-	s_azimuthal *lambert_az;	/* parameters for Lambert Azimuthal Equal Area 
-									coordinates */
-	s_conic *lambert_cc;		/* parameters for Lambert Conformal Conic 
-									coordinates */
+    char comment[255];          /* first line for comments */
+    s_general *general;         /* general processing details */
+    s_geocoding *geocoding;     /* geocoding parameters */
+    s_resampling *resampling;   /* resampling parameters */
+    s_azimuthal *polar;         /* parameters for Polarstereo coordinates */
+    s_utm *utm;                 /* parameters for UTM coordinates */
+    s_conic *albers;            /* parameters for Albers coordinates */
+    s_azimuthal *lambert_az;    /* parameters for Lambert Azimuthal Equal Area
+                                    coordinates */
+    s_conic *lambert_cc;        /* parameters for Lambert Conformal Conic
+                                    coordinates */
 } s_config;
 
 typedef enum
 {
-	CEOS=1,
-	ASF,
-	GEOTIFF,
-	JPEG,
-	ENVI,
-	ESRI,
-	PPM,
-	PNG,
-	LAS
+    CEOS=1,
+    ASF,
+    GEOTIFF,
+    JPEG,
+    ENVI,
+    ESRI,
+    PPM,
+    PNG,
+    LAS
 } file_format_t;
 
 /* checking return values in the main program */
