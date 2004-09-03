16098945288919b9287354cc50343d7eabf8cc0a
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-09-03 22:37:35 +0000

    Initialize logflag and quietflag to false.

--- src_lib/asf/log.c
+++ src_lib/asf/log.c
@@ -4,11 +4,11 @@ Log file utilities
 
 #include "log.h"
 
-FILE *fLog;         /* log file stream pointer */
-char logFile[256];  /* log file name */
-char logbuf[4096];  /* buffer for log file output */
-int logflag;        /* flag for log file output */
-int quietflag;      /* flag for short or long output */
+FILE *fLog;          /* log file stream pointer */
+char logFile[256];   /* log file name */
+char logbuf[4096];   /* buffer for log file output */
+int logflag=FALSE;   /* flag for log file output */
+int quietflag=FALSE; /* flag for short or long output */
 
 void printLog(char *msg)
 {
