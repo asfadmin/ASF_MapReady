ef6584447e30ca15b8bd0b878c0b73300dd2c13e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-09-03 22:34:17 +0000

    Move bulk of diagnostics.h code to asf_reporting.h. Rest of the code goes away Check it out for some new fangled logging if the need arises.

--- include/asf_reporting.h
+++ include/asf_reporting.h
@@ -34,5 +34,33 @@ void asfPrintError(const char *format, ...);
 /* Print the commandline captured, date, and PID to screen & logfile */
 void asfSplashScreen(int argc, char **argv);
 
+/* Prototypes from diagnostics.c *********************************************/
+/* Given a condition, and an optional printf()-style message string (possibly
+   with additional variadic arguments), this macro will print an assert style
+   message and the optional message to the terminal & log file iff the
+   condition is false.  */
+#define asfRequire(COND,...) \
+        (require_function (__FILE__, __LINE__, COND, #COND, __VA_ARGS__))
+/* This routine is onlyused by the require macro, never use it elsewhere!  */
+void require_function (const char *file, int line, int condition,
+                       const char *condSrt, const char *message, ...)
+#ifdef __GNUC__
+ __attribute__ ((format (printf, 5, 6)))
+#endif
+; /* <--- semicolon for require_function */
+
+/* Given a printf()-style message string and additional variadic
+   arguments for the message string, in that order, this macro will
+   print message to terminal, and log message prefixed with source file
+   and line information to the log file. */
+#define asfDie(...) (die_function (__FILE__, __LINE__, __VA_ARGS__))
+/* This routine is used by the die macro.  It should never be used
+   anywhere else.  */
+void die_function (const char *file, int line, const char *message, ...)
+#ifdef __GNUC__
+ __attribute__ ((format (printf, 3, 4), noreturn))
+#endif
+; /* <--- semicolon for die_function */
+
 
 #endif
