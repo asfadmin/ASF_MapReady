f98a60b5a7b6b33c55f9b980b286bbd7619339db
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-09-03 22:18:24 +0000

    Remove print_splash_screen() function (use asfSplashScreen in libasf_reporting)

--- src/asf_import/utilities.c
+++ src/asf_import/utilities.c
@@ -17,25 +17,6 @@ int firstRecordLen(char *ceosName)
 }
 
 
-/* Default splash screen, the same for all the tools
-   This function should be called first in the "we're good enough" part of command line parsing */
-void print_splash_screen(int argc, char* argv[])
-{
-  int ii;
-
-  sprintf(logbuf, "\nCommand line:\n");
-  for (ii = 0; ii < argc; ii++) {
-    sprintf(logbuf, "%s %s",logbuf, argv[ii]);
-  }
-  asfPrintStatus("%s\n"
-                 "\n"
-                 "Date: %s\n"
-                 "PID:  %i\n"
-                 "\n",
-                 logbuf, date_time_stamp(), (int)getpid());
-}
-
-
 /* Check to see if an option was supplied or not
    If it was found, return its argument number
    Otherwise, return FLAG_NOT_SET */
