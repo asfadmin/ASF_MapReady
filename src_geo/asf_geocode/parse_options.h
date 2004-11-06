5ad7da68053e5e5d2a62f66b3db4617e38572fe5
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-11-06 00:35:06 +0000

    Moved here from src/geocode2

---
+++ src_geo/asf_geocode/parse_options.h
@@ -0,0 +1,12 @@
+#ifndef PARSE_OPTIONS_H
+#define PARSE_OPTIONS_H
+
+#include "asf_meta.h"
+
+void set_options_testing(int is_testing);
+project_parameters_t * parse_projection_options(int *argc, char **argv[],
+						projection_type_t * proj_type);
+void parse_other_options(int *argc, char **argv[],
+			 double *height, double *pixel_size);
+void parse_log_options(int *argc, char **argv[]);
+#endif
