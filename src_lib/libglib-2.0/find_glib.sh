6a3e25ab6882316c5d19388a2ddfc4362c33fb8a
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2004-12-07 18:37:21 +0000

    Moving libglib to libglib-2.0 to make find_dependencies happy.

---
+++ src_lib/libglib-2.0/find_glib.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+
+# This script looks in a hard coded list of directories for GLIB.
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Directories to look in.
+dirs="/usr/local /usr $HOME/local"
+# dirs="/usr/local"
+
+for dir in $dirs; do
+    if [ -r $dir/lib/libglib-2.0.a \
+         -a -r $dir/include/glib-2.0/glib.h ] ; then
+	echo "$dir"
+        exit 0
+    fi
+done
