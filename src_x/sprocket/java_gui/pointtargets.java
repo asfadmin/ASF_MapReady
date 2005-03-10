572a1bcc760ed023133d2f07de4f70155d278766
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-03-10 19:49:31 +0000

    Whitespace. Better naming. Use of Constants.blah instead of string.

--- src_x/sprocket/java_gui/pointtargets.java
+++ src_x/sprocket/java_gui/pointtargets.java
@@ -58,45 +58,40 @@ class pointtargets {
             Target T = (Target)v.elementAt(ii);
             T.print();
             pt no = new pt(T.cs_dev_id,
-                           (int)( T.projected_image_x),
+                           (int)(T.projected_image_x),
                            (int)(T.projected_image_y),
                            T);
             if( (T.actual_image_x != 0) || ( T.actual_image_y != 0) ) {
                actualize(T.cs_dev_id,
-                         (int)(T.actual_image_x+.5),
-                         (int)(T.actual_image_y+.5));
+                         (int)(T.actual_image_x + 0.5),
+                         (int)(T.actual_image_y + 0.5));
             }
             alltargets.add(no);
          }
       }
       catch (Exception e){
-         logger.log("error importing point targets.");
+         logger.log("Error importing point targets.");
       }
       resetvisible();
    }
    
    public void exportTargets() {
-      String outfile = fileName + ".ground_target_final";      
-         //filename extension for output will probably change.
-      String end = System.getProperty("line.separator");
+      String outfile = fileName + Constants.ground_target_file_located;      
       try {
          PrintStream ps = new PrintStream (new FileOutputStream(outfile));
-         for(int i=0; i < visibletargets.size(); i++) {
+         for (int i=0; i < visibletargets.size(); i++) {
             pt now = (pt) visibletargets.elementAt(i);
-            if(now.actualized) {   
+            if (now.actualized) {   
                Target T = (Target)trgs.elementAt(i);
                T.actual_image_x = now.actualx;
                T.actual_image_y = now.actualy;
                T.print(ps);
-                 }
+            }
          }
-
-         
-         
          ps.close();
       }
       catch(IOException e) {
-         logger.log("error exporting point targets.");
+         logger.log("Error exporting point targets.");
       }
    }
    
@@ -114,10 +109,10 @@ class pointtargets {
       }
    }
    
-   public void actualize(String devname, int actualx, int actualy) {
-      for(int i=0; i < visibletargets.size(); i++) {
+   public void actualize  (String devname, int actualx, int actualy) {
+      for (int i=0; i < visibletargets.size(); i++) {
          pt now = (pt) visibletargets.elementAt(i);
-         if(now.devname.equals(devname)) {
+         if (now.devname.equals(devname)) {
             now.actualx = actualx;
             now.actualy = actualy;
             now.actualized = true;
@@ -125,8 +120,9 @@ class pointtargets {
          }
       }
    }
-   
-   public Vector zoompoints(int realx, int realy, int zwidth, int zheight) { //give me the preadjusted x and y.
+
+   //give me the preadjusted x and y.
+   public Vector zoompoints(int realx, int realy, int zwidth, int zheight) { 
       Vector zoomvector = new Vector(30);   //30 is just whatever.
       
       for(int i=0; i < visibletargets.size(); i++) {
@@ -141,11 +137,13 @@ class pointtargets {
                zoomvector.addElement(visibletargets.elementAt(i));
          }
       }
-      
       return zoomvector;
    }
+
 }
 
+/* ***********************************************************************/
+// CLASS PT
 class pt {
    String devname;
    int actualx;
@@ -155,16 +153,16 @@ class pt {
    boolean actualized;
    Target t;
 
-   pt (String devname, int potentialx, int potentialy, Target T) {
-      this.devname = devname;
-      this.potentialx = potentialx;
-      this.potentialy = potentialy;
-      T = t;
+   pt (String _devname, int _potentialx, int _potentialy, Target _T) {
+      this.devname = _devname;
+      this.potentialx = _potentialx;
+      this.potentialy = _potentialy;
+      _T = t;
    }
    
-   public void actualize(int actualx, int actualy) {
-      this.actualx = actualx;
-      this.actualy = actualy;
+   public void actualize(int _actualx, int _actualy) {
+      this.actualx = _actualx;
+      this.actualy = _actualy;
       actualized = true;
    }
 }
