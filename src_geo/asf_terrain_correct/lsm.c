5360d891dabfc00fd62c4a5e8f04cac61db67e2c
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-05-20 22:08:14 +0000

    Handle edge pixels differently.

--- src_geo/asf_terrain_correct/lsm.c
+++ src_geo/asf_terrain_correct/lsm.c
@@ -53,42 +53,54 @@ lsm_generate_mask(DEMGeomInfo *dgi)
 
   FloatImage *mask = float_image_new (dgi->nrows, dgi->ncols);
 
-  double dx = 1; // FIXME! 2 * self->dx;
-  double dy = 1; // FIXME! 2 * self->dy;
-
   for (row = 0; row < dgi->nrows; ++row) {
-
     for (col = 0; col < dgi->ncols; ++col) {
-      double z = dem_geom_info_get_dem_height(dgi, row, col);
-      double d = dem_geom_info_get_nadir_distance(dgi, row, col);
+      lsm_mask_value_t pixel_value;
 
-      double zn = dem_geom_info_get_dem_height(dgi, row - 1, col);
-      double zs = dem_geom_info_get_dem_height(dgi, row + 1, col);
-      double zw = dem_geom_info_get_dem_height(dgi, row, col - 1);
-      double ze = dem_geom_info_get_dem_height(dgi, row, col + 1);
+      if (row == 0 || row == dgi->nrows - 1 || 
+	  col == 0 || col == dgi->ncols - 1) {
+	pixel_value = MASK_NORMAL_VALUE;
+      } 
+      else {
+	double z = dem_geom_info_get_dem_height(dgi, row, col);
+	double d = dem_geom_info_get_nadir_distance(dgi, row, col);
+	
+	double zn = dem_geom_info_get_dem_height(dgi, row - 1, col);
+	double zs = dem_geom_info_get_dem_height(dgi, row + 1, col);
+	double zw = dem_geom_info_get_dem_height(dgi, row, col - 1);
+	double ze = dem_geom_info_get_dem_height(dgi, row, col + 1);
+	
+	double tn = dem_geom_info_get_imaging_time(dgi, row - 1, col);
+	double ts = dem_geom_info_get_imaging_time(dgi, row + 1, col);
+	double tw = dem_geom_info_get_imaging_time(dgi, row, col - 1);
+	double te = dem_geom_info_get_imaging_time(dgi, row, col + 1);
+	
+	double dx = ( dem_geom_info_get_x(dgi, row + 1, col) -
+		      dem_geom_info_get_x(dgi, row - 1, col) );
+	
+	double dy = ( dem_geom_info_get_y(dgi, row, col + 1) -
+		      dem_geom_info_get_y(dgi, row, col - 1) );
+	
+	double dd = (ze - zw)/dx * (te - tw)/dx + (zn - zs)/dy * (tn - ts)/dy;
+	
+	double z_s = dem_geom_info_get_satellite_height(dgi, row, col);
+	
+	double R = d - (z_s - z) * dd;
+	double A = (z_s - z) + d * dd;
+      
+	if (A <= 0)
+	  pixel_value = MASK_ACTIVE_SHADOW_VALUE;
+	else if (R <= 0)
+	  pixel_value = MASK_ACTIVE_LAYOVER_VALUE;
+	else
+	  pixel_value = MASK_NORMAL_VALUE;
+      }
 
-      double tn = dem_geom_info_get_imaging_time(dgi, row - 1, col);
-      double ts = dem_geom_info_get_imaging_time(dgi, row + 1, col);
-      double tw = dem_geom_info_get_imaging_time(dgi, row, col - 1);
-      double te = dem_geom_info_get_imaging_time(dgi, row, col + 1);
-
-      double dd = (ze - zw)/dx * (te - tw)/dx + (zn - zs)/dy * (tn - ts)/dy;
-
-      double z_s = dem_geom_info_get_satellite_height(dgi, row, col);
-
-      double R = d - (z_s - z) * dd;
-      double A = (z_s - z) + d * dd;
-
-      if (A <= 0)
-	float_image_set_pixel(mask, row, col, MASK_ACTIVE_SHADOW_VALUE);
-      else if (R <= 0)
-	float_image_set_pixel(mask, row, col, MASK_ACTIVE_LAYOVER_VALUE);
-
-      // FIXME: check in a temp version that always gives an empty mask
-      //        just so we can get the interace set.
-      float_image_set_pixel(mask, row, col, MASK_NORMAL_VALUE);
+      float_image_set_pixel(mask, row, col, pixel_value);
     }
   }
 
+  int max_dim = dgi->nrows > dgi->ncols ? dgi->nrows : dgi->ncols;
+  float_image_export_as_jpeg(mask, "lsm.jpg", max_dim, NAN);
   return mask;
 }
