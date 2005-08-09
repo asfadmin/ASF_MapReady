d3f34f543a6fe833b33981fe37da635a3304b673
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-08-09 23:00:50 +0000

    Just put hdr2ddr function in here. main is in hdr2ddr_main.c now.

--- experimental/hdr2ddr/hdr2ddr.c
+++ experimental/hdr2ddr/hdr2ddr.c
@@ -1,134 +1,70 @@
-/******************************************************************************
-*                                                                             *
-* Copyright (c) 2004, Geophysical Institute, University of Alaska Fairbanks   *
-* All rights reserved.                                                        *
-*                                                                             *
-* Redistribution and use in source and binary forms, with or without          *
-* modification, are permitted provided that the following conditions are met: *
-*                                                                             *
-*    * Redistributions of source code must retain the above copyright notice, *
-*      this list of conditions and the following disclaimer.                  *
-*    * Redistributions in binary form must reproduce the above copyright      *
-*      notice, this list of conditions and the following disclaimer in the    *
-*      documentation and/or other materials provided with the distribution.   *
-*    * Neither the name of the Geophysical Institute nor the names of its     *
-*      contributors may be used to endorse or promote products derived from   *
-*      this software without specific prior written permission.               *
-*                                                                             *
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" *
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   *
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  *
-* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE    *
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         *
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        *
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    *
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     *
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     *
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  *
-* POSSIBILITY OF SUCH DAMAGE.                                                 *
-*                                                                             *
-*       For more information contact us at:                                   *
-*                                                                             *
-*       Alaska Satellite Facility                                             *
-*       Geophysical Institute                   http://www.asf.alaska.edu     *
-*       University of Alaska Fairbanks          uso@asf.alaska.edu            *
-*       P.O. Box 757320                                                       *
-*       Fairbanks, AK 99775-7320                                              *
-*                                                                             *
-******************************************************************************/
-
 #include "asf.h"
-#include "asf_meta.h"
-#include "las.h"
+#include "ddr.h"
 #include "hdr.h"
 
-#define VERSION 1.0
-
-static
-void usage(char *name)
+void hdr2ddr(jpl_header *hdr, struct DDR *ddr)
 {
- printf("\n"
-	"USAGE:\n"
-	"   %s <hdr_name> <las_name>\n",name);
- printf("\n"
-	"REQUIRED ARGUMENTS:\n"
-	"   hdr_name   Base name of the hdr file.  I.E. if <hdr_name> is\n"
-	"                \"test1\", then \"test1.hdr\" must be in the working directory\n"
- 	"   las_name    Base name of the ddr file.  I.E. if <las_name> is\n"
-	"                \"test2\", then both \"test2.ddr\" and \"test2.meta\" must be\n"
-	"                in the working directory\n");
-printf("\n"
-	"DESCRIPTION:\n"
-	"   %s converts HDR file to DDR file.\n", name);
- printf("\n"
-	"Version %.2f, ASF SAR Tools\n"
-	"\n",VERSION);
- exit(EXIT_FAILURE);
+  // Initialize ddr values
+  c_intddr(ddr);
+
+  // Image dimension
+  ddr->nl = hdr->line_count;
+  ddr->ns = hdr->sample_count;
+
+  // Number of bands; int
+  ddr->nbands = 1;
+
+  // Data type; int
+  ddr->dtype = DTYPE_FLOAT;
+
+  // Worthless date & time fields; both char[12]
+  strcpy (ddr->last_used_date,"");
+  strcpy (ddr->last_used_time,"");
+
+  // System byte ordering style; char[12]
+  strcpy(ddr->system, "ieee-std");
+
+  // Projection units; char[12]
+  strcpy(ddr->proj_units, "seconds");
+  ddr->valid[DDPUV] = VALID;
+
+  // Increment per sample in x & y directions; both double
+  ddr->line_inc   = fabs(hdr->line_increment) * 1200;
+  ddr->sample_inc = fabs(hdr->sample_increment) * 1200;
+  ddr->valid[DDINCV] = VALID;
+
+  // Line/sample relative to master image; both int
+  ddr->master_line   = 1;
+  ddr->master_sample = 1;
+
+  // Projection distance per pixel; both double (pixel size)*/
+  ddr->pdist_y = fabs(hdr->line_increment) * 3600;
+  ddr->pdist_x = fabs(hdr->sample_increment) * 3600;
+  ddr->valid[DDPDV] = VALID;
+
+  // Projection stuff
+  ddr->proj_code = 0; // Things are in Geographic
+  ddr->zone_code = 0;
+  ddr->valid[DDZCV] = VALID;
+  ddr->valid[DDPCV] = VALID;
+
+  // Datum Code; int
+  ddr->datum_code = 12; // Pulled out of thin air
+  ddr->valid[DDDCV] = VALID;
+
+  // Corner Coordinates; all double[2]
+  ddr->upleft[0] = hdr->start_lat*3600;
+  ddr->upleft[1] = hdr->start_lon*3600;
+  ddr->loleft[0] = hdr->start_lat*3600 +
+                   hdr->line_count * hdr->line_increment*3600;
+  ddr->loleft[1] = hdr->start_lon*3600;
+  ddr->upright[0] = hdr->start_lat*3600;
+  ddr->upright[1] = hdr->start_lon*3600 +
+                    hdr->sample_count * hdr->sample_increment*3600;
+  ddr->loright[0] = hdr->start_lat*3600 +
+                    hdr->line_count * hdr->line_increment*3600;
+  ddr->loright[1] = hdr->start_lon*3600 +
+                    hdr->sample_count * hdr->sample_increment*3600;
+  ddr->valid[DDCCV] = VALID;
+
 }
-
-int main(int argc, char **argv)
-{
-  FILE *fp;
-  char las_name[255];
-  char hdr_name[255];
-  char line[255], value[255], key[255];
-  struct DDR ddr;
-  jpl_header *hdr;
-  extern int currArg; /* from cla.h in asf.h... initialized to 1 */
-  
-  /* Parse command line */
-  if (argc-currArg < 2)
-    {printf("Insufficient arguments.\n"); usage(argv[0]);}
-  if (argc-currArg > 2)
-    {printf("Excessive arguments.\n"); usage(argv[0]);}
-  create_name(hdr_name, argv[currArg], ".hdr");
-  create_name(las_name, argv[currArg+1], ".ddr");
-  
-  /* Allocate memory for JPL header structure */
-  hdr = (jpl_header *) MALLOC(sizeof(jpl_header));
-
-  /* Read .hdr and fill meta structures */ 
-  fp = FOPEN(hdr_name, "r");
-  fgets(line, 255, fp);
-  sscanf(line, "%d", &hdr->magnitude_bytes);
-  fgets(line, 255, fp);
-  sscanf(line, "%d", &hdr->elevation_bytes);
-  fgets(line, 255, fp);
-  sscanf(line, "%s", hdr->data_type, key);
-  fgets(line, 255, fp);
-  sscanf(line, "%lf %lf", &hdr->elevation_scale, &hdr->elevation_shift);
-  fgets(line, 255, fp);
-  sscanf(line, "%lf %lf", &hdr->line_increment, &hdr->sample_increment);
-  fgets(line, 255, fp);
-  sscanf(line, "%lf %lf", &hdr->start_lat, &hdr->start_lon);
-  fgets(line, 255, fp);
-  sscanf(line, "%d %d", &hdr->line_count, &hdr->sample_count);
-  FCLOSE(fp);
-
-  /*
-  printf("Magnitude Bytes per Pixel: %d\n", hdr->magnitude_bytes);
-  printf("Elevation Bytes per Pixel: %d\n", hdr->elevation_bytes);
-  printf("Date file type: %s\n", hdr->data_type);
-  printf("Elevation Scale and Shift(M): %.8lf %.8lf\n", hdr->elevation_scale, 
-	 hdr->elevation_shift);
-  printf("Post Spacing(Deg): %.8lf %.8lf\n", hdr->line_increment, 
-	 hdr->sample_increment);
-  printf("Starting corner position(s,c): %.6lf %.6lf\n", hdr->start_lat, 
-	 hdr->start_lon);
-  printf("Data file dimensions: %d %d\n", hdr->line_count, hdr->sample_count);
-  */
-
-  /* Fill ddr struct with valid data */
-  hdr2ddr(hdr, &ddr);
-  
-  /* Write DDR file */
-  c_putddr(las_name, &ddr);
-  
-  /* Clean and report */
-  printf("   Wrote %s from %s.\n",
-	 las_name, hdr_name);
-  
-  return 0;
-}
-
-
