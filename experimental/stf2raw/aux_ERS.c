41b564deaca867e719dbf3007a8c366c58afc1a1
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-08-23 18:47:04 +0000

    Renamed aux.h to auxiliary.h because Win32 systems don't allow files named aux

--- experimental/stf2raw/aux_ERS.c
+++ experimental/stf2raw/aux_ERS.c
@@ -1,7 +1,7 @@
 /*ERS Auxiliary data decoding utilites.*/
 #include "asf.h"
 #include "decoder.h"
-#include "aux.h"
+#include "auxiliary.h"
 
 /*******************************************
 DecodeAux:
