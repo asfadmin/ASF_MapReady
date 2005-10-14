c96301b82aec02d09d5a2c551d9eade5fe05b151
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-10-14 19:06:24 +0000

    Added a "vector_project" method, for use in the local slope calculations.

--- src/asf_terrain_correct/vector.c
+++ src/asf_terrain_correct/vector.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <math.h>
 #include <stdlib.h>
-
+#include <stdio.h> // FIXME can take this out
 #include "vector.h"
 
 Vector *
@@ -83,7 +83,7 @@ vector_multiply (Vector *self, double factor)
 double
 vector_magnitude (Vector *self)
 {
-  return sqrt (pow (self->x, 2) + pow (self->y, 2) + pow (self->z, 2));
+  return sqrtl (powl (self->x, 2) + powl (self->y, 2) + powl (self->z, 2));
 }
 
 double
@@ -94,6 +94,58 @@ vector_angle (Vector *self, Vector *other)
 
   return acos (vector_dot (self, other) / (vector_magnitude (self) 
 					   * vector_magnitude (other)));
+}
+
+static void
+vector_normalize(Vector *self)
+{
+  vector_multiply (self, 1.0/vector_magnitude(self));
+}
+
+Vector *
+vector_project (Vector *self, Vector *other1, Vector *other2)
+{
+  assert (vector_magnitude (self) > 0.0);
+  assert (vector_magnitude (other1) > 0.0);
+  assert (vector_magnitude (other2) > 0.0);
+
+  double m = vector_magnitude (other1);
+
+  /* compute the normal for the plane */
+  Vector *n = vector_cross (other1, other2);
+  vector_normalize(n);
+
+  assert (vector_magnitude (n) > 0.0);
+  assert (is_relatively_small(vector_dot (n, other1), m));
+  assert (is_relatively_small(vector_dot (n, other2), m));
+
+  Vector *ns = vector_cross (n, self);
+  vector_normalize(ns);
+
+  Vector * s2 = vector_copy(self);
+  vector_normalize(s2);
+
+  assert (vector_magnitude (ns) > 0.0);
+  assert (is_relatively_small(vector_dot (ns, n), m));
+  assert (is_relatively_small(vector_dot (ns, s2), m));
+
+  Vector *ret = vector_cross (ns, n);
+  vector_normalize(ret);
+
+  assert (is_relatively_small(vector_dot (ret, n), m));
+  assert (is_relatively_small(vector_dot (ret, ns), m));
+
+  double a = vector_angle (ret, self);
+
+  if (a > 3.141592654)
+    vector_multiply (ret, -1);
+
+  assert (vector_angle(ret, self) < 1.6);
+
+  vector_free(n);
+  vector_free(ns);
+
+  return ret;
 }
 
 void
