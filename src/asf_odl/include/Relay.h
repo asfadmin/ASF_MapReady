2bc92314746efc296dd7601d45d12ac9baeab6f4
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-10-24 22:36:45 +0000

    Copy the va_list instead of setting it equal to the source list: -       va_list new = _ap;             \ +       va_list new;                    \ +       va_copy(new,_ap);              \ This should fix the build failures some systems have. Also did some whitespace editting.

--- src/asf_odl/include/Relay.h
+++ src/asf_odl/include/Relay.h
@@ -1,13 +1,13 @@
 /*=============================================================================
- |  @(#)Relay.h	1.2 96/02/23 19:19:44
+ |  @(#)Relay.h 1.2 96/02/23 19:19:44
  |
  |  Message Relay Object.
  |  Copyright (C) Jet Propulsion Laboratory.
  |  Alaska SAR Facility (ASF) Project.
  |
  *============================================================================*/
-#ifndef	_RELAY_H_
-#define	_RELAY_H_
+#ifndef _RELAY_H_
+#define _RELAY_H_
 
 #ifdef  NOSTDARG
 #include <varargs.h>
@@ -17,7 +17,7 @@
 #include "Msg.h"
 
 
-#ifdef	NOSTDARG
+#ifdef  NOSTDARG
 void*   Relay();
 #else
 void*   Relay(Msg_t, void* obj, va_list);
@@ -25,31 +25,33 @@ void*   Relay(Msg_t, void* obj, va_list);
 
 
 #ifdef  NOSTDARG
-#define	va_init(_ap,_msg,_obj)		\
-    Msg_t   _msg;			\
-    va_list _ap;			\
-    va_start(_ap);			\
-    _msg = va_arg(_ap, Msg_t);		\
-    if (_msg !=_Relay)			\
-	*(void**)&_obj = va_arg(_ap, void*); \
-    else {				\
-        va_list new = _ap;		\
-        _msg = va_arg(new, Msg_t);	\
-	*(void**)&_obj = va_arg(new, void*); \
-        _ap  = va_arg(new, va_list);	\
+#define va_init(_ap,_msg,_obj)               \
+    Msg_t   _msg;                            \
+    va_list _ap;                             \
+    va_start(_ap);                           \
+    _msg = va_arg(_ap, Msg_t);               \
+    if (_msg !=_Relay)                       \
+        *(void**)&_obj = va_arg(_ap, void*); \
+    else {                                   \
+        va_list new;                         \
+        va_copy(new,_ap);                    \
+        _msg = va_arg(new, Msg_t);           \
+        *(void**)&_obj = va_arg(new, void*); \
+        _ap  = va_arg(new, va_list);         \
     }
 #else
-#define	va_init(_ap,_msg,_obj)		\
-    va_list _ap;			\
-    va_start(_ap, _msg);		\
-    if (_msg !=_Relay)			\
-	*(void**)&_obj = va_arg(_ap, void*); \
-    else {				\
-        va_list new = _ap;		\
-        _msg = va_arg(new, Msg_t);	\
-	*(void**)&_obj = va_arg(new, void*); \
-        _ap  = va_arg(new, va_list);	\
+#define va_init(_ap,_msg,_obj)               \
+    va_list _ap;                             \
+    va_start(_ap, _msg);                     \
+    if (_msg !=_Relay)                       \
+        *(void**)&_obj = va_arg(_ap, void*); \
+    else {                                   \
+        va_list new;                         \
+        va_copy(new,_ap);                    \
+        _msg = va_arg(new, Msg_t);           \
+        *(void**)&_obj = va_arg(new, void*); \
+        _ap  = va_arg(new, va_list);         \
     }
 #endif
 
-#endif	/*!_RELAY_H_ */
+#endif  /*!_RELAY_H_ */
