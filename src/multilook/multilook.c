9e565a1ed237968240774876918bed1a65881c8b
rgens <rgens@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-12-05 20:41:25 +0000

    Commented out the RGB three-band DDR business. Still needs to be replaced with something. The SAR training processor depends on this stuff. Get on that stuff a little later.

--- src/multilook/multilook.c
+++ src/multilook/multilook.c
@@ -210,7 +210,7 @@ int main(int argc, char *argv[])
 	/* Create 3-band image's DDR.
 	   Currently metadata file don't know anything about multiband imagery.
 	   We will need to convert the current version for single band amplitude
-	   image back to metadata version 0.9 and change a couple of values */
+	   image back to metadata version 0.9 and change a couple of values *
 	sprintf(cmd, "convert_meta %s 1.3 %s 0.9", metaIn, metaOut);
 	system(cmd);
 
@@ -218,7 +218,7 @@ int main(int argc, char *argv[])
 	newddr.dtype=EBYTE;
 	newddr.nbands=3;
 	c_putddr(imgfile,&newddr);
-  
+	*/
   
 	fiamp = fopenImage(fnm1,"rb");
 	fiphase = fopenImage(fnm2,"rb");
@@ -333,7 +333,7 @@ int main(int argc, char *argv[])
 			printf("   Completed %3.0f percent\n", percent);
 			percent+=5.0;
 		}
-    
+		/*
 		for (i=0;i<outWid;i++)
 		{
 			redPtr[i] = imgData[i].red;
@@ -351,12 +351,13 @@ int main(int argc, char *argv[])
 		FSEEK64(flas,blu_offset,SEEK_SET);
 		FWRITE(bluPtr,1,outWid,flas);
     
-		/* reposition data for next read */
+		* reposition data for next read *
 		for (i=0;i<nitems;i++)
 		{
 			ampIn[i] = ampIn[i + newitems];
 			phaseIn[i] = phaseIn[i + newitems];
 		}
+		*/
 	}
   
 	/* 
@@ -364,7 +365,7 @@ int main(int argc, char *argv[])
 	* a 3 sequential band image
 	*/
 /*	printf("\n\tdone with multilook\n");
-	printf("writing out LAS/RGB image file\n");*/
+	printf("writing out LAS/RGB image file\n");*
 	printf("   Completed 100 percent\n\n   Wrote %lld bytes of data\n\n", 
 	       (long long)(outLen*outWid*4));
 	if (logflag) {
@@ -374,6 +375,7 @@ int main(int argc, char *argv[])
 	  StopWatchLog(fLog);
 	  FCLOSE(fLog);
 	}
+*/
 
 	FREE(ampIn);
 	FREE(phaseIn);
