044b07f14b6415391187bfbc54faff9a0e27296e
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-12-13 22:32:53 +0000

    Change the calculation so it doesn't assume a range 0-255.

--- src_tc/sarmodel/cintensity.c
+++ src_tc/sarmodel/cintensity.c
@@ -4,12 +4,12 @@
 
 #include "constants.h"
 
-   void cintensity(sat,tar,normal,bytes,sini)
+   void cintensity(sat,tar,normal,doubles,sini)
 
    double sat[],
           tar[],
           normal[],
-          *bytes,
+          *doubles,
           *sini;
 
    {
@@ -32,10 +32,20 @@
    cosi = cosi / magdiff;
    iang = acos(cosi);   
    *sini = sin(iang);
+
+   // not clear what this really does
    bottem = *sini + 0.1 * cosi;
-   sigma = .0133 * cosi / (bottem * bottem * bottem);
+
+   // replace this ....
+//   sigma = .0133 * cosi / (bottem * bottem * bottem);
+//   if (sigma < 0.0) sigma = 0.0;
+//   *bytes = 267.0000 * sqrt(sigma);
+//   if (*bytes > 255.0) *bytes = 255.0;
+
+   // .... with this   --kh
+   sigma = cosi / (bottem * bottem * bottem);
    if (sigma < 0.0) sigma = 0.0;
-   *bytes = 267.0000 * sqrt(sigma);
-   if (*bytes > 255.0) *bytes = 255.0;
+   *doubles = sqrt(sigma);
+
    return;
    }   
