159cb3307a2324f54b0e41b3915948bfb3f24384
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-12-15 20:28:27 +0000

    Added untested code to import internal format DEMs

--- src/asf_terrain_correct/map_projected_dem.c
+++ src/asf_terrain_correct/map_projected_dem.c
@@ -1,16 +1,16 @@
 // Implementation of the interface described in map_projected_dem.h.
 
 #include <math.h>
-
+#include <string.h>
 #include <gsl/gsl_math.h>
-
 #include <ddr.h>
 #include <libasf_proj.h>
 #include "map_projected_dem.h"
+#include <asf_reporting.h>
 
 MapProjectedDEM *
-map_projected_dem_new_from_las (const char *las_header_file, 
-				const char *las_data_file)
+map_projected_dem_new_from_las (const char *las_header_file,
+                                const char *las_data_file)
 {
   MapProjectedDEM *self = g_new (MapProjectedDEM, 1);
 
@@ -37,9 +37,9 @@ map_projected_dem_new_from_las (const char *las_header_file,
 
   // Read the actual data.
   g_assert (ddr.dtype == DTYPE_SHORT);
-  self->data = float_image_new_from_file_with_sample_type 
-    (self->size_x, self->size_y, las_data_file, 0, 
-     FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN, 
+  self->data = float_image_new_from_file_with_sample_type
+    (self->size_x, self->size_y, las_data_file, 0,
+     FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN,
      FLOAT_IMAGE_SAMPLE_TYPE_SIGNED_TWO_BYTE_INTEGER);
 
   // The signed 16 bit LAS DEMs use this magic number to mean "no
@@ -51,13 +51,13 @@ map_projected_dem_new_from_las (const char *las_header_file,
   for ( ii = 0 ; ii < self->size_y ; ii++ ) {
     for ( jj = 0 ; jj < self->size_x ; jj++ ) {
       if ( gsl_fcmp (float_image_get_pixel (self->data, jj, ii), no_data_value,
-		     0.0001) == 0) {
-	uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
-			       MAP_PROJECTED_DEM_INVALID);
+                     0.0001) == 0) {
+        uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
+                               MAP_PROJECTED_DEM_INVALID);
       }
       else {
-	uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
-			       MAP_PROJECTED_DEM_VALID);	
+        uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
+                               MAP_PROJECTED_DEM_VALID);
       }
     }
   }
@@ -66,16 +66,88 @@ map_projected_dem_new_from_las (const char *las_header_file,
   // data and the mask had better be the same as the size of this
   // instance.
   g_assert (self->size_x == self->data->size_x
-	    && self->size_y == self->data->size_y
-	    && self->size_x == self->invalid_data_mask->size_x
-	    && self->size_y == self->invalid_data_mask->size_y);
+            && self->size_y == self->data->size_y
+            && self->size_x == self->invalid_data_mask->size_x
+            && self->size_y == self->invalid_data_mask->size_y);
 
   return self;
 }
 
 MapProjectedDEM *
-map_projected_dem_new_subdem (MapProjectedDEM *model, double x_start, 
-			      double x_end, double y_start, double y_end)
+map_projected_dem_new_from_asf_internal (const char *asf_header_file,
+                                         const char *asf_data_file)
+{
+
+  meta_parameters *meta = meta_read (asf_header_file);
+  asfRequire(meta->sar->image_type == 'P',
+             "DEM is not projected, this makes no sense\n");
+  asfRequire(meta->projection != NULL,
+             "Metadata projection block is not present!\n");
+
+  MapProjectedDEM *self = g_new (MapProjectedDEM, 1);
+
+  self->size_x = meta->general->sample_count;
+  self->size_y = meta->general->line_count;
+  self->upper_left_x = meta->projection->startX;
+  self->upper_left_y = meta->projection->startY;
+  self->projection_coordinates_per_x_pixel = meta->projection->perX;
+  self->projection_coordinates_per_y_pixel = meta->projection->perY;
+  self->projection_type = meta->projection->type;
+  switch (self->projection_type) {
+    case UNIVERSAL_TRANSVERSE_MERCATOR:
+    case POLAR_STEREOGRAPHIC:
+    case ALBERS_EQUAL_AREA:
+    case LAMBERT_CONFORMAL_CONIC:
+    case LAMBERT_AZIMUTHAL_EQUAL_AREA:
+    case STATE_PLANE:
+      memcpy (&(self->projection_type), &(meta->projection->param),
+              sizeof(project_parameters_t));
+      break;
+    case SCANSAR_PROJECTION:
+      asfPrintError("The DEM in the SCANSAR Projection; this makes no sense.\n");
+      break;
+    default:
+      asfPrintError("DEM has an unknown projection type.\n");
+  }
+
+  // Read the actual data.
+  self->data = float_image_new_from_file (
+                self->size_x, self->size_y, asf_data_file, 0,
+                FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN);
+
+  // Use this magic number to mean "no data" to create the invalid_data_mask.
+  const float no_data_value = 0.0;
+  self->invalid_data_mask = uint8_image_new (self->size_x, self->size_y);
+  size_t ii, jj;
+  for ( ii = 0 ; ii < self->size_y ; ii++ ) {
+    for ( jj = 0 ; jj < self->size_x ; jj++ ) {
+      float pixel_value = float_image_get_pixel( self->data, jj, ii);
+      if ( gsl_fcmp( pixel_value, no_data_value, 0.0001) == 0) {
+        uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
+                               MAP_PROJECTED_DEM_INVALID);
+      }
+      else {
+        uint8_image_set_pixel (self->invalid_data_mask, jj, ii,
+                               MAP_PROJECTED_DEM_VALID);
+      }
+    }
+  }
+
+  // For internal consistency, the size of the images that store the
+  // data and the mask had better be the same as the size of this
+  // instance.
+  asfRequire (   self->size_x == self->data->size_x
+              && self->size_y == self->data->size_y
+              && self->size_x == self->invalid_data_mask->size_x
+              && self->size_y == self->invalid_data_mask->size_y,
+              "DEM and internally generated DEM mask are inconsistently sized\n");
+
+  return self;
+}
+
+MapProjectedDEM *
+map_projected_dem_new_subdem (MapProjectedDEM *model, double x_start,
+                              double x_end, double y_start, double y_end)
 {
   g_assert (x_start < x_end);
   g_assert (y_start < y_end);
@@ -104,7 +176,7 @@ map_projected_dem_new_subdem (MapProjectedDEM *model, double x_start,
   }
   self->size_x = width;
   self->projection_coordinates_per_x_pixel = pcpxp;
-  
+
   // Find the corner and extent of the new subdem in the y direction.
   // This is a bit different than the x direction computations because
   // the FloatImage y index increases in a downward direction, and map
@@ -126,7 +198,7 @@ map_projected_dem_new_subdem (MapProjectedDEM *model, double x_start,
   self->data = float_image_new_subimage (model->data, xps, yps, width, height);
 
   self->invalid_data_mask = uint8_image_new_subimage (model->invalid_data_mask,
-						      xps, yps, width, height);
+                                                      xps, yps, width, height);
 
   self->projection_type = model->projection_type;
   self->projection_parameters = model->projection_parameters;
@@ -136,7 +208,7 @@ map_projected_dem_new_subdem (MapProjectedDEM *model, double x_start,
 
 void
 map_projected_dem_get_x_y_h (MapProjectedDEM *self, ssize_t pixel_x,
-			     ssize_t pixel_y, double *x, double *y, double *h)
+                             ssize_t pixel_y, double *x, double *y, double *h)
 {
   *x = self->upper_left_x + self->projection_coordinates_per_x_pixel * pixel_x;
   *y = self->upper_left_y - self->projection_coordinates_per_y_pixel * pixel_y;
@@ -144,13 +216,13 @@ map_projected_dem_get_x_y_h (MapProjectedDEM *self, ssize_t pixel_x,
 }
 
 void
-map_projected_dem_get_latitudes_longitudes_heights 
+map_projected_dem_get_latitudes_longitudes_heights
   (MapProjectedDEM *self, ssize_t row, double *latitudes, double *longitudes,
    double *heights)
 {
   // Routine to use to inverse project an array of points.
   int (*project_arr_inv) (project_parameters_t *pps, double *x, double *y,
-			  double **lat, double **lon, long length);
+                          double **lat, double **lon, long length);
   project_arr_inv = NULL;   // Compiler reassurance.
   switch ( self->projection_type ) {
   case UNIVERSAL_TRANSVERSE_MERCATOR:
@@ -169,10 +241,10 @@ map_projected_dem_get_latitudes_longitudes_heights
 
   size_t ii;
   for ( ii = 0 ; ii < self->size_x ; ii++ ) {
-    proj_x[ii] = (self->upper_left_x 
-		  + self->projection_coordinates_per_x_pixel * ii);
-    proj_y[ii] = (self->upper_left_y 
-		  - self->projection_coordinates_per_y_pixel * row);
+    proj_x[ii] = (self->upper_left_x
+                  + self->projection_coordinates_per_x_pixel * ii);
+    proj_y[ii] = (self->upper_left_y
+                  - self->projection_coordinates_per_y_pixel * row);
 
     // kludge to work around some proj oddities
     if (self->projection_type == UNIVERSAL_TRANSVERSE_MERCATOR)
@@ -181,9 +253,9 @@ map_projected_dem_get_latitudes_longitudes_heights
     heights[ii] = float_image_get_pixel (self->data, ii, row);
   }
 
-  int return_code = project_arr_inv (&(self->projection_parameters), proj_x, 
-				     proj_y, &latitudes, &longitudes,
-				     self->size_x);
+  int return_code = project_arr_inv (&(self->projection_parameters), proj_x,
+                                     proj_y, &latitudes, &longitudes,
+                                     self->size_x);
   g_assert (return_code == TRUE);
 
   g_free (proj_y);
