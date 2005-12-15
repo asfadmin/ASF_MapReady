3b0849ae29d28e7763d474e2a08cfa9e0662efab
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2005-12-15 20:08:14 +0000

    More accurate variable names

--- src/libasf_reporting/diagnostics.c
+++ src/libasf_reporting/diagnostics.c
@@ -9,24 +9,25 @@ niftiness to make it possible to explain things to the user
 
 
 void require_function (const char *file, int line, int condition,
-                       const char *condStr, const char *message, ...)
+                       const char *conditionString, const char *formatString,
+		       ...)
 {
   if ( !condition ) {
     va_list ap;       /* Variadic arguments pointer.  */
-    char temp[4096];  /* Combination of condition string & message */
+    char temp[4096];  /* Combination of condition string & formatString */
 
     sprintf (temp, "%s:%d: failed assertion `%s'\n%s",
-             file, line, condStr, message);
+             file, line, conditionString, formatString);
     asfPrintError (temp, ap);
   }
 }
 
-void die_function (const char *file, int line, const char *message, ...)
+void die_function (const char *file, int line, const char *formatString, ...)
 {
   va_list ap;       /* Variadic arguments pointer.  */
   char temp[4096];  /* Combination of condition string & message */
 
-  sprintf (temp, "At source file %s line %d: %s", file, line, message);
+  sprintf (temp, "At source file %s line %d: %s", file, line, formatString);
   asfPrintError (temp, ap);
   /* Reassure compiler that this noreturn fctn doesn't return (without
      having to declare asfPrintError to be `noreturn'.  */
