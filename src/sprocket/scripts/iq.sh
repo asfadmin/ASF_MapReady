6ce00806504deb8c096762167ca7635a968d8bc4
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-01-27 02:04:00 +0000

    Create the java dir for sprocket classes (new build system does not do it anymore)

--- src/sprocket/scripts/iq.sh
+++ src/sprocket/scripts/iq.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-# Dynamically get the path to all our Java classes. 
+# Dynamically get the path to all our Java classes.
 # Take the absolute path to this scipt ($0), and work backwards to the tools
 # base directory, then append the /java directory. Gross hack, but it works.
 # Example
-#    $0 = /usr/local/asf_tools/bin/linux/script_name
-#    dirname returns /usr/local/asf_tools/bin/linux
+#    $0 = /usr/local/asf_tools/bin/script_name
 #    dirname returns /usr/local/asf_tools/bin
 #    dirname returns /usr/local/asf_tools
 #    then append /java
 #    and BLAMO, our classpath is /usr/local/asf_tools/java
 tempDir=`dirname $0`
-tempDir=`dirname $tempDir`
 tempDir=`dirname $tempDir`
 CLASSPATH=${tempDir}/java
 export CLASSPATH
