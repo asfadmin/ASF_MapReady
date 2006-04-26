c9fd3dae111d1c297cee89b7455f5a3705a47d76
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-04-26 22:36:01 +0000

    Call the library function in libasf_sar to do the reskew_deming.

--- src/reskew_dem/reskew_dem.c
+++ src/reskew_dem/reskew_dem.c
@@ -66,13 +66,6 @@ BUGS:
 
 #define VERSION 1.6
 #define NUM_ARGS 4
-//#define FALSE 1
-//#define TRUE 0
-double earth_radius; /* current earth radius, meters (FIXME: update with azimuth, range) */
-double satHt; /* satellite height from center of earth, meters */
-double slant_to_first, slant_per; /* slant ranges, meters (FIXME: split out DEM and SAR slant ranges) */
-int gr_ns,sr_ns;
-
 
 void usage(char *name)
 {
@@ -101,12 +94,7 @@ void usage(char *name)
 
 int main(int argc, char *argv[])
 {
-	float *grDEMline,*srDEMline,*outAmpLine;
-	register int line,nl,percent;
 	char inDEMfile[255],outDEMfile[255],outAmpFile[255],inMetafile[255];
-	FILE *inDEM,*outDEM,*outAmp;
-	meta_parameters *metaIn, *metaDEM;
-//	struct DDR ddrDEM,outddr,inddr;
 
 	system("date");
 	printf("Program: reskew_dem\n\n");
@@ -134,104 +122,6 @@ currArg=1;
 	strcpy(outDEMfile,argv[currArg+2]);
 	strcpy(outAmpFile,argv[currArg+3]);
 
-/*Extract DDRs*/
-	//c_getddr(inDEMfile,&ddrDEM);
-	//outddr=ddrDEM;
-	//nl=ddrDEM.nl;
-	//gr_ns=ddrDEM.ns;
-	//sr_ns=gr_ns; 
-	/*sr_ns=gr_ns-400;*/  /* The 400 pixels here has to match the 
-	  extra amount added in the demIFM script.*/
-	//outddr.ns=sr_ns;
-	//c_putddr(outDEMfile,&outddr);
-	//c_putddr(outAmpFile,&outddr);
-
-/* Get metadata */
-	metaIn = meta_read(inMetafile);
-	metaDEM = meta_read(inDEMfile);
-	nl = metaDEM->general->line_count;
-	gr_ns = metaDEM->general->sample_count;
-	sr_ns = metaIn->general->sample_count;
-/* The 400 pixels here has to match theextra
-	                         * amount added in the demIFM script.*/
-	//metaOut->general->sample_count = sr_ns;
-	
-	earth_radius = meta_get_earth_radius(metaIn, nl/2, 0);
-	satHt = meta_get_sat_height(metaIn, nl/2, 0);
-	meta_get_slants(metaIn, &slant_to_first, &slant_per);
-
-	
-
-/*Open files.*/
-	inDEM  = fopenImage(inDEMfile,"rb");
-	outDEM = fopenImage(outDEMfile,"wb");
-	outAmp = fopenImage(outAmpFile,"wb");
-
-/*Allocate more memory (this time for data lines*/
-	grDEMline  = (float *)MALLOC(sizeof(float)*gr_ns);
-	srDEMline  = (float *)MALLOC(sizeof(float)*sr_ns);
-	outAmpLine = (float *)MALLOC(sizeof(float)*sr_ns);
-	
-/* Read deskewed data, write out reskewed data */
-	percent = 0;
-	for (line=0; line<nl; line++)
-	{
-		if ((line*100/nl)==percent) {
-		  printf("\r   Completed %3d percent",percent);
-		  percent+=5;
-		}
-#if 0
-		getFloatLine(inDEM,&ddrDEM,line,grDEMline);
-		dem_gr2sr(grDEMline,srDEMline,outAmpLine);
-		putFloatLine(outDEM,&outddr,line,srDEMline);
-		putFloatLine(outAmp,&outddr,line,outAmpLine);
-#else
-		get_float_line(inDEM,metaDEM,line,grDEMline);
-		dem_gr2sr(grDEMline,srDEMline,outAmpLine);
-		put_float_line(outDEM,metaIn,line,srDEMline);
-		put_float_line(outAmp,metaIn,line,outAmpLine);
-#endif
-	}
-	printf("\r   Completed 100 percent\n\n");
-
-	sprintf(logbuf,"   Converted %d lines from ground range to slant range.\n\n", nl);
-	printf("%s", logbuf);
-	// if (logflag) { printLog(logbuf); }
-
-/* Write meta files */
-	meta_write(metaIn, outDEMfile);
-	meta_write(metaIn, outAmpFile);
-
-/* Free memory, close files, & exit */
-	meta_free(metaDEM);
-	meta_free(metaIn);
-	FREE(grDEMline);
-	FREE(srDEMline);
-	FREE(outAmpLine);
-	FCLOSE(inDEM);
-	FCLOSE(outDEM);
-	FCLOSE(outAmp);
-
+	reskew_dem(inMetafile, inDEMfile, outDEMfile, outAmpFile);
 	exit(EXIT_SUCCESS);
 }
-
-
-
-float srE2srH(float srEpix,float height)
-{ /* {{{ */
-	double er;
-	double srE;
-	double cosPhi;
-	double srH;
-	er = earth_radius;
-	srE=slant_to_first+slant_per*srEpix;
-	/* Calculate ground angle to this slant range */
-	cosPhi=(satHt*satHt + er*er - srE*srE)/(2*satHt*er);
-	/* Calculate slant range with new earth height */
-	er+=height;
-	srH=sqrt(satHt*satHt + er*er - 2 * satHt * er * cosPhi);
-	return (srH-slant_to_first)/slant_per;
-} /* }}} */
-
-
-//#endif
