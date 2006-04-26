caf589072335754325ad24a2e3762e5fb8c7c8dc
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-04-26 20:29:02 +0000

    Use the new library function gr2sr.

--- src/gr2sr/gr2sr.c
+++ src/gr2sr/gr2sr.c
@@ -24,9 +24,13 @@ ALGORITHM REFERENCES:
 BUGS:
 
 ******************************************************************************/
-#include "gr2sr.h"
+#include "asf_sar.h"
 
-#define VERSION 0.1
+#include <stdio.h>
+#include <asf_reporting.h>
+#include <stdlib.h>
+
+#define VERSION 0.2
 
 static void usage(char *progname)
 {
@@ -46,24 +50,10 @@ static void usage(char *progname)
 
 int main(int argc,char *argv[])
 {
-  meta_parameters *inMeta, *outMeta;
+  float srPixSize= -1;   /* output pixel size             */
 
-  int   np, nl;         /* in number of pixels,lines      */
-  int   onp, onl;       /* out number of pixels,lines     */
-  int   ii;
-  float gr2sr[MAX_IMG_SIZE];    /* GR 2 SR resampling vector for Range  */
-  int   lower[MAX_IMG_SIZE];    /* floor of gr2sr vector                */
-  int   upper[MAX_IMG_SIZE];    /* ceiling of gr2sr vector              */
-  float ufrac[MAX_IMG_SIZE];    /* Upper fraction from gr2sr vector     */
-  float lfrac[MAX_IMG_SIZE];    /* Lower fraction from gr2sr vector     */
-  float srPixSize=0.0; /* output pixel size */
-
-  float *inBuf;          /* Input buffer                  */
-  float *outBuf;         /* Output buffer                 */
   char  infile[256];     /* Input file name               */
   char  outfile[256];    /* Output file name              */
-  FILE  *fpi, *fpo;      /* File pointers                 */
-  int   line;            /* Loop counter                  */
 
   if ((argc != 5 && argc != 3) || (argc == 5 && strcmp(argv[1], "-p") != 0)) {
     usage(argv[0]);
@@ -77,82 +67,13 @@ int main(int argc,char *argv[])
     create_name(outfile,argv[2],".img");
   }
 
-  inMeta = meta_read(infile);
-
   if (argc == 5) {
     srPixSize = atof(argv[2]);
-  } else {
-    /*
-      In an e-mail from Rick:
-       Slant Range Pixel Size = C (speed of light) / [SampleRate (sample 
-       rate) * 2,000,000.]
-
-       SampleRate can be extracted from the L1 metadata :
-          RNG CMPLX SAMPLE RATE        18.9599991
-
-       The meta->sar->range_sampling_rate is 10^6 times the value above,
-       so we use C/(2*meta->sar->range_sampling_rate)
-    */
-    srPixSize = SPD_LIGHT / ((2.0 * inMeta->sar->range_sampling_rate) *
-      inMeta->general->sample_count / inMeta->sar->original_sample_count);
+    gr2sr_pixsiz(infile, outfile, srPixSize);
   }
-
-  nl = inMeta->general->line_count;
-  np = inMeta->general->sample_count;
-
-  onl=nl;
-  gr2sr_vec(inMeta, srPixSize, gr2sr);
-  
-  /* Determine the output image size */
-  onp = 0;
-  for (ii=0; ii<MAX_IMG_SIZE; ii++) {
-     if (gr2sr[ii]<np) onp=ii; /* gr input still in range-- keep output */
-     else break; /* gr input is off end of image-- stop sr output */
+  else {
+    gr2sr(infile, outfile);
   }
-  
-  /* Split gr2sr into resampling coefficients */
-  for (ii=0; ii<onp; ii++) {
-     lower[ii] = (int) gr2sr[ii];
-     upper[ii] = lower[ii] + 1;
-     ufrac[ii] = gr2sr[ii] - (float) lower[ii];
-     lfrac[ii] = 1.0 - ufrac[ii];
-     if (lower[ii]>=np) lower[ii]=np-1; /* range clip */
-     if (upper[ii]>=np) upper[ii]=np-1; /* range clip */
-  }
-  
-  outMeta = meta_copy(inMeta);
-  outMeta->sar->slant_shift += ((inMeta->general->start_sample)
-                                * inMeta->general->x_pixel_size);
-  outMeta->general->start_sample = 0.0;
-  outMeta->sar->sample_increment = 1.0;
-  outMeta->sar->image_type       = 'S';
-  outMeta->general->x_pixel_size = srPixSize;
-  outMeta->general->sample_count = onp;
-
-  asfPrintStatus("Input  lines, samples: %i %i\n",nl,np);
-  asfPrintStatus("Output lines, samples: %i %i\n",onl,onp);
-
-  fpi = FOPEN(infile,"rb");
-  fpo = FOPEN(outfile,"wb");
-  inBuf = (float *) MALLOC (np*sizeof(float));
-  outBuf = (float *) MALLOC (onp*sizeof(float));
-
-  for (line = 0; line < onl; line++) {
-    get_float_line(fpi, inMeta, line, inBuf);
-    for (ii=0; ii<onp; ii++) { /* resample to slant range */
-       outBuf[ii] = inBuf[lower[ii]]*lfrac[ii]+inBuf[upper[ii]]*ufrac[ii];
-    }
-    put_float_line(fpo,outMeta,line,outBuf);
-  }
-
-  meta_write(outMeta, outfile);
-  meta_free(inMeta);
-  meta_free(outMeta);
-
-  FREE(inBuf);
-  FREE(outBuf);
-  FCLOSE(fpi);
-  FCLOSE(fpo);
 
   exit(EXIT_SUCCESS);
 }
