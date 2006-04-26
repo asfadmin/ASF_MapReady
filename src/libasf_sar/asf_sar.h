5f48422befa9bff21431c8d98115d4dcdd13deaa
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-04-26 22:49:25 +0000

    Prototype added for reskew_dem

--- src/libasf_sar/asf_sar.h
+++ src/libasf_sar/asf_sar.h
@@ -7,4 +7,8 @@
 int gr2sr(const char *infile, const char *outfile);
 int gr2sr_pixsiz(const char *infile, const char *outfile, float srPixSize);
 
+/* Prototypes from reskew_dem.c */
+int reskew_dem(char *inMetafile, char *inDEMfile, char *outDEMfile,
+	       char *outAmpFile);
+
 #endif
