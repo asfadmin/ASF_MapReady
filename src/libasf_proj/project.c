001ef8b435cedb7ce02800f4f99f10cdaaaa3e44
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-05-09 17:48:38 +0000

    Cut and paste in a function implementation of a asf_meta function (we have the header but don't link against the library itself).

--- src/libasf_proj/project.c
+++ src/libasf_proj/project.c
@@ -23,6 +23,60 @@ round (double arg)
 #endif /* #ifndef win32 */
 #endif /* #ifndef linux */
 
+// Fill in major and minor with the axes lenghts of spheroid.  This is
+// a cut-and-paste implementation of a function defined in asf_meta.h,
+// that we can't actually use here because we don't actually link
+// against that library (only include the header).
+void
+spheroid_axes_lengths (spheroid_type_t spheroid, double *major, double *minor)
+{
+  switch ( spheroid ) {
+  case BESSEL_SPHEROID:
+    *major = BESSEL_SEMIMAJOR;
+    *minor = *major - (1.0 / BESSEL_INV_FLATTENING) * *major;
+    break;
+  case CLARKE1866_SPHEROID:
+    *major = CLARKE1866_SEMIMAJOR;
+    *minor = *major - (1.0 / CLARKE1866_INV_FLATTENING) * *major;
+    break;
+  case CLARKE1880_SPHEROID:
+    *major = CLARKE1880_SEMIMAJOR;
+    *minor = *major - (1.0 / CLARKE1880_INV_FLATTENING) * *major;
+    break;
+  case GEM6_SPHEROID:
+    *major = GEM6_SEMIMAJOR;
+    *minor = *major - (1.0 / GEM6_INV_FLATTENING) * *major;
+    break;
+  case GEM10C_SPHEROID:
+    *major = GEM10C_SEMIMAJOR;
+    *minor = *major - (1.0 / GEM10C_INV_FLATTENING) * *major;
+    break;
+  case GRS1980_SPHEROID:
+    *major = GRS1980_SEMIMAJOR;
+    *minor = *major - (1.0 / GRS1980_INV_FLATTENING) * *major;
+    break;
+  case INTERNATIONAL1924_SPHEROID:
+    *major = INTERNATIONAL1924_SEMIMAJOR;
+    *minor = *major - (1.0 / INTERNATIONAL1924_INV_FLATTENING) * *major;
+    break;
+  case INTERNATIONAL1967_SPHEROID:
+    *major = INTERNATIONAL1967_SEMIMAJOR;
+    *minor = *major - (1.0 / INTERNATIONAL1967_INV_FLATTENING) * *major;
+    break;
+  case WGS72_SPHEROID:
+    *major = WGS72_SEMIMAJOR;
+    *minor = *major - (1.0 / WGS72_INV_FLATTENING) * *major;
+    break;
+  case WGS84_SPHEROID:
+    *major = WGS84_SEMIMAJOR;
+    *minor = *major - (1.0 / WGS84_INV_FLATTENING) * *major;
+    break;
+  default:
+    assert (0);
+    break;
+  }
+}
+
 // The default as advertised in the interface.
 static datum_type_t input_spheroid = GEM6_SPHEROID;
 void project_set_input_spheroid(spheroid_type_t spheroid)
