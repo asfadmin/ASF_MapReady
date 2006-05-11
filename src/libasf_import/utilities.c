2df5debef680d6811b88c2b5fc46eb1d5ac6f3cf
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-05-11 23:55:59 +0000

    Remove function also available in asf_meta.

--- src/libasf_import/utilities.c
+++ src/libasf_import/utilities.c
@@ -10,17 +10,6 @@ char *strdup(char *);
 #endif
 
 /* Helpful functions */
-int firstRecordLen(char *ceosName)
-{
-  FILE *f;
-  struct HEADER h;
-
-  f=FOPEN(ceosName,"rb");    /*Open file.*/
-  FREAD(&h,1,12,f);          /*Read first CEOS header.*/
-  FCLOSE(f);                 /*Close file*/
-  return bigInt32(h.recsiz); /*put recsiz in proper endian format & return*/
-}
-
 
 /* Check to see if an option was supplied or not
    If it was found, return its argument number
