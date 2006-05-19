5f478605d736db7208ae70fa99c878271f5cb40f
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-05-19 23:29:54 +0000

    Regeocoding of DEMs with datum transformation.  Not tested yet.  Needs addition of pseudoprojection support to libasf_raster to have a chance of working.

--- src/libasf_geocode/geocode_dem.c
+++ src/libasf_geocode/geocode_dem.c
@@ -2,6 +2,7 @@
 
 // Standard libraries.
 #include <math.h>
+#include <string.h>
 
 // Libraries from packages outside ASF.
 #include <glib.h>
@@ -15,6 +16,17 @@
 
 // Headers defined by this library.
 #include "asf_geocode.h"
+
+// Return true iff (x, y) falls in the region
+// ([0, image->size_x - 1), [0, image->size_y - 1))
+static int
+in_image (FloatImage *image, ssize_t x, ssize_t y)
+{
+  g_assert (image->size_x <= SSIZE_MAX && image->size_y <= SSIZE_MAX);
+  ssize_t sz_x = image->size_x, sz_y = image->size_y;
+
+  return 0 <= x && x < sz_x && 0 <= y && y < sz_y;
+}
 
 int
 geocode_dem (projection_type_t projection_type,	// What we are projection to.
@@ -44,6 +56,11 @@ geocode_dem (projection_type_t projection_type,	// What we are projection to.
 
   // We only deal with reprojection map projected DEMs.
   g_assert (imd->projection != NULL);
+
+  // FIXME: what to do with background value is something that still
+  // needs to be determined (probably in consultation with the guys
+  // working on terrain correction).
+  const float background_value = 0.0;
 
   // At the moment the projector_t and array_projector_t function that
   // "transform" pseudoprojection coordinates (i.e. lat/lons) into lat
@@ -100,7 +117,7 @@ geocode_dem (projection_type_t projection_type,	// What we are projection to.
   double min_y = DBL_MAX;
   double max_y = -DBL_MAX;
 
-  // In going around the edge, we are just tryin gto determine the
+  // In going around the edge, we are just trying to determine the
   // extent of the image in the horizontal, so we don't care about
   // height yet.
 
@@ -128,7 +145,7 @@ geocode_dem (projection_type_t projection_type,	// What we are projection to.
       return_code = unproject_input (ipp, xpc, ypc, ASF_PROJ_NO_HEIGHT,
 				     &(lats[current_edge_point]),
 				     &(lons[current_edge_point]), NULL);
-	g_assert (return_code);
+      g_assert (return_code);
       current_edge_point++;
     }
     for ( ; ii > 0 ; ii-- ) {
@@ -186,8 +203,12 @@ geocode_dem (projection_type_t projection_type,	// What we are projection to.
   size_t oix_max = ceil ((max_x - min_x) / pixel_size);
   size_t oiy_max = ceil ((max_y - min_y) / pixel_size);
 
+  // Output image dimensions.
+  size_t oi_size_x = oix_max + 1;
+  size_t oi_size_y = oiy_max + 1;
+
   // Output image.
-  FloatImage *oim = float_image_new (oix_max + 1, oiy_max + 1);
+  FloatImage *oim = float_image_new (oi_size_x, oi_size_y);
 
   // Convenience macros for getting a pixel.
 #define SET_PIXEL(x, y, value) float_image_set_pixel (oim, x, y, value)
@@ -211,10 +232,232 @@ geocode_dem (projection_type_t projection_type,	// What we are projection to.
     g_assert_not_reached ();
   }
 
-  // Set the pixels of the output image.
+  // We need to find the z coordinates in the output projection of all
+  // the pixels in the input DEM.  We store these values in their own
+  // FloatImage instance.
+
+  //FloatImage *x_coords = float_image_new (ii_size_x, ii_size_y);
+  //FloatImage *y_coords = float_image_new (ii_size_x, ii_size_y);
+  FloatImage *z_coords = float_image_new (ii_size_x, ii_size_y);
+
+  // We transform the points using the array transformation function
+  // for efficiency, but we don't want to do them all at once, since
+  // that would require huge gobs of memory.
+  const size_t max_transform_chunk_pixels = 5000000;
+  size_t rows_per_chunk = max_transform_chunk_pixels / ii_size_x;
+  size_t chunk_pixels = rows_per_chunk * ii_size_x;
+  double *chunk_x = g_new (double, chunk_pixels);
+  double *chunk_y = g_new (double, chunk_pixels);
+  double *chunk_z = g_new (double, chunk_pixels);
+  double *lat = g_new (double, chunk_pixels);
+  double *lon = g_new (double, chunk_pixels);
+  double *height = g_new (double, chunk_pixels);
+
+  // Transform all the chunks, storing results in the z coordinate image.
+  size_t ii, jj, kk;		// Index variables.
+  for ( ii = 0 ; ii < ii_size_y ; ) {
+    size_t rows_remaining = ii_size_y - ii;
+    size_t rows_to_load
+      = rows_per_chunk < rows_remaining ? rows_per_chunk : rows_remaining;
+    for ( jj = 0 ; jj < rows_to_load ; jj++ ) {
+      for ( kk = 0 ; kk < ii_size_x ; kk++ ) {
+	size_t current_chunk_pixel = jj * ii_size_x + kk;
+	size_t current_image_row = ii * rows_per_chunk + jj;
+	chunk_x[current_chunk_pixel] = ipb->startX + kk * ipb->perX;
+	chunk_y[current_chunk_pixel] 
+	  = ipb->startY + current_image_row * ipb->perY;
+	chunk_z[current_chunk_pixel]
+	  = float_image_get_pixel (iim, kk, current_image_row);
+      }
+    }
+    long current_chunk_pixels = rows_to_load * ii_size_x;
+    project_set_datum (ipb->datum);
+    array_unproject_input (ipp, chunk_x, chunk_y, chunk_z, &lat, &lon,
+			   &height, current_chunk_pixels);
+    project_set_datum (datum);
+    array_project_output (pp, lat, lon, height, &chunk_x, &chunk_y, &chunk_z,
+			  current_chunk_pixels);
+    for ( jj = 0 ; jj < rows_to_load ; jj++ ) {
+      for ( kk = 0 ; kk < ii_size_x ; kk++ ) {
+	size_t current_chunk_pixel = jj * ii_size_x + kk;
+	size_t current_image_row = ii * rows_per_chunk + jj;
+	// Current pixel x, y, z coordinates.
+	//float cp_x = (float) chunk_x[current_chunk_pixel];
+	//float cp_y = (float) chunk_y[current_chunk_pixel];
+	float cp_z = (float) chunk_z[current_chunk_pixel];
+	//float_image_set_pixel (x_coords, kk, current_image_row, cp_x);
+	//float_image_set_pixel (y_coords, kk, current_image_row, cp_y);
+	float_image_set_pixel (z_coords, kk, current_image_row, cp_z);
+      }
+    }
+
+    ii += rows_to_load;
+  }
+
+  g_free (chunk_x);
+  g_free (chunk_y);
+  g_free (chunk_z);
+  g_free (lat);
+  g_free (lon);
+  g_free (height);
+
+  // Now we want to determine the pixel coordinates in the input which
+  // correspond to each of the output pixels.  We can then sample the
+  // new height value already computed for that input pixel to
+  // determine the pixel value to use as output.
+
+  // We want to proceed in chunks as we did when going in the other
+  // direction.
+  rows_per_chunk = max_transform_chunk_pixels / oi_size_x;
+  chunk_pixels = rows_per_chunk * oi_size_x;
+  chunk_x = g_new (double, chunk_pixels);
+  chunk_y = g_new (double, chunk_pixels);
+  // We don't have height information in this direction, nor do we care.
+  chunk_z = NULL;
+  lat = g_new (double, chunk_pixels);
+  lon = g_new (double, chunk_pixels);
+  // We don't have height information in this direction, nor do we care.
+  height = NULL;
+
+  // Transform all the chunks, using the results to form the output image.
+  for ( ii = 0 ; ii < oi_size_y ; ) {
+    size_t rows_remaining = oi_size_y - ii;
+    size_t rows_to_load
+      = rows_per_chunk < rows_remaining ? rows_per_chunk : rows_remaining;
+    for ( jj = 0 ; jj < rows_to_load ; jj++ ) {
+      for ( kk = 0 ; kk < oi_size_x ; kk++ ) {
+	size_t current_chunk_pixel = jj * oi_size_x + kk;
+	size_t current_image_row = ii * rows_per_chunk + jj;
+	chunk_x[current_chunk_pixel] = min_x + kk * pixel_size;
+	chunk_y[current_chunk_pixel] = max_y - current_image_row * pixel_size;
+      }
+    }
+    long current_chunk_pixels = rows_to_load * ii_size_x;
+    project_set_datum (datum);
+    array_unproject_input (pp, chunk_x, chunk_y, NULL, &lat, &lon, NULL,
+			   current_chunk_pixels);
+    project_set_datum (ipb->datum);
+    array_project_output (ipp, lat, lon, NULL, &chunk_x, &chunk_y, NULL,
+			  current_chunk_pixels);
+    for ( jj = 0 ; jj < rows_to_load ; jj++ ) {
+      for ( kk = 0 ; kk < oi_size_x ; kk++ ) {
+	size_t current_chunk_pixel = jj * oi_size_x + kk;
+	size_t current_image_row = ii * rows_per_chunk + jj;
+
+	// Compute pixel coordinates in input image.
+	ssize_t in_x 
+	  = (chunk_x[current_chunk_pixel] - ipb->startX) / ipb->perX;
+	ssize_t in_y
+	  = -(chunk_y[current_chunk_pixel] - ipb->startY) / ipb->perY;
+
+	if ( in_image (z_coords, in_x, in_y) ) {
+	  // FIXME: something needs to be done somewhere about
+	  // propogating no data values.
+	  float_image_set_pixel (oim, kk, current_image_row,
+				 float_image_sample (z_coords, in_x, in_y,
+						     resample_method));
+	}
+	else {
+	  float_image_set_pixel (oim, kk, current_image_row, background_value);
+	}
+      }
+    }
+
+    ii += rows_to_load;
+  }
+
+  g_free (chunk_x);
+  g_free (chunk_y);
+  g_free (lat);
+  g_free (lon);
+
+
+  // Store the output image, and free image resources.
+  GString *output_data_file = g_string_new (output_image->str);
+  g_string_append (output_data_file, ".img");
+  return_code = float_image_store (oim, output_data_file->str,
+				   FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN);
+  g_assert (return_code == 0);
+  float_image_free (oim);
+  g_string_free (output_data_file, TRUE);
+
+  // Now we need some metadata for the output image.  We will just
+  // start with the metadata from the input image and add the
+  // geocoding parameters.
+
+  GString *input_meta_file = g_string_new (input_image->str);
+  g_string_append (input_meta_file, ".meta");
+
+  GString *output_meta_file = g_string_new (output_image->str);
+  g_string_append (output_meta_file, ".meta");
+
+  meta_parameters *omd = meta_read (input_meta_file->str);
+
+  // Adjust the metadata to correspond to the output image instead of
+  // the input image.
+
+  omd->general->x_pixel_size = pixel_size;
+  omd->general->y_pixel_size = pixel_size;
+  omd->general->line_count = oi_size_x;
+  omd->general->sample_count = oi_size_y;  
+
+  // SAR block is not really appropriate for map projected images, but
+  // since it ended up with this value that can signify map
+  // projectedness in it somehow, we fill it in for safety.
+  omd->sar->image_type = 'P';
+
+  // Note that we have already verified that the input image is
+  // projected, and since we initialize the output metadata from there
+  // we know we will have a projection block.
+  omd->projection->type = projection_type;
+  omd->projection->startX = min_x;
+  omd->projection->startY = max_y;
+  omd->projection->perX = pixel_size;
+  omd->projection->perY = -pixel_size;
+  strcpy (omd->projection->units, "meters");
+
+  // Set the spheroid axes lengths as appropriate for the output datum.
+  spheroid_axes_lengths (datum_spheroid (datum), &(omd->projection->re_major),
+			 &(omd->projection->re_minor));
+
+  // What the heck, might as well set the ones in the general block as
+  // well.
+  spheroid_axes_lengths (datum_spheroid (datum), &(omd->general->re_major),
+			 &(omd->general->re_minor));
+
+  // Latitude and longitude at center of the output image.  We will
+  // set these relative to the spheroid underlying the datum in use
+  // for the projected image.  Yeah, that seems appropriate.
+  double lat_0, lon_0;
+  project_set_input_spheroid (datum_spheroid (datum));
+  double center_x = omd->projection->startX + (omd->projection->perX
+					       * omd->general->line_count / 2);
+  double center_y = (omd->projection->startY
+		     + (omd->projection->perY
+			* omd->general->sample_count / 2));
+  unproject_output (pp, center_x, center_y, ASF_PROJ_NO_HEIGHT, &lat_0, &lon_0,
+		    NULL);
+  omd->general->center_latitude = lat_0;
+  omd->general->center_longitude = lon_0;
+
+  // FIXME: We are ignoring the meta_location fields for now since I'm
+  // not sure whether they are supposed to refer to the corner pixels
+  // or the corners of the data itself.
+
+  if ( lat_0 > 0.0 ) {
+    omd->projection->hem = 'N';
+  }
+  else {
+    omd->projection->hem = 'S';
+  }
+
+  // Convert the projection parameter values back into degrees.
+  to_degrees (projection_type, pp);
+  omd->projection->param = *pp;
+  meta_write (omd, output_meta_file->str);
+  meta_free (omd);
+
   g_assert (0);			/* Unfinished.  */
-  oim = oim; iim = iim;		/* FIXME: remove this compiler reassurance.  */
-  output_image = output_image;	/* FIXME: remove this compiler reassurance.  */
 
   return 0;
 }
