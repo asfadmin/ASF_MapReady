b651bc89d0f0a80400764fbefec5787fbcbfbf0c
bkerin <bkerin@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-05-26 17:59:03 +0000

    Added comment explaining potential wrongess of datum_spheroid wrt data from the outside world.

--- src/asf_meta/datum_spheroid.c
+++ src/asf_meta/datum_spheroid.c
@@ -2,8 +2,16 @@
 
 #include <asf_meta.h>
 
-/* Return the spheroid associated with a given datum.  This function
-   fails if given a datum it hasn't been taught about yet.  */
+/* Return the spheroid generally associated with a given datum.
+   Unfortunately, in the larger world, a given datum isn't really
+   always necessarily associated with a particular spheroid.  For the
+   purposes of the data on the insdie of the ASF tools world, however,
+   we consider the correspondence to be as described in this function.
+   So be sure to test on import if this correspondence is really
+   true!  
+
+   This function fails if given a datum it hasn't been taught about
+   yet.  */
 spheroid_type_t
 datum_spheroid (datum_type_t datum)
 {
