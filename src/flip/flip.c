47ab4aab33552a28014df42214b298a9c219cff0
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-06-06 01:33:26 +0000

    Rename the tool to "flip", and the first argument is "v" or "h" to indicate how we should flip.

--- src/flip/flip.c
+++ src/flip/flip.c
@@ -22,21 +22,45 @@
 #include <spheroids.h>
 #include <asf_contact.h>
 
+void usage()
+{
+    printf("Usage: flip [v|h|vh] <input basename> <output basename>\n");
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
-    printf("Usage: flipv <input basename> <output basename>\n");
-    exit(1);
+  if (argc != 4) {
+    usage();
+  }
+
+  if ( ! (strcmp(argv[1], "v") == 0 ||
+	  strcmp(argv[1], "h") == 0 ||
+	  strcmp(argv[1], "hv") == 0 ||
+	  strcmp(argv[1], "vh") == 0))
+  {
+    usage();
+  }
+
+  int vert = strchr(argv[1], 'v') != NULL;
+  int horz = strchr(argv[1], 'h') != NULL;
+
+  if (!vert && !horz) {
+    usage();
   }
 
   char input_meta_name[256], input_data_name[256];
   char output_meta_name[256], output_data_name[256];
 
-  sprintf(input_meta_name, "%s.meta", argv[1]);
-  sprintf(input_data_name, "%s.img", argv[1]);
+  sprintf(input_meta_name, "%s.meta", argv[2]);
+  sprintf(input_data_name, "%s.img", argv[2]);
 
-  sprintf(output_meta_name, "%s.meta", argv[2]);
-  sprintf(output_data_name, "%s.img", argv[2]);
+  sprintf(output_meta_name, "%s.meta", argv[3]);
+  sprintf(output_data_name, "%s.img", argv[3]);
+
+  printf("Flipping image %s.\n", 
+	 vert && horz ? "vertically and horizontally" :
+	 (vert ? "vertically" : "horizontally"));
 
   printf("Input data file: %s\n", input_data_name);
   printf("Output data file: %s\n", output_data_name);
@@ -50,7 +74,11 @@ int main(int argc, char *argv[])
 			      input_data_name, 0,
 			      FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN);
 
-  float_image_flip_y(input);
+  if (horz)
+    float_image_flip_x(input);
+
+  if (vert)
+    float_image_flip_y(input);
 
   float_image_store(input, output_data_name,
 		    FLOAT_IMAGE_BYTE_ORDER_BIG_ENDIAN);
