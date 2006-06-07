79eae0a888c3274f2b64e9005f2f681300a39aaa
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-06-07 23:27:18 +0000

    Redid parse_cla so that it is just a command-line parser as advertised.  Was able to remove the metadata parameter.

--- src/ardop/parse_cla.c
+++ src/ardop/parse_cla.c
@@ -41,10 +41,6 @@
   {printf("   *****You need %i arguments for the keyword %s.\n\n",currArg-argc+num_args,argv[currArg-1]);return 0;} \
   else currArg+=num_args;
 
-/*Prototypes:*/
-void get_params(char *,struct ARDOP_PARAMS *,meta_parameters **);
-
-
 /******************************************
 Parse_cla:
 	Parses ARDOP command-line options to
@@ -56,7 +52,8 @@ for this scene.
 
 ******************************************/
 
-int parse_cla(int argc,char *argv[],struct ARDOP_PARAMS *g,meta_parameters **meta_out)
+
+int parse_cla(int argc,char *argv[], struct ARDOP_PARAMS *g)
 {
 	int read_offset = 0,   /* Flag - Read resampling offsets from file?   */
 	    read_dopplr = 0;   /* Flag - Read doppler constant from file?     */
@@ -65,51 +62,17 @@ int parse_cla(int argc,char *argv[],struct ARDOP_PARAMS *g,meta_parameters **met
 	char fName_slope[256], /* Input slope,intercept (offsets) file        */
 	   fName_doppler[256]; /* Input doppler constant file                 */
 	FILE  *fp;
-	meta_parameters *meta;
 	
 	/* Preset Optional Command Line Parameters
 	   ---------------------------------------------------------*/
-	g->hamFlag=0;
-	g->kaiFlag=0;
-	g->pwrFlag=0;
-	g->sigmaFlag=0;
-	g->gammaFlag=0;
-	g->betaFlag=0;
-	g->nextend=0.0;
-	g->pctbw=g->pctbwaz=0.0;
-	g->fd=0;
-	g->fdd=-99;   /*Doppler slope of -99 means to figure it out ourselves.*/
-	g->fddd=-99;  /*Doppler slope of -99 means to figure it out ourselves.*/
-	g->ifirstline = 0;  /* First line to process                          */
-	g->npatches = 100;  /* (Absurdly Large) Number of patches             */
-	g->ifirst = 0;      /* i,q byte samples to skip                       */
-	g->isave = 0;       /* start range bin                                */
-	g->na_valid = -99;  /* Valid output samples per patch (-99->determine)*/
-	g->iflag = 1;       /* Debug Flag                                     */
-	g->deskew=0;
-	g->sloper = g->interr = g->slopea = g->intera = 0.0;
-	g->dsloper = g->dinterr = g->dslopea = g->dintera = 0.0;  
-	strcpy(g->CALPRMS,"NO");
-	
+        fill_default_ardop_params(g);
+
 	/* Process the Command Line Arguments
 	   ------------------------------------*/
 	if (argc<3)
 		{printf("Must give input and output filenames.\n");return 0;}
 	strcpy(g->in1,argv[argc-2]);
 	strcpy(g->out,argv[argc-1]);
-
-	
-	/*Create ARDOP_PARAMS struct as well as meta_parameters.*/
-	if (extExists(g->in1,".in"))
-	{/*Read parameters from ARDOP parameter file*/
-		read_params(g->in1,g);
-		if (extExists(g->in1,".meta"))/*Input file has .meta attached: read it*/
-			meta=meta_read(g->in1);
-		else /*No .meta exists--fabricate one*/
-			meta=raw_init();
-	}
-	else    /*Read parameters & .meta from CEOS.*/
-		get_params(g->in1,g,&meta);
 
 	while (currArg < (argc-2)) {
 		char *key=argv[currArg++];
@@ -165,120 +128,6 @@ int parse_cla(int argc,char *argv[],struct ARDOP_PARAMS *g,meta_parameters **met
 		fscanf(fp,"%f %f %f", &g->fd,&g->fdd,&g->fddd);
 		FCLOSE(fp);
 	}
-	
-	if (g->fdd==-99.0)
-	{
-		double old_dop=g->fd;
-	/*Estimate Doppler in scene center.*/
-		estdop(g->in1, 1000, &g->fd,&g->fdd,&g->fddd);
-	/*De-ambiguify doppler based on old value*/
-		while (g->fd-old_dop<-0.5) g->fd+=1.0;
-		while (g->fd-old_dop> 0.5) g->fd-=1.0;
-	}
-
-/*Copy fields from ARDOP_PARAMS struct to meta_parameters struct.*/
-	meta->sar->image_type              = 'S';          /*Slant range image*/
-	meta->sar->look_count              = g->nlooks;
-	meta->sar->deskewed                = g->deskew;
-	meta->sar->range_time_per_pixel    = 1.0/g->fs;
-	meta->sar->azimuth_time_per_pixel  = 1.0/g->prf;
-	meta->sar->slant_shift             = g->slantOff;
-	meta->sar->time_shift              = g->timeOff;
-	meta->sar->slant_range_first_pixel = g->r00;
-	meta->sar->wavelength              = g->wavl;
-	meta->sar->prf                     = g->prf;
-	meta->sar->earth_radius            = g->re;
-	meta->sar->satellite_height        = g->re+g->ht;
-	meta->sar->range_doppler_coefficients[0] = g->fd*g->prf;
-	meta->sar->range_doppler_coefficients[1] = g->fdd*g->prf;
-	meta->sar->range_doppler_coefficients[2] = g->fddd*g->prf;
-	meta->sar->azimuth_doppler_coefficients[0] = g->fd*g->prf;
-	meta->sar->azimuth_doppler_coefficients[1] = 0.0;
-	meta->sar->azimuth_doppler_coefficients[2] = 0.0;
-
-	strcpy (meta->general->system, meta_get_system());
-	meta->general->data_type = REAL32;
-	meta->general->band_number = 0;
-	meta->general->x_pixel_size = meta->sar->range_time_per_pixel
-	                                                   * (speedOfLight/2.0);
-	meta->general->y_pixel_size = meta->sar->azimuth_time_per_pixel
-	                                               * g->vel * (g->re/g->ht);
-
-	*meta_out = meta;
 
 	return 1;
-}
-
-/******************************************************************************
-NAME:	get_params - Creates input file for the ASF SAR processor
-SYNOPSIS: get_params(in)
-DESCRIPTION:
-   Fills all values in ardop_global.h.  The purpose is to automate creation
-   of input parameter files by g->interrogating the CEOS metadata structures.
-
-EXTERNAL ASSOCIATES:
-    get_asf_facdr	 Retrieves ASF facility data record
-    get_ifiledr		 Retrieves image file descriptor record
-    get_dssr		 Retrieves data set summary record
-    jpl_odl_library	 Retrieves information in odl Engineering files
-
-FILE REFERENCES:   in1.dat, in1.trl, in1.ldr	Extraction of metadata	
-
-PROGRAM HISTORY:
-    VERS:   DATE:  AUTHOR:      PURPOSE:
-    --------------------------------------------------------------------------
-    0.0	     7/96  T. Logan 	Automatically Create SAR Processor Input File
-    1.0	    11/96  T. Logan	Create version for SAR Processor (no INSAR)
-    2.0	    1/98  O. Lawlor  Modfied to support RADARSAT
-
-ALGORITHM DESCRIPTION: Fills each of the ASP globals with appropriate values.
-  Parameters are read from CEOS, hardcoded, calculated, or estimated.
-******************************************************************************/
-#include "odl.h"
-
-void get_params(char *file,struct ARDOP_PARAMS *g,meta_parameters **meta_out)
-{
-	meta_parameters *meta;
-	struct VFDRECV asf_facdr1;
-	struct IOF_VFDR ifiledr;
-	struct dataset_sum_rec dssr1;
-
-/* First, we set the hardcoded parameters: */
-	g->rhww = 0.8;             /* Range Spectrum Weight - a little    */
-	g->nlooks = 5;             /* Produces square pixels (5 X 1 look) */
-	g->wavl = 0.0565646;       /* SAR wavelength, in meters.          */
-	g->slope = 4.1913749E+11;  /* Chirp Frequency slope (Hz/Sec)      */
-	g->azres = 8.0;            /* ERS-1 azimuth resolution, in meters */
-	
-/* Get .meta metadata parameters */
-	meta=meta_create(file);
-	
-/* Read CEOS structures */
-	get_asf_facdr(file,&asf_facdr1);
-	get_ifiledr(file,&ifiledr);
-	get_dssr(file,&dssr1);
-
-/* Extract parameters of interest from CEOS metadata */
-        g->nla      = ifiledr.datgroup;                /* number of range bins*/
-        g->re       = asf_facdr1.eradcntr*1000.0;      /* Radius of the Earth */
-        g->ht       = asf_facdr1.scalt*1000.0;         /* Spacecraft Altitude */
-        g->r00      = asf_facdr1.sltrngfp*1000.0;      /* Slant Range 1st Pix */
-        g->prf      = asf_facdr1.prfreq;               /* Pulse Rep. Frequency*/
-        g->fs       = dssr1.rng_samp_rate * 1000000.0; /* Range sampling rate */
-        g->pulsedur = dssr1.rng_length / 1000000.0;    /* Beam pulse length   */
-        g->wavl     = dssr1.wave_length;               /* Beam wavength       */
-
-/*Finally, add the computed parameters:*/
-	
-	/*Compute the spacecraft height from the state vector.*/
-	g->ht=vecMagnitude(meta->state_vectors->vecs[0].vec.pos)-g->re;
-
-	/*Magnitude of s/c orbital velocity (m/sec)*/
-	g->vel=vecMagnitude(meta->state_vectors->vecs[0].vec.vel);
-
-	g->nla-=g->pulsedur*g->fs;/*Subtract the (wasted) pulse length samples*/
-	
-	*meta_out=meta;
-	
-	return;
 }
