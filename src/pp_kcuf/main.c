c52bd253aa4811afda7dadd389e946f53cfab71e
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-06-17 21:36:05 +0000

    Update test driver to match library changes

--- src/pp_kcuf/main.c
+++ src/pp_kcuf/main.c
@@ -3,8 +3,11 @@
 
 main(int argc, char *argv[])
 {
+    double er, atpp;
     printf("Program: pp_kcuf <ASF L1 CEOS .L file>\n\n");
     if (argc<=1) exit(1);
-    printf("PP Earth Radius: %.3f m\n", pp_get_earth_radius(argv[1]));
+    pp_get_corrected_values(argv[1], &er, &atpp);
+    printf("PP Earth Radius: %.3f m\n", er);
+    printf("PP seconds per azimuth line: %.9f s\n", atpp);
     return 0;
 }
