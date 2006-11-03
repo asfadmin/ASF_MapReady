362845e8b2aacda29ef60cf73548df33337e9858
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-11-03 18:05:24 +0000

    Revert to r4115 -- the improvements I made utilizing vnsnprintf's size-0 capability don't work on some older compilers, it seems.

--- src/asf/print_alerts.c
+++ src/asf/print_alerts.c
@@ -7,55 +7,6 @@ DESCRIPTION:
 ******************************************************************************/
 #include "asf.h"
 
-static const int SEVERITY_DEBUG = 0;
-static const int SEVERITY_STATUS = 1;
-static const int SEVERITY_INFO = 2;
-static const int SEVERITY_WARNING = 3;
-static const int SEVERITY_ERROR = 4;
-
-typedef void callback_t(int severity, const char *str, void *params);
-static callback_t *callback_fn = NULL;
-static void *callback_params = NULL;
-void register_asfPrint_callback(callback_t c, void *params)
-{
-    callback_fn = c;
-    callback_params = params;
-}
-
-static void asfPrintImpl(int severity, const char *format, ...)
-{
-  char *buf = NULL;
-  int len;
-
-  va_list ap;
-  va_start(ap, format);
-  len = vsnprintf(buf, 0, format, ap);
-  va_end(ap);
-
-  len += 2; // terminator, plus padding
-  buf = MALLOC(len*sizeof(char));
-
-  va_start(ap, format);
-  vsnprintf(buf, len, format, ap);
-  va_end(ap);
-
-  if (severity > SEVERITY_STATUS || !quietflag) {
-    printf("%s", buf);
-    fflush (stdout);
-  }
-
-  if (logflag) {
-    fprintf(fLog, "%s", buf);
-    fflush (fLog);
-  }
-
-  if (callback_fn) {
-    callback_fn(severity, buf, callback_params);
-  }
-
-  FREE(buf);
-}
-
 /* Do not print to the terminal, only report to the log file */
 void asf_print_to_log_only(const char *format, ...)
 {
@@ -71,90 +22,101 @@ void asf_print_to_log_only(const char *format, ...)
 /* Basically a printf that pays attention to quiet & log flags */
 void asfPrintStatus(const char *format, ...)
 {
-  char *buf = NULL;
-  int len;
   va_list ap;
+  if (!quietflag) {
+    va_start(ap, format);
+    vprintf(format, ap);
+    fflush (stdout);
+    va_end(ap);
+  }
+  if (logflag) {
+    va_start(ap, format);
+    vfprintf(fLog, format, ap);
+    va_end(ap);
 
-  va_start(ap, format);
-  len = 2 + vsnprintf(buf, 0, format, ap);
-  va_end(ap);
-
-  buf = MALLOC(len*sizeof(char));
-  va_start(ap, format);
-  vsnprintf(buf, len, format, ap);
-  va_end(ap);
-
-  asfPrintImpl(SEVERITY_STATUS, "%s", buf);
-  FREE(buf);
+    fflush (fLog);
+  }
 }
 
 /* Basically a printf that pays attention to log flag but NOT the quiet flag */
 void asfForcePrintStatus(const char *format, ...)
 {
-  char *buf = NULL;
-  int len;
   va_list ap;
 
   va_start(ap, format);
-  len = 2 + vsnprintf(buf, 0, format, ap);
+  vprintf(format, ap);
   va_end(ap);
 
-  buf = MALLOC(len*sizeof(char));
-  va_start(ap, format);
-  vsnprintf(buf, len, format, ap);
-  va_end(ap);
+  if (logflag) {
+    va_start(ap, format);
+    vfprintf(fLog, format, ap);
+    va_end(ap);
 
-  asfPrintImpl(SEVERITY_INFO, "%s", buf);
-  FREE(buf);
+    fflush (fLog);
+  }
 }
 
 /* Report warning to user & log file, then continue the program  */
 void asfPrintWarning(const char *format, ...)
 {
-  char *buf = NULL;
-  int len;
   va_list ap;
+  char warningBegin[64];
+  char warningEnd[64];
+
+  sprintf(warningBegin,"\n** Warning: ********\n");
+  sprintf(warningEnd,"** End of warning **\n\n");
+
+  printf(warningBegin);
+  if (logflag) {
+    fprintf(fLog, warningBegin);
+  }
 
   va_start(ap, format);
-  len = 2 + vsnprintf(buf, 0, format, ap);
+  vprintf(format, ap);
   va_end(ap);
 
-  buf = MALLOC(len*sizeof(char));
-  va_start(ap, format);
-  vsnprintf(buf, len, format, ap);
-  va_end(ap);
+  if (logflag) {
+    va_start(ap, format);
+    vfprintf(fLog, format, ap);
+    va_end(ap);
+  }
 
-  static const char *warningBegin = "\n** Warning: ********\n";
-  static const char *warningEnd = "** End of warning **\n\n";
-
-  asfPrintImpl(SEVERITY_WARNING, "%s%s%s", warningBegin, buf, warningEnd);
-  FREE(buf);
+  printf(warningEnd);
+  fflush (stdout);
+  if (logflag) {
+    fprintf(fLog, warningEnd);
+    fflush (fLog);
+  }
 }
 
 
 /* Report to user & logfile, then die  */
 void asfPrintError(const char *format, ...)
 {
-  char *buf = NULL;
-  int len;
   va_list ap;
+  char errorBegin[64];
+  char errorEnd[64];
+
+  sprintf(errorBegin,"\n** Error: ********\n");
+  sprintf(errorEnd,"** End of error **\n\n");
+
+  printf(errorBegin);
+  if (logflag) fprintf(fLog, errorBegin);
 
   va_start(ap, format);
-  len = 2 + vsnprintf(buf, 0, format, ap);
+  vprintf(format, ap);
   va_end(ap);
-
-  buf = MALLOC(len*sizeof(char));
-  va_start(ap, format);
-  vsnprintf(buf, len, format, ap);
-  va_end(ap);
-
-  static const char *errorBegin = "\n** Error: ********\n";
-  static const char *errorEnd = "** End of error **\n\n";
-
-  asfPrintImpl(SEVERITY_ERROR, "%s%s%s", errorBegin, buf, errorEnd);
-  FREE(buf);
 
   if (logflag) {
+    va_start(ap, format);
+    vfprintf(fLog, format, ap);
+    va_end(ap);
+  }
+
+  printf(errorEnd);
+
+  if (logflag) {
+    fprintf(fLog, errorEnd);
     FCLOSE(fLog);
   }
 
