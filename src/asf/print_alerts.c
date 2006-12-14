ad1c76d34ca3fd95140ca1237779930296a4bd3b
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2006-12-14 23:15:58 +0000

    "Stop" button support, taken from the branch.  Needed for the interferometry gui's stop button.

--- src/asf/print_alerts.c
+++ src/asf/print_alerts.c
@@ -7,6 +7,16 @@ DESCRIPTION:
 ******************************************************************************/
 #include "asf.h"
 
+static void check_stop()
+{
+    char stop_file[255];
+    sprintf(stop_file, "%s/stop.txt", get_asf_tmp_dir());
+    if (fileExists(stop_file)) {
+        remove(stop_file);
+        asfPrintError("Processing stopped by user!\n");
+    }
+}
+
 /* Do not print to the terminal, only report to the log file */
 void asf_print_to_log_only(const char *format, ...)
 {
@@ -26,8 +36,9 @@ void asfPrintStatus(const char *format, ...)
   if (!quietflag) {
     va_start(ap, format);
     vprintf(format, ap);
-    fflush (stdout);
     va_end(ap);
+  
+    fflush (stdout);
   }
   if (logflag) {
     va_start(ap, format);
@@ -36,6 +47,8 @@ void asfPrintStatus(const char *format, ...)
 
     fflush (fLog);
   }
+
+  check_stop();
 }
 
 /* Basically a printf that pays attention to log flag but NOT the quiet flag */
@@ -125,4 +138,83 @@ void asfPrintError(const char *format, ...)
   // presumably do it for us.
 
   exit(EXIT_FAILURE);
+}
+
+/******************************************************************************
+ * Report the number of lines processed out of the total number of lines */
+void asfLineMeter(int currentLine, int totalLines)
+{
+  char *null="", *newline="\n", *endline;
+  char *present="ing", *past="ed ", *tense;
+  int blather;
+
+  /* Since C is 0 indexed and totalLines is 1 indexed, add 1 to currentLine*/
+  currentLine++;
+
+  /* Flag to report every 128 lines */
+  blather = ((currentLine%128==0)||(currentLine==1)||(currentLine==totalLines))
+            ? TRUE : FALSE;
+
+  /* Leave if we're not going to blather at the user (or log) */
+  if (!blather) return;
+
+  /* Concoct status message */
+  endline = (currentLine!=totalLines) ? null : newline;
+  tense   = (currentLine!=totalLines) ? present : past;
+  sprintf(logbuf,"Process%s %5d of %5d lines.%s",
+          tense, currentLine, totalLines, endline);
+
+  /* Report to terminal */
+  if (!quietflag) {
+    printf("%c%s",'\r',logbuf);
+    fflush(NULL);
+  }
+
+  /* Report to the log as well */
+  /* Only on the last line */
+  if (logflag && currentLine==totalLines) {
+    sprintf(logbuf,"%s%c",logbuf,'\n');
+    printLog(logbuf);
+  }
+
+  check_stop();
+}
+
+/******************************************************************************
+ * Print the percent thats been completed to stdout */
+void asfPercentMeter(double inPercent)
+{
+  char *null="", *newline="\n", *endline;
+  static int oldPercent=-1;
+  int newPercent;
+  int blather;
+
+  /* Get inPercent to integer form */
+  newPercent = (int)(inPercent * 100.0);
+
+  /* Flag to report every 1% */
+  blather = (newPercent-oldPercent == 1) ? TRUE : FALSE;
+  oldPercent = newPercent;
+
+  /* Quit now if we're not going to blather at the user */
+  if (!blather) return;
+
+  /* Figure status message in correct format & tense */
+  endline = (newPercent!=100) ? null : newline;
+  sprintf(logbuf,"Processed %3d%%%s", newPercent, endline);
+
+  /* Report to terminal */
+  if (!quietflag) {
+    printf("%c%s",'\r',logbuf);
+    fflush(NULL);
+    inPercent++;
+  }
+  /* Report to the log as well */
+  /* Only on the last line */
+  if (logflag && newPercent==100) {
+    sprintf(logbuf,"%s%c",logbuf,'\n');
+    printLog(logbuf);
+  }
+
+  check_stop();
 }
