0493a62a541a1a751462bbb5fae1ef90932b7c07
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-01-31 20:16:36 +0000

    Level of conversion between meta versions no longer needed, look to asf_meta libary for this functionality

--- src/libasf_meta/earth_radius2datum.h
+++
@@ -1,4 +0,0 @@
-/* some temporary functions that translate datum codes into minor and major axes - forth and back */
-
-int earth_radius2datum(double re_major, double re_minor);
-void datum2earth_radius(int datum, double *re_major, double *re_minor);
