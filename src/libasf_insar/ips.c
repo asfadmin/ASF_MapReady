9807696ece13b355a6147d635333caba16bd0faf
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-02-01 03:29:33 +0000

    asf_import() now supports the -band option and the band_id is in the parameter list ...I put a NULL in for the band_id (see below) for now since that will result in the default of importing all available bands, e.g. same behavior as before adding the -band option to asf_import()

--- src/libasf_insar/ips.c
+++ src/libasf_insar/ips.c
@@ -214,7 +214,11 @@ int ips(dem_config *cfg, char *configFile, int createFlag)
 	check_return(1, "master image data file does not exist");
       if (!fileExists("master.000.par")) 
 	check_return(1, "master image metadata file does not exist");
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_master,
+    // FIXME: If we implement the -band option (for import) for 
+    // asf_convert, then the 4th parameter below will need to reflect
+    // this rather than just be NULL.  NULL defaults to importing all
+    // available bands. (Applies to the next 2 calls to asf_import())
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_master,
                               cfg->general->lat_begin, cfg->general->lat_end,
                               NULL, NULL, NULL, NULL, "master", "a"),
                    "ingesting master image (asf_import)");
@@ -222,7 +226,7 @@ int ips(dem_config *cfg, char *configFile, int createFlag)
 	check_return(1, "slave image data file does not exist");
       if (!fileExists("slave.000.par")) 
 	check_return(1, "slave image metadata file does not exist"); 
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_slave,
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_slave,
                               cfg->general->lat_begin, cfg->general->lat_end,
                               NULL, NULL, NULL, NULL, "slave", "b"),
                    "ingesting slave image (asf_import)");
@@ -258,7 +262,11 @@ int ips(dem_config *cfg, char *configFile, int createFlag)
 	check_return(1, "master image data file does not exist");
       if (!fileExists("master.L")) 
 	check_return(1, "master image metadata file does not exist");
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_master,
+    // FIXME: If we implement the -band option (for import) for 
+    // asf_convert, then the 4th parameter below will need to reflect
+    // this rather than just be NULL.  NULL defaults to importing all
+    // available bands.
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_master,
                               cfg->general->lat_begin, cfg->general->lat_end,
                               NULL, NULL, NULL, NULL, "master", "a"),
 		   "ingesting master image (asf_import)");
@@ -266,7 +274,11 @@ int ips(dem_config *cfg, char *configFile, int createFlag)
 	check_return(1, "slave image data file does not exist");
       if (!fileExists("master.L")) 
 	check_return(1, "slave image metadata file does not exist"); 
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_slave,
+    // FIXME: If we implement the -band option (for import) for 
+    // asf_convert, then the 4th parameter below will need to reflect
+    // this rather than just be NULL.  NULL defaults to importing all
+    // available bands.
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_slave,
                               cfg->general->lat_begin, cfg->general->lat_end,
                               NULL, NULL, NULL, NULL, "slave", "b"),
 		   "ingesting slave image (asf_import)");
@@ -697,10 +709,14 @@ int ips(dem_config *cfg, char *configFile, int createFlag)
 	 (vfdr1.datgroup < vfdr2.datgroup) ? vfdr1.datgroup : vfdr2.datgroup;
 	 }
       */
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_master,
+    // FIXME: If we implement the -band option (for import) for 
+    // asf_convert, then the 4th parameter below will need to reflect
+    // this rather than just be NULL.  NULL defaults to importing all
+    // available bands. (Applies to next 2 calls)
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_master,
                               -99.0, -99.0, NULL, NULL, NULL, NULL, "master", "a_cpx"),
 		   "ingesting master image (asf_import)");
-      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, cfg->ingest->prc_slave,
+      check_return(asf_import(r_AMP, FALSE, format, NULL, NULL, NULL, cfg->ingest->prc_slave,
                               -99.0, -99.0, NULL, NULL, NULL, NULL, "slave", "b_cpx"),
  		   "ingesting slave image (asf_import)");
       check_return(c2p("a_cpx", "a"), 
