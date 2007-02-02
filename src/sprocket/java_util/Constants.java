909108dbffebc7c68a8f840635a49d0e5735e53e
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-02-02 17:51:09 +0000

    Java interface and some C programs. Was used by cal/val dept for calibration

--- src/sprocket/java_util/Constants.java
+++
@@ -1,9 +0,0 @@
-
-public class Constants {
-   public static String data_file = ".img";
-   public static String metafile = ".metadata";
-   public static String ground_target_file = ".ground_target";
-   public static String ground_target_file_located = ".ground_target_final";
-   public static String I_data_file = ".dataI";
-   public static String Q_data_file = ".dataQ";
- }
