ae8c17e9c04d4d308c3f5402d1b3fcd35d5b5d33
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-02-13 19:03:20 +0000

    Allow either style of look up table, 3 values per line (Rudi's style), or 4 values per line (Orion).  All lines in the file are assumed to follow the same style as used on the first.
    Require comment lines to start with #, and allow as many as the user
    wants, even intermixed with the data lines.  Using 3 values per line
    style, the dn value is associated with the actual data lines count.

--- src/libasf_raster/look_up_table.c
+++ src/libasf_raster/look_up_table.c
@@ -7,22 +7,22 @@ static void interp(unsigned char *buf, int left, int right)
   int ii;
   float r,g,b;
 
-  r = (float)(buf[right] - buf[left]) / (float)(right-left);
-  g = (float)(buf[right+1] - buf[left+1]) / (float)(right-left);
-  b = (float)(buf[right+2] - buf[left+2]) / (float)(right-left);
+  r = (float)(buf[right*3] - buf[left*3]) / (float)(right-left);
+  g = (float)(buf[right*3+1] - buf[left*3+1]) / (float)(right-left);
+  b = (float)(buf[right*3+2] - buf[left*3+2]) / (float)(right-left);
 
-  for (ii=left+1; ii<right; ii+=3) {
-      buf[ii] = (unsigned char)((r * (ii-left) + 0.5) + buf[ii]);
-      buf[ii+1] = (unsigned char)((g * (ii-left) + 0.5) + buf[ii+1]);
-      buf[ii+2] = (unsigned char)((b * (ii-left) + 0.5) + buf[ii+2]);
+  for (ii=left+1; ii<right; ++ii) {
+      buf[ii*3] = (unsigned char)((r * (ii-left) + 0.5) + buf[left*3]);
+      buf[ii*3+1] = (unsigned char)((g * (ii-left) + 0.5) + buf[left*3+1]);
+      buf[ii*3+2] = (unsigned char)((b * (ii-left) + 0.5) + buf[left*3+2]);
   }
 }
 
-void read_lut(char *lutFile, unsigned char *lut_buffer)
+static void read_lut(char *lutFile, unsigned char *lut_buffer)
 {
   FILE *fp;
   char heading[1024];
-  int l,n,ii;
+  int n,ii;
 
   // fill with zeroes initially
   for (ii=0; ii<768; ii++)
@@ -44,33 +44,86 @@ void read_lut(char *lutFile, unsigned char *lut_buffer)
               fp = fopen(heading, "r");
               if (!fp) {
                   // ok, now give up.
-                  asfPrintError("Couldn't open look up table file: %s\n", lutFile);
+                  asfPrintError("Couldn't open look up table file: %s\n",
+                                lutFile);
               }
           }
       }
   }
 
-  fgets(heading, 1024, fp); // first line is a comment line... required
-  l=1;
-  for (ii=0; ii<768; ii+=3) {
-    int dn, dn_prev=0, red, green, blue;
-    fgets(heading, 1024, fp);
-    ++l; // line number
+  // l: line number
+  // vl: valid line number (counts lines with actual data)
+  int l=0, vl=0, dn, dn_prev=0, style=0;
+  char *p;
 
-    n = sscanf(heading, "%d %d,%d,%d", &dn, &red, &green, &blue);
-    if (n != 4) break;
-    
-    if (dn<0 || dn>255 || red<0 || red>255 || green<0 || green>255 || blue<0 || blue>255) 
-        asfPrintError("read_lut> Illegal values on line %d in %s: %d %d,%d,%d\n",
+  for (ii=0; ii<768; ii+=3) {
+    int red, green, blue;
+
+    p = fgets(heading, 1024, fp);
+    if (!p) break; // eof
+    ++l; // increment line number
+
+    while (heading[0] == '#') {
+        // read more lines if that one was a comment
+        p = fgets(heading, 1024, fp);
+        if (!p) break; // eof
+        ++l;
+    }
+
+    // style==1 --> 4 values per line: dn red,green,blue
+    // style==2 --> 3 values per line: red,green,blue
+    // we don't allow mixing of the styles
+
+    if (!style)
+    {
+        // on the non-comment line, figure out which style of lutFile this is.
+        n = sscanf(heading, "%d %d,%d,%d", &dn, &red, &green, &blue);
+        if (n != 4) {
+            n = sscanf(heading, "%d,%d,%d", &red, &green, &blue);
+            if (n != 3) {
+                asfPrintError("Line %d of lutFile appears invalid:\n%s\n",
+                              l, heading);
+            }
+            dn = 0;
+            style = 2;
+        } else
+            style = 1;
+
+    }
+    else
+    {
+        // subsequent lines must follow first valid line's style
+        if (style == 1) {
+            n = sscanf(heading, "%d %d,%d,%d", &dn, &red, &green, &blue);
+            if (n != 4) break;
+        } else if (style == 2) {
+            n = sscanf(heading, "%d,%d,%d", &red, &green, &blue);
+            if (n != 3) break;
+            dn = vl;
+        } else
+            asfPrintError("read_lut> Confused: lut style==%d\n", style);
+    }
+
+    ++vl; // increment valid data line count
+
+    if (dn<0 || dn>255 || red<0 || red>255 || green<0 || green>255 ||
+        blue<0 || blue>255) 
+    {
+        asfPrintError("read_lut> Illegal values on line %d in %s: "
+                      "%d %d,%d,%d\n",
                       l, lutFile, dn, red, green, blue);
+    }
+
     if (dn_prev>dn)
+    {
         asfPrintError("read_lut> In look up table file: %s\n"
                       "On line %d, index values are not in order: %d -> %d\n",
                       lutFile, l, dn_prev, dn);
+    }
 
-    lut_buffer[dn] = (unsigned char)red;
-    lut_buffer[dn+1] = (unsigned char)green;
-    lut_buffer[dn+2] = (unsigned char)blue;
+    lut_buffer[dn*3] = (unsigned char)red;
+    lut_buffer[dn*3+1] = (unsigned char)green;
+    lut_buffer[dn*3+2] = (unsigned char)blue;
 
     if (dn-dn_prev > 1)
         interp(lut_buffer,dn_prev,dn);
@@ -80,9 +133,9 @@ void read_lut(char *lutFile, unsigned char *lut_buffer)
   FCLOSE(fp);
 
   // FIXME: remove this debug junk
-  printf("Read: %s\n", lutFile);
-  for (ii=0; ii<768; ii+=3)
-      printf("%d: %d,%d,%d\n", ii/3, lut_buffer[ii], lut_buffer[ii+1], lut_buffer[ii+2]);
+  //printf("Read: %s\n", lutFile);
+  //for (ii=0; ii<768; ii+=3)
+  //    printf("%d: %d,%d,%d\n", ii/3, lut_buffer[ii], lut_buffer[ii+1], lut_buffer[ii+2]);
 }
 
 void apply_look_up_table(char *lutFile, unsigned char *in_buffer,
@@ -115,7 +168,4 @@ void apply_look_up_table(char *lutFile, unsigned char *in_buffer,
     rgb_buffer[(ii*3)+1] = lut_buffer[in_buffer[ii]*3+1];
     rgb_buffer[(ii*3)+2] = lut_buffer[in_buffer[ii]*3+2];
   }
-
-  // Clean up
-  FREE(lut_buffer);
 }
