4a32b21fc48e88cc5ace630c71c7f66d1201a7f2
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-03-26 17:24:42 +0000

    Allow .jpeg when looking for the output thumbs.

--- src/asf_convert_gui/execute.c
+++ src/asf_convert_gui/execute.c
@@ -351,7 +351,9 @@ static void set_thumbnail(GtkTreeIter *iter, const gchar * tmp_dir,
         sprintf(thumbnail_name, "%s/%s_thumb.jpg", tmp_dir, basename);
 
         if (!fileExists(thumbnail_name)) {
-            if (strcmp(findExt(out_full), ".jpg") == 0) {
+            if (strcmp_case(findExt(out_full), ".jpg") == 0 ||
+                strcmp_case(findExt(out_full), ".jpeg") == 0)
+            {
                 scaling_required = TRUE;
                 strcpy(thumbnail_name, out_full);
             }
