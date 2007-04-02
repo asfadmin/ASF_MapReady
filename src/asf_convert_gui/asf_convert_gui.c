23409156e8d255136bb04dd4befa2f96dfadcdb5
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-04-02 21:55:10 +0000

    Print out share dir.

--- src/asf_convert_gui/asf_convert_gui.c
+++ src/asf_convert_gui/asf_convert_gui.c
@@ -23,6 +23,7 @@ main(int argc, char **argv)
     set_font();
 
     glade_xml_file = (gchar *)find_in_share("asf_convert_gui.glade");
+    asfPrintStatus("Found share dir: %s\n", get_asf_share_dir());
     glade_xml = glade_xml_new(glade_xml_file, NULL, NULL);
 
     g_free(glade_xml_file);
