97f3859b26730c5085edb437452f24c4d01bb384
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-04-19 00:25:42 +0000

    Change the wording on the Geotiff filter for DEMs

--- src/asf_convert_gui/terrcorr.c
+++ src/asf_convert_gui/terrcorr.c
@@ -230,7 +230,7 @@ static void create_file_chooser_dialogs()
                                     img_filt);
 
         GtkFileFilter *tif_filt = gtk_file_filter_new();
-        gtk_file_filter_set_name(tif_filt, "GeoTIFF Files (*.tif)");
+        gtk_file_filter_set_name(tif_filt, "GeoTIFF DEMs (*.tif)");
         gtk_file_filter_add_pattern(tif_filt, "*.tif");
         gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(dem_browse_widget),
                                     tif_filt);
