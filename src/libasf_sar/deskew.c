d0b57f1a52e210f59fd5fac03dbc93e2ef0902f0
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-04-24 08:21:16 +0000

    Fix some small bugs, clean up a little.

--- src/libasf_sar/deskew.c
+++ src/libasf_sar/deskew.c
@@ -7,17 +7,24 @@
 // This is the guts of the "deskew" tool, moved here in to this
 // library so we can call it from asf_terrcorr.
 
-// Deskew uses the squint angle of an image along with the
-// look angle to determine the amount of parallelogram shift
-// skew that has been introduced in an image due to the doppler
-// centroid choosen during image processing.  It then remaps
-// the image (using bi-linear interpolation) to remove this skew.
+// Comments from the original deskew code ==>
+
+//   Deskew uses the squint angle of an image along with the
+//   look angle to determine the amount of parallelogram shift
+//   skew that has been introduced in an image due to the doppler
+//   centroid choosen during image processing.  It then remaps
+//   the image (using bi-linear interpolation) to remove this skew.
+
+// However, it doesn't look to me like bi-linear interpolation is
+// taking place.  This probably a good thing, actually...
 
 // Giving the same input & output filename is allowed, the
 // input data will be overwritten.
 
 // This code should actually work on data that is already deskewed,
 // no changes are made, output matches input
+
+static int iabs(int x) { return x>0 ? x : -x; }
 
 void deskew(const char *infile, const char *outfile)
 {
@@ -27,13 +34,14 @@ void deskew(const char *infile, const char *outfile)
   int np = meta->general->sample_count;
   int nb = meta->general->band_count;
   char **band_name = extract_band_names(meta->general->bands, nb);
+  int b, i, j, d=meta->sar->deskewed != 0;
 
   if (!meta->sar)
     asfPrintError("Cannot deskew data without a sar block!\n");
 
   char *tmp_outfile;
   int do_rename = FALSE;
-  if (strcmp(infile, outfile) == 0 && nb>0) {
+  if (strcmp(infile, outfile) == 0 && nb>1) {
     // user wants to deskew in-place
     // we can't actually do that on multi-band data, too much to keep in memory
     // use a temporary file, then clobber input file
@@ -53,19 +61,18 @@ void deskew(const char *infile, const char *outfile)
   double time, slant, dop;
   meta_get_timeSlantDop(meta, 0, 0, &time, &slant, &dop);
 
-  double look, yaw;
+  double look, yaw, fac;
   getLookYaw(g, slant, dop, &look, &yaw);
+  fac = sin(look)*sin(yaw);
   free_geolocate(g);
-
-  double fac = sin(look)*sin(yaw);
-
-  int b, i, j, d=meta->sar->deskewed != 0;
 
   // the "lower" array stores the required shifts, indexed by column
   // (the amount of shift is row-independent)
   int *lower = MALLOC(np * sizeof(int));
   for (i=0; i<np; ++i)
-    lower[i] = (int) (fac*(float)i);
+    lower[i] = (int) (fac*(double)i);
+
+  asfPrintStatus("Maximum shift: %d pixels.\n", iabs(lower[np-1]));
 
   float *ibuf = MALLOC(np * sizeof(float));
   float *obuf = CALLOC(np*nl, sizeof(float));
@@ -73,7 +80,7 @@ void deskew(const char *infile, const char *outfile)
   FILE *fpi = fopenImage(infile, "rb");
 
   for (b=0; b<nb; ++b) {
-    if (nb>0)
+    if (nb>1)
       asfPrintStatus("Deskewing band: %s\n", band_name[b]);
 
     // apply deskewing to this band
@@ -101,13 +108,12 @@ void deskew(const char *infile, const char *outfile)
   FREE(lower);
 
   // if we output to a temporary file, now clobber the input
-  if (do_rename) {
+  if (do_rename)
     rename(tmp_outfile, outfile);
-  }
-
   FREE(tmp_outfile);
 
   // only need to update the deskewed flag in the metadata
   meta->sar->deskewed = 1;
   meta_write(meta, outfile);
+  meta_free(meta);
 }
