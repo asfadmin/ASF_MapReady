1759d52bbcab819976f87623224f082a3f8a12f5
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-06-29 00:57:51 +0000

    print_proj_info should support scansar.

--- src/libasf_geocode/asf_geocode.c
+++ src/libasf_geocode/asf_geocode.c
@@ -88,6 +88,11 @@ char *proj_info_as_string(projection_type_t projection_type,
             pp->lamaz.center_lat, pp->lamaz.center_lon);
         break;
 
+    case SCANSAR_PROJECTION:
+        sprintf(ret,
+            "Projection: ScanSAR\n");
+        break;
+
     case LAT_LONG_PSEUDO_PROJECTION:
         sprintf(ret,
             "Projection: Lat/Lon (pseudoprojection)\n");
