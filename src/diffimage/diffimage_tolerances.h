74c9c24006fa48e37765a5b1502c35f361f64e27
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-07-25 06:32:04 +0000

    Temporary loosening up of the tolerance for standard deviation comparisons.  We ought to change to the Wilcoxon test and stop making the assumption that the data distribution is Gaussian and that 6-sigma is fully-contained within the available range.

--- src/diffimage/diffimage_tolerances.h
+++ src/diffimage/diffimage_tolerances.h
@@ -5,7 +5,7 @@
 #define MIN_DIFF_TOL 0.5 // Max allowed difference, expressed as percent of 6-sigma range of baseline data
 #define MAX_DIFF_TOL 0.5
 #define MEAN_DIFF_TOL 0.5
-#define SDEV_DIFF_TOL 0.5
+#define SDEV_DIFF_TOL 0.7 // Should be .5%, but not so good for non-Gaussian distributions (FIXME: Change to Wilcoxon test)
 // #define RMSE_DIFF_TOL 0.5 // Max allowed difference, in percent of baseline rmse, between internal data rmse's
 #define BYTE_PSNR_TOL -16.0       // Minimum PSNRs v. data type
 #define INTEGER16_PSNR_TOL -5.46
