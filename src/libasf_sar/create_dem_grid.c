ad14bc628afea13f380819aa3b272aa028a426a2
rgens <rgens@50fedc65-a7ff-0310-b841-aa85ec389475>
2007-10-23 12:56:33 +0000

    The solution was already in the code. Just commented out. Seems like we ran into this before but preferred a different way of dealing with it. Oh well ...

--- src/libasf_sar/create_dem_grid.c
+++ src/libasf_sar/create_dem_grid.c
@@ -172,9 +172,9 @@ int create_dem_grid_ext(const char *demName, const char *sarName,
 		     &demProj_z);
       /*Compute the line,sample coordinates of this location in the DEM.*/
       dem_x = (demProj_x - metaDem->projection->startX) /
-          metaDem->projection->perX; //- metaDem->general->start_sample;
+          metaDem->projection->perX - metaDem->general->start_sample;
       dem_y = (demProj_y - metaDem->projection->startY) /
-          metaDem->projection->perY; //- metaDem->general->start_line;
+          metaDem->projection->perY - metaDem->general->start_line;
 
       if (dem_x > 0 && dem_y > 0 &&
 	  dem_x < metaDem->general->sample_count &&
