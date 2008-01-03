75c2a6cc890b4593e95b688150b0e1f0b4ed7d60
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-01-03 06:44:19 +0000

    MapReady merges

--- src/deskew/deskew.c
+++ src/deskew/deskew.c
@@ -5,13 +5,13 @@ SYNOPSIS:  deskew <infile> <outfile>
 
 DESCRIPTION:
 
-	Deskew uses the squint angle of an image along with the
+    Deskew uses the squint angle of an image along with the
         look angle to determine the amount of parallelogram shift
         skew that has been introduced in an image due to the doppler
         centroid choosen during image processing.  It then remaps
         the image (using bi-linear interpolation) to remove this
         skew.
- 
+
 
 EXTERNAL ASSOCIATES:
     NAME:               USAGE:
@@ -24,7 +24,7 @@ FILE REFERENCES:
 PROGRAM HISTORY:
     VERS:   DATE:  AUTHOR:      PURPOSE:
     ---------------------------------------------------------------
-    1.0		   Tom Logan
+    1.0        Tom Logan
 
 HARDWARE/SOFTWARE LIMITATIONS:
 
@@ -47,33 +47,31 @@ BUGS:
 *                                                                             *
 *       For more information contact us at:                                   *
 *                                                                             *
-*	Alaska Satellite Facility	    	                              *
-*	Geophysical Institute			www.asf.alaska.edu            *
-*       University of Alaska Fairbanks		uso@asf.alaska.edu	      *
-*	P.O. Box 757320							      *
-*	Fairbanks, AK 99775-7320					      *
-*									      *
+*   Alaska Satellite Facility                                         *
+*   Geophysical Institute           www.asf.alaska.edu            *
+*       University of Alaska Fairbanks      uso@asf.alaska.edu        *
+*   P.O. Box 757320                               *
+*   Fairbanks, AK 99775-7320                          *
+*                                         *
 ******************************************************************************/
 
 #include "asf.h"
 #include "asf_sar.h"
-
-#define VERSION 1.5
+#include "asf_license.h"
+#include "deskew_help.h"
 
 int main(int argc,char *argv[])
 {
   char infile[255], outfile[255];
-  
-  if (argc != 3)
-    { 
-      printf("\nUsage: %s <infile> <outfile>\n",argv[0]);
-      printf("       <infile> 	input file name (.img,.meta)\n");
-      printf("       <outfile>  output file name (.img,.meta)\n");
-      printf("\n"
-	     "Removes doppler skew from image.\n");
-      printf("Version %.2f, ASF SAR TOOLS\n\n",VERSION);
+
+  if (argc > 1) {
+      check_for_help(argc, argv);
+      handle_license_and_version_args(argc, argv, TOOL_NAME);
+  }
+  if (argc != 3) {
+      usage();
       exit(1);
-    }
+  }
 
   create_name(infile, argv[1], ".img");
   create_name(outfile, argv[2], ".img");
