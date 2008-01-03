4c60a93cb8ac9eb77dfd8b145cf8730d0875e798
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-01-03 06:44:35 +0000

    MapReady merges

---
+++ src/extract_rgb_palette/extract_rgb_palette_help.h
@@ -0,0 +1,91 @@
+#ifndef _EXTRACT_RGB_PALETTE_HELP_H_
+#define _EXTRACT_RGB_PALETTE_HELP_H_
+
+// NOTES:
+//   1. Don't put a '\n' at the end of the string definitions.  Formatting
+//      is handled by usage() and print_help()
+//   2. To match the formatting found in the other ASF include files and tool
+//      output, use the examples below, i.e. a) paragraphs are 3 spaces from
+//      the left, b) second line of options starts 13 spaces from the left,
+//      c) option descriptions start 8 spaces from the left, etc.
+//   3. License, version, and contact info are defined elsewhere (see asf_version.h,
+//      svn_rev.h, asf_contact.h, and asf_license.h.  These files are included by
+//      help.c
+//
+
+// TOOL_NAME is required
+#ifdef  TOOL_NAME
+#undef  TOOL_NAME
+#endif
+#define TOOL_NAME \
+        "extract_rgb_palette"
+
+// TOOL_USAGE is required
+#ifdef  TOOL_USAGE
+#undef  TOOL_USAGE
+#endif
+#define TOOL_USAGE \
+        TOOL_NAME" <infile> [-help]"
+
+// TOOL_DESCRIPTION is required
+#ifdef  TOOL_DESCRIPTION
+#undef  TOOL_DESCRIPTION
+#endif
+#define TOOL_DESCRIPTION \
+    "   The extract_rgb_palette tool scans a graphics file image for a palette associated\n" \
+    "   with indexed RGB color mapping and dumps this palette to stdout."
+
+// TOOL_INPUT is required but is allowed to be an empty string
+#ifdef  TOOL_INPUT
+#undef  TOOL_INPUT
+#endif
+#define TOOL_INPUT \
+        "   <infile>  The name of the input data file."
+
+// TOOL_OUTPUT is required but is allowed to be an empty string
+#ifdef  TOOL_OUTPUT
+#undef  TOOL_OUTPUT
+#endif
+#define TOOL_OUTPUT \
+    "   Output is to stdout (screen) only.  Format:\n\n" \
+    "   <pixelvalue> <red component> <green component> <blue component>\n" \
+    "     ...etc\n\n" \
+    "   Pixel value range varies by data type contained in the file.  RGB components\n" \
+    "   range from 0 to 255."
+
+// TOOL_OPTIONS is required but is allowed to be an empty string
+#ifdef  TOOL_OPTIONS
+#undef  TOOL_OPTIONS
+#endif
+#define TOOL_OPTIONS \
+    "   -help\n" \
+    "        Print this help information and then exit."
+
+// TOOL_EXAMPLES is required but is allowed to be an empty string
+#ifdef  TOOL_EXAMPLES
+#undef  TOOL_EXAMPLES
+#endif
+#define TOOL_EXAMPLES \
+    ""
+
+// TOOL_LIMITATIONS is required but is allowed to be an empty string
+#ifdef  TOOL_LIMITATIONS
+#undef  TOOL_LIMITATIONS
+#endif
+#define TOOL_LIMITATIONS \
+    "  Only TIFF and GeoTIFF files are currently supported."
+
+// TOOL_SEE_ALSO is required but is allowed to be an empty string
+#ifdef  TOOL_SEE_ALSO
+#undef  TOOL_SEE_ALSO
+#endif
+#define TOOL_SEE_ALSO \
+    ""
+
+// Prototypes
+void check_for_help(int argc, char* argv[]);
+void usage();
+void print_help();
+
+#endif // _EXTRACT_RGB_PALETTE_HELP_H_
+
