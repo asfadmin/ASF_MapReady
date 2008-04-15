2a55409424fcba71198ec944f88e6463f02cf118
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-04-15 02:36:30 +0000

    Initialize the new general->radiometry value to r_AMP, as the radiometry enum doesn't have an "unknown" value (don't really think it needs one, this isn't a critical value).

--- src/asf_meta/meta_init.c
+++ src/asf_meta/meta_init.c
@@ -43,6 +43,7 @@ meta_general *meta_general_init(void)
   strcpy(general->processor, MAGIC_UNSET_STRING);
   general->data_type = MAGIC_UNSET_INT;
   general->image_data_type = MAGIC_UNSET_INT;
+  general->radiometry = r_AMP;
   strcpy(general->system, MAGIC_UNSET_STRING);
   strcpy(general->acquisition_date, MAGIC_UNSET_STRING);
   general->orbit = MAGIC_UNSET_INT;
