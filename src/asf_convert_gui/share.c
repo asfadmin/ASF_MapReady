a82f677622613d1c489ef6d373dd7cc49f06ad6c
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-05-16 03:30:28 +0000

    Call the new get_asf_bin_dir_win(), which uses CreateProcess() on windows (instead of fork()), which is faster and less cygwiny.

--- src/asf_convert_gui/share.c
+++ src/asf_convert_gui/share.c
@@ -14,7 +14,7 @@ char *
 find_in_bin(const char * filename)
 {
     char * ret = (char *) malloc(sizeof(char) *
-        (strlen(get_asf_bin_dir()) + strlen(filename) + 5));
+        (strlen(get_asf_bin_dir_win()) + strlen(filename) + 5));
     sprintf(ret, "%s%c%s", get_asf_bin_dir(), DIR_SEPARATOR, filename);
     return ret;
 }
