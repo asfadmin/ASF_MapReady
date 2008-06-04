53ebb05f0fffffb668f77e70532dcfb108b5743b
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-06-04 03:12:23 +0000

    When using asfSystem(), need to call get_asf_bin_dir_win(), now. Fixes mantis bug 860.

--- src/asf_view/meta.c
+++ src/asf_view/meta.c
@@ -173,8 +173,8 @@ static char *
 find_in_bin(const char * filename)
 {
     char * ret = (char *) malloc(sizeof(char) *
-        (strlen(get_asf_bin_dir()) + strlen(filename) + 5));
-    sprintf(ret, "%s%c%s", get_asf_bin_dir(), DIR_SEPARATOR, filename);
+        (strlen(get_asf_bin_dir_win()) + strlen(filename) + 5));
+    sprintf(ret, "%s%c%s", get_asf_bin_dir_win(), DIR_SEPARATOR, filename);
     return ret;
 }
 
