891c755baa0d7cf3018c55f6f4f4a7effeae6ba1
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-09-19 03:24:21 +0000

    update build system to build the JPL mosaic suite of tools and ready it to deal with RPM builds

---
+++ contrib/jplmosaicsuite/Makerules.mk
@@ -0,0 +1,15 @@
+# rules.mk
+# Universal rules for makefiles one directory deeper
+
+include ../../../make_support/system_rules
+
+BINDIR = ../../../bin
+
+FC     = g77
+FFLAGS = $(OPTFLAG) $(HP_SGI) $(LARGEFILE_FLAGS) -ffixed-line-length-132
+#FFLAGS = $(OPTFLAG) $(LARGEFILE_FLAGS) \
+#         -fno-globals -Wno-globals -ffixed-line-length-132
+
+CC     = gcc
+CFLAGS = $(OPTFLAG) $(LARGEFILE_FLAGS)
+X_LIBS   = -L/usr/X11R6/lib -lXm -lXt -lX11 -lm
