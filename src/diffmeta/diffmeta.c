9728fe1e80ab6f1b3e8fa1fbc8daa2167e7c5b33
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-11-04 02:27:21 +0000

    Move incidence angle transform to sar block comparison (Was in transform block still...)

--- src/diffmeta/diffmeta.c
+++ src/diffmeta/diffmeta.c
@@ -2264,6 +2264,47 @@ void diff_check_metadata(char *outputFile, int is_not_a_geotiff, char *metafile1
                         msar1->azimuth_doppler_coefficients[2],
                         msar2->azimuth_doppler_coefficients[2],
                         DM_DOP_AZIMUTH_QUAD_TOL, &failed);
+    float incid_a1 = MAGIC_UNSET_DOUBLE;
+    float incid_a2 = MAGIC_UNSET_DOUBLE;
+    if (msar1->incid_a != NULL && msar2->incid_a != NULL) {
+        char block_id[256] = "";
+        char var_name[256] = "";
+        int i;
+        for (i=0; i<6; i++) {
+            //        incid_a1 = mver1 >= 2.8 ? msar1->incid_a[i] : mtrans1->incid_a[i];
+            //        incid_a2 = mver2 >= 2.8 ? msar2->incid_a[i] : mtrans2->incid_a[i];;
+            //        sprintf(block_id, "File 1 %s block, File 2 %s block - incid_a(%d)",
+            //                mver1 >= 2.8 ? "Sar" : "Transform",
+            //                mver2 >= 2.8 ? "Sar" : "Transform",
+            //                i);
+            incid_a1 = msar1->incid_a[i];
+            incid_a2 = msar2->incid_a[i];
+            strcpy(block_id, "Sar");
+            sprintf(var_name, "incid_a[%d]", i);
+            if (meta_is_valid_double(incid_a1) &&
+                meta_is_valid_double(incid_a2))
+            {
+                compare_meta_double_with_tolerance(compare_err_msgs, block_id, var_name,
+                                                incid_a1, incid_a2,
+                                                (i == 0) ? DM_INCID_A0_TOL :
+                                                (i == 1) ? DM_INCID_A1_TOL :
+                                                (i == 2) ? DM_INCID_A2_TOL :
+                                                (i == 3) ? DM_INCID_A3_TOL :
+                                                (i == 4) ? DM_INCID_A4_TOL :
+                                                (i == 5) ? DM_INCID_A5_TOL :
+                                                        0.0001,
+                                                        &failed);
+            }
+            else if (( meta_is_valid_double(incid_a1) && !meta_is_valid_double(incid_a2)) ||
+                    (!meta_is_valid_double(incid_a1) &&  meta_is_valid_double(incid_a2)))
+            {
+                sprintf(&compare_err_msgs[strlen(compare_err_msgs)],
+                        "  [%s] Baseline and new version %s are different:\n    %f\n    %f\n\n",
+                        block_id, var_name, incid_a1, incid_a2);
+                failed = 1;
+            }
+        }
+    }
   }
   ////////////////////////////////////////////////////////////
   // SAR Block Reporting
@@ -2431,42 +2472,6 @@ void diff_check_metadata(char *outputFile, int is_not_a_geotiff, char *metafile1
     }
     // Ignore comparing origin_lat (appears to not be used)
     // Ignore comparing origin_lon (appears to not be used)
-    float incid_a1 = MAGIC_UNSET_DOUBLE;
-    float incid_a2 = MAGIC_UNSET_DOUBLE;
-    for (i=0; i<6; i++) {
-//        incid_a1 = mver1 >= 2.8 ? msar1->incid_a[i] : mtrans1->incid_a[i];
-//        incid_a2 = mver2 >= 2.8 ? msar2->incid_a[i] : mtrans2->incid_a[i];;
-//        sprintf(block_id, "File 1 %s block, File 2 %s block - incid_a(%d)",
-//                mver1 >= 2.8 ? "Sar" : "Transform",
-//                mver2 >= 2.8 ? "Sar" : "Transform",
-//                i);
-        incid_a1 = msar1->incid_a[i];
-        incid_a2 = msar2->incid_a[i];
-        strcpy(block_id, "Sar");
-        sprintf(var_name, "incid_a[%d]", i);
-        if (meta_is_valid_double(incid_a1) &&
-            meta_is_valid_double(incid_a2))
-        {
-            compare_meta_double_with_tolerance(compare_err_msgs, block_id, var_name,
-                                               incid_a1, incid_a2,
-                                               (i == 0) ? DM_INCID_A0_TOL :
-                                               (i == 1) ? DM_INCID_A1_TOL :
-                                               (i == 2) ? DM_INCID_A2_TOL :
-                                               (i == 3) ? DM_INCID_A3_TOL :
-                                               (i == 4) ? DM_INCID_A4_TOL :
-                                               (i == 5) ? DM_INCID_A5_TOL :
-                                               0.0001,
-                                               &failed);
-        }
-        else if (( meta_is_valid_double(incid_a1) && !meta_is_valid_double(incid_a2)) ||
-                 (!meta_is_valid_double(incid_a1) &&  meta_is_valid_double(incid_a2)))
-        {
-            sprintf(&compare_err_msgs[strlen(compare_err_msgs)],
-                    "  [%s] Baseline and new version %s are different:\n    %f\n    %f\n\n",
-                    block_id, var_name, incid_a1, incid_a2);
-            failed = 1;
-        }
-    }
     for (i=0; i<10; i++) {
         sprintf(block_id, "Transform - map_a(%d)", i);
         sprintf(var_name, "map2ls_a[%d]", i);
