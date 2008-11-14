902daa2f3c9d81afafbdf78f8184b87b9a56489a
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-11-14 04:33:04 +0000

    Update help output

--- src/fftMatch/fftMatch.c
+++ src/fftMatch/fftMatch.c
@@ -5,7 +5,7 @@ SYNOPSIS: fftMatch [-m <matchFile> [-c <corrImg.ext>]] [-log <file>] [-quiet]
 			<img1.ext> <img2.ext>
 
 DESCRIPTION:
-	
+
 	fftMatch lines up two images, to slightly better the
 	single-pixel precision.  It will work with images of any
 	size, but is most efficient when the image dimensions are
@@ -46,7 +46,7 @@ ALGORITHM DESCRIPTION:
 
 ALGORITHM REFERENCES:
 
-BUGS:	
+BUGS:
 	The images must have at least 75% overlap.
         fftMatch will occasionally fail to find the
         correct offset (especially with noisy images).
@@ -97,7 +97,7 @@ int main(int argc,char **argv)
 
   fLog=NULL;
   logflag=quietflag=0;
-  
+
   /* process command line */
   /* q has a : because we want to have it be -quiet (: = uiet) */
   while ((c=getopt(argc,argv,"m:c:l:q:")) != EOF)
@@ -115,9 +115,9 @@ int main(int argc,char **argv)
 	    if (fLog) FCLOSE(fLog);
 	    usage(argv[0]);
 	  }
-	break;			
+	break;
       case 'l':/* -log <filename>, get logfile; this is sorta hacked */
-	if (0==strncmp(optarg,"og",2)) 
+	if (0==strncmp(optarg,"og",2))
 	  {
 	    sscanf(argv[optind++], "%s", logFile);
 	    logflag=1;
@@ -126,17 +126,17 @@ int main(int argc,char **argv)
 	else usage(argv[0]);
 	break;
       case 'q':/* -quiet flag; this is also hacked */
-	if (0==strncmp(optarg,"uiet",4)) 
+	if (0==strncmp(optarg,"uiet",4))
 	  quietflag=1;
 	else usage(argv[0]);
 	break;
       default:
 	if (fLog) FCLOSE(fLog);
 	usage(argv[0]);
-	break;	
+	break;
       }
     }
-  
+
   if ((argc-optind) != 2) {
     if ((argc-optind) > 2) printf("\nToo many inputs.\n");
     if ((argc-optind) < 2) printf("\nToo few inputs.\n");
@@ -147,7 +147,7 @@ int main(int argc,char **argv)
     inFile1=argv[optind];
     inFile2=argv[optind+1];
   }
-  
+
   fftMatch(inFile1, inFile2, corrFile, &bestLocX, &bestLocY, &certainty);
 
   if (descFile) {
@@ -164,7 +164,7 @@ void usage(char *name)
 {
   printf("\nUSAGE:\n"
 	 "   %s [-m <matchFile> [-c <corrImg.ext>]] [-log <file>] [-quiet]\n"
-	 "            <img1.ext> <img2.ext>\n"
+	 "            <master basename> <slave basename>\n"
 	 "\nOPTIONS:\n"
 	 "   -m <matchFile>:   an output file, listing how much image 2\n"
 	 "                       should be shifted to line up with image 1:\n"
@@ -175,14 +175,15 @@ void usage(char *name)
 	 "   -log <file>:      allows the output to be written to a log file\n"
 	 "   -quiet:           suppresses the output to the essentials\n"
 	 "\nINPUTS:\n"
-	 "   <img1.ext>:  any single-banded LAS image.\n"
-	 "   <img2.ext>:  an image to line up with the first.\n"
-	 "                  image 2 should not be bigger than image 1\n"
+	 "   <master basename>: basename of any image in ASF Internal format (.img, .meta)\n"
+	 "   <slave basename>:  an image to line up with the first.\n"
+	 "                      The slave image should not be bigger than the master image\n"
 	 "\nDESCRIPTION:\n"
-	 "   fftMatch lines up two images, to slightly better the\n"
-	 "   single-pixel precision.  It will work with images of any\n"
+	 "   fftMatch lines up the 'slave' image to the 'master' image to better\n"
+	 "   than single-pixel precision.  It will work with images of any\n"
 	 "   size, but is most efficient when the image dimensions are\n"
-	 "   near a power of 2.  The images need not be square.\n"
-	 "\nVersion %.2f, ASF SAR TOOLS\n\n",name,VERSION);
+	 "   near a power of 2.  The images need not be square.  If the two images\n"
+     "   have more than one band, only the first band from each image aligned.\n"
+	 "\nVersion %.2f, ASF MAPREADY TOOLKIT\n\n",name,VERSION);
   exit(1);
 }
