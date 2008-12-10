62db9eb82a8afa945398e423c0faff3f0e97e33e
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2008-12-10 04:29:04 +0000

    New version for create_thumbs (in trunk)

--- include/asf_version.h
+++ include/asf_version.h
@@ -27,7 +27,7 @@
 #define JPL_VERSION 1.0
 
 // Create_thumbs
-#define CREATE_THUMBS_VERSION_STRING "2.0.1"
+#define CREATE_THUMBS_VERSION_STRING "2.0.3-dev"
 #define CREATE_THUMBS_VERSION 2.0
 
 // Data_qc
