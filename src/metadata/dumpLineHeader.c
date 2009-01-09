d98aee4a222c7ab3ba1f414152b30c23b75ac0e7
bdixon <bdixon@50fedc65-a7ff-0310-b841-aa85ec389475>
2009-01-09 05:23:47 +0000

    Merge removal of firstRecordLen() from dumpLineHeader.c since it duplicates (exactly) the function of the same name in asf_meta.a

--- src/metadata/dumpLineHeader.c
+++ src/metadata/dumpLineHeader.c
@@ -78,17 +78,6 @@ int strmatches(const char *key, ...)
     return found;
 }
 
-int firstRecordLen(const char *ceosName)
-{
-  FILE *f;
-  struct HEADER h;
-
-  f=FOPEN(ceosName,"rb");    /*Open file.*/
-  FREAD(&h,1,12,f);          /*Read first CEOS header.*/
-  FCLOSE(f);                 /*Close file*/
-  return bigInt32(h.recsiz); /*put recsiz in proper endian format & return*/
-}
-
 void dumpLineHeader(const char *inName, const char *template, int line_number)
 {
   struct HEADER hdr;
