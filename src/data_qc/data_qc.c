2ca8ea13e416894ee7216d9e58f237454d0cc912
pdenny <pdenny@50fedc65-a7ff-0310-b841-aa85ec389475>
2009-01-23 03:29:12 +0000

    Remove firstRecordLen function as it was an exact duplicate of what is in the the asf_meta library

--- src/data_qc/data_qc.c
+++ src/data_qc/data_qc.c
@@ -149,17 +149,6 @@ typedef enum {
   CEOS_TFDR=193         // Trailer File Descriptor Record - ALOS
 } CEOS_RECORD_TYPE;
 
-int firstRecordLen(const char *ceosName)
-{
-  FILE *f;
-  struct HEADER h;
-
-  f=FOPEN(ceosName,"rb");    /*Open file.*/
-  FREAD(&h,1,12,f);          /*Read first CEOS header.*/
-  FCLOSE(f);                 /*Close file*/
-  return bigInt32(h.recsiz); /*put recsiz in proper endian format & return*/
-}
-
 char *strip_alos_prep(const char *name)
 {
   char *prep = (char *) MALLOC(sizeof(char)*25);
