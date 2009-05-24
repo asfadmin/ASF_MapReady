a9cab6271ed88b40f827e5ae1a3344bfd2a72bc7
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2009-05-24 00:05:02 +0000

    Go to 2.3.0-dev of MapReady in the trunk.

--- include/asf_version.h
+++ include/asf_version.h
@@ -15,11 +15,11 @@
 #define REQ_VERSION 2.0
 
 // MapReady
-#define MAPREADY_VERSION_STRING "2.2.5-dev"
+#define MAPREADY_VERSION_STRING "2.2.5"
 #define MAPREADY_VERSION 2.2
 
 // Convert To Vector
-#define C2V_VERSION_STRING "2.2.4-dev"
+#define C2V_VERSION_STRING "2.3.0-dev"
 #define C2V_VERSION 2.2
 
 // JPL Mosaic Suite
