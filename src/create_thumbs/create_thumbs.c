666d3d7e2dfdca95196981a15783f00e1cd32f83
rgens <rgens@50fedc65-a7ff-0310-b841-aa85ec389475>
2009-08-31 20:43:20 +0000

    Did not change the asf_import call in here after changing the signature of the library function.

--- src/create_thumbs/create_thumbs.c
+++ src/create_thumbs/create_thumbs.c
@@ -907,7 +907,6 @@ void generate_level0_thumbnail(const char *file, int size, int verbose, level_0_
                        (char *)inDataName,  /* input basename         */
                        ancillary_file,      /* ancillary file         */
 		       NULL,                // colormap
-		       0,                   // classification
                        out_file);           /* output basename        */
             char *out_meta = appendExt(out_file, ".meta");
             meta_parameters *md = meta_read(out_meta);
@@ -981,7 +980,6 @@ void generate_level0_thumbnail(const char *file, int size, int verbose, level_0_
                        (char *)*dataName,   /* input basename         */
                        ancillary_file,      /* ancillary file         */
 		       NULL,                // colormap
-		       0,                   // classification
                        out_file);           /* output basename        */
             char *out_meta = appendExt(out_file, ".meta");
             meta_parameters *md = meta_read(out_meta);
@@ -1043,7 +1041,6 @@ void generate_level0_thumbnail(const char *file, int size, int verbose, level_0_
                        (char *)file,        /* input basename         */
                        ancillary_file,      /* ancillary file         */
 		       NULL,                // colormap
-		       0,                   // classification
                        out_file);           /* output basename        */
             char *out_meta = appendExt(out_file, ".meta");
             meta_parameters *md = meta_read(out_meta);
