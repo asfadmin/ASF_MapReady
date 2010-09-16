ce5419f27f63f5a88839e2d10070463de7c37729
rgens <rgens@50fedc65-a7ff-0310-b841-aa85ec389475>
2010-09-16 17:24:19 +0000

    Forgot some old tools that could be affected by the changes that I checked in yesterday. Fixed those.

--- src/remap/quadratic.c
+++ src/remap/quadratic.c
@@ -12,12 +12,6 @@ double quadratic_eval(const quadratic_2d *c,double x,double y)
 	return c->A+c->B*x+c->C*y+c->D*x*x+c->E*x*y+c->F*y*y;
 }
 
-void quadratic_write(const quadratic_2d *c,FILE *stream)
-{
-	fprintf(stream,"%.14f %.14f %.14f %.14f %.14f %.14f\n",
-		c->A,c->B,c->C,c->D,c->E,c->F);
-}
-
 void quadratic_read(quadratic_2d *c,FILE *stream)
 {
 	if (6!=
