70eeaadb508e4972dab046995b1b32548658fab5
etrochim <etrochim@50fedc65-a7ff-0310-b841-aa85ec389475>
2011-09-30 21:23:00 +0000

    [US1275] Optimized the radiometric terrain correction to execute much faster and use far less memory but using a DEM in the same geometry as the image. It works but there are still some further optimizations to make.

--- src/libasf_terrcorr/rtc.c
+++ src/libasf_terrcorr/rtc.c
@@ -2,30 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "vector.h"
 
-// kludge, for testing
-int skip_it(int kk) {
-  return kk < 1500*3482 || kk > 2100*3482;
-}
-
-static float *
-read_dem(meta_parameters *meta_dem, char *demImg)
+static void push_next_height_line(float **localHeights, meta_parameters *meta_dem, FILE *dem_fp, int ns, int line)
 {
-  int ns = meta_dem->general->sample_count;
-  int nl = meta_dem->general->line_count;
-  float *demData = MALLOC(sizeof(float)*ns*nl);
-
-  FILE *fp = FOPEN(demImg, "rb");
-
-  int ii;
-  for (ii=0; ii<nl; ++ii) {
-    get_float_line(fp, meta_dem, ii, demData + ii*ns);
-    asfLineMeter(ii,nl);
-  }
-
-  FCLOSE(fp);
-  return demData;
+  float *demLine = MALLOC(sizeof(float)*ns);
+  get_float_line(dem_fp, meta_dem, line, demLine);
+  FREE(localHeights[0]);
+  localHeights[0] = localHeights[1];
+  localHeights[1] = localHeights[2];
+  localHeights[2] = demLine;
 }
 
 static void geodetic_to_ecef(double lat, double lon, double h, Vector *v)
@@ -48,216 +35,71 @@ static void geodetic_to_ecef(double lat, double lon, double h, Vector *v)
   v->z = (a*(1-e2)/f + h)*sin_lat;
 }
 
-static int num_tot = 0;
-static int num_in = 0;
-
-static float
-bilinear_interp(float *demData, int nl, int ns, double l, double s)
-{
-
-  ++num_tot;
-
-  if (l<0 || l>=nl || s<0 || s>=ns) {
-    return 0;
-  }
-
-  ++num_in;
-
-  int ix = (int)s;
-  int iy = (int)l;
-
-  float p00 = demData[ix   + ns*(iy  )];
-  float p10 = demData[ix+1 + ns*(iy  )];
-  float p01 = demData[ix   + ns*(iy+1)];
-  float p11 = demData[ix+1 + ns*(iy+1)];
-
-  float a00 = p00;
-  float a10 = p10 - p00;
-  float a01 = p10 - a00;
-  float a11 = p00 - p01 - p10 + p11;
-
-  return a00 + a10*(s-ix) + a01*(l-iy) + a11*(s-ix)*(l-iy);
-}
-
-static float height_at(float *heights, int nl, int ns, int line, int samp)
-{
-  assert(line>=-1);
-  assert(samp>=-1);
-  assert(line<=nl);
-  assert(samp<=ns);
-  int kk = (line+1)*(ns+2) + samp+1;
-  assert(kk < (nl+2)*(ns+2));
-  return heights[kk];
-}
-
-static void llh_at(float *lats, float *lons, float *heights,
-                   int nl, int ns, int line, int samp,
-                   double *lat, double *lon, double *h)
-{
-  assert(line>=-1);
-  assert(samp>=-1);
-  assert(line<=nl);
-  assert(samp<=ns);
-  int kk = (line+1)*(ns+2) + samp+1;
-  assert(kk < (nl+2)*(ns+2));
-  *lat = (double)(lats[kk]);
-  *lon = (double)(lons[kk]);
-  *h = (double)(heights[kk]);
-}
-
-static void
-calculate_normals(meta_parameters *meta_in, meta_parameters *meta_dem,
-                  float *demData, Vector **normals_ret, Vector **pos_ret,
-                  float **heights_ret)
-{
-  int nl = meta_in->general->line_count;
-  int ns = meta_in->general->sample_count;
-  int dnl = meta_dem->general->line_count;
-  int dns = meta_dem->general->sample_count;
-
-  int map_nl = nl+2;
-  int map_ns = ns+2;
-  int size = map_nl*map_ns;
-  int ii, jj, kk;
-
-  meta_parameters *meta_tmp = meta_copy(meta_in);
-  meta_tmp->general->sample_count = map_ns;
-  meta_tmp->general->line_count = map_nl;
-
-  Vector *normals = MALLOC(sizeof(Vector)*nl*ns);
-  Vector *pos = MALLOC(sizeof(Vector)*nl*ns);
-
-  float *lats = MALLOC(sizeof(float)*size);
-  float *lons = MALLOC(sizeof(float)*size);
-  float *heights = MALLOC(sizeof(float)*size);
-  float *tmpbuf = MALLOC(sizeof(float)*map_ns);
-
-  asfPrintStatus("Calculating lat/lons ...\n");
-  FILE *fpTmp = FOPEN("lats.img", "wb");
-
-  kk=0;
-  for (ii=-1; ii<=nl; ++ii) {
-    for (jj=-1; jj<=ns; ++jj) {
-      assert(kk<size);
-      if (skip_it(kk)) {
-        lats[kk] = lons[kk] = 0;
-      } else {
-        double lat, lon;
-        meta_get_latLon(meta_in, ii, jj, 0, &lat, &lon);
-        lats[kk] = (float)lat;
-        lons[kk] = (float)lon;
-      }
-      tmpbuf[jj+1] = lats[kk];
-      ++kk;
-    }
-    asfLineMeter(ii,nl+1);
-    put_float_line(fpTmp, meta_tmp, ii+1, tmpbuf);
-  }
-  FCLOSE(fpTmp);
-  meta_write(meta_tmp, "lats.meta");
-
-  asfPrintStatus("Calculating heights ...\n");
-
-  fpTmp = FOPEN("heights.img", "wb");
-
-  kk=0;
-  for (ii=-1; ii<=nl; ++ii) {
-    for (jj=-1; jj<=ns; ++jj) {
-      assert(kk<size);
-      tmpbuf[jj+1] = 0;
-      if (skip_it(kk)) {
-        heights[kk] = 0;
-      } else {
-        double lat, lon, line, samp, dummy;
-        llh_at(lats, lons, heights, nl, ns, ii, jj, &lat, &lon, &dummy);
-        meta_get_lineSamp(meta_dem, lat, lon, 0, &line, &samp);
-        heights[kk] = bilinear_interp(demData, dnl, dns, line, samp);
-      }
-      tmpbuf[jj+1] = heights[kk];
-      ++kk;
-    }
-    asfLineMeter(ii,nl+1);
-    put_float_line(fpTmp, meta_tmp, ii+1, tmpbuf);
-  }
-  FCLOSE(fpTmp);
-  meta_write(meta_tmp, "heights.meta");
-
-  asfPrintStatus("Calculating terrain normals...\n");
-  fpTmp = FOPEN("angles.img", "wb");
-  for (jj=0; jj<ns+2; ++jj)
-    tmpbuf[jj] = 0;
-  put_float_line(fpTmp, meta_tmp, 0, tmpbuf);
-  put_float_line(fpTmp, meta_tmp, nl+1, tmpbuf);
-
-  kk=0;
-  for (ii=0; ii<nl; ++ii) {
-    tmpbuf[0] = tmpbuf[ns] = 0;
-    for (jj=0; jj<ns; ++jj) {
-      assert(kk<nl*ns);
-      if (skip_it(kk)) {
-        normals[kk].x = normals[kk].y = 0; normals[kk].z = 1;
-        tmpbuf[jj+1] = 0;
-      } else {
-        Vector v1, v2, v;
-        double lat, lon, h;
-
-        llh_at(lats, lons, heights, nl, ns, ii-1, jj, &lat, &lon, &h);
-        geodetic_to_ecef(lat, lon, h, &v1);
-        llh_at(lats, lons, heights, nl, ns, ii+1, jj, &lat, &lon, &h);
-        geodetic_to_ecef(lat, lon, h, &v);
-        vector_subtract(&v1, &v);
-
-        llh_at(lats, lons, heights, nl, ns, ii, jj-1, &lat, &lon, &h);
-        geodetic_to_ecef(lat, lon, h, &v2);
-        llh_at(lats, lons, heights, nl, ns, ii, jj+1, &lat, &lon, &h);
-        geodetic_to_ecef(lat, lon, h, &v);
-        vector_subtract(&v2, &v);
-
-        Vector *n = vector_cross(&v2, &v1);
-        vector_multiply(n, 1./vector_magnitude(n));
-
-        llh_at(lats, lons, heights, nl, ns, ii, jj, &lat, &lon, &h);
-        geodetic_to_ecef(lat, lon, h, &pos[kk]);
-
-        geodetic_to_ecef(lat, lon, h, &v1);
-        geodetic_to_ecef(lat, lon, h+100, &v);
-        vector_subtract(&v1,&v);
-        vector_multiply(&v1, 1./vector_magnitude(&v1));
-
-        normals[kk] = *n;
-        double ang = acos(vector_dot(n,&v1));
-        tmpbuf[jj+1] = ang*R2D;
-
-        vector_free(n);
-      }
-      ++kk;
-    }
-    asfLineMeter(ii, nl);
-    put_float_line(fpTmp, meta_tmp, ii+1, tmpbuf);
-  }
-  FCLOSE(fpTmp);
-  meta_write(meta_tmp, "angles.meta");
-
-  FREE(lats);
-  FREE(lons);
-  FREE(tmpbuf);
-  meta_free(meta_tmp);
-
-  *normals_ret = normals;
-  *pos_ret = pos;
-  *heights_ret = heights;
-}
-
 static Vector get_satpos(meta_parameters *meta, int line)
 {
   int ns = meta->general->sample_count;
-  double lat, lon, h;
-  meta_get_latLon(meta, line, ns/2, 0, &lat, &lon);
-  h = meta_get_sat_height(meta,line,ns/2);
+  double t = meta_get_time(meta, line, ns/2);
+
+  // find the state vector closest to the specified time
+  int ii,closest_ii=0;
+  double closest_diff=9999999;
+  for (ii=0; ii<meta->state_vectors->vector_count; ++ii) {
+    double diff = fabs(meta->state_vectors->vecs[ii].time - t);
+    if (diff < closest_diff) {
+      closest_ii = ii;
+      closest_diff = diff;
+    }
+  }
+
+  stateVector closest_vec = meta->state_vectors->vecs[closest_ii].vec;
+  double closest_time = meta->state_vectors->vecs[closest_ii].time;
+
+  stateVector stVec = propagate(closest_vec, closest_time, t);
 
   Vector satpos;
-  geodetic_to_ecef(lat, lon, h, &satpos);
+  satpos.x = stVec.pos.x;
+  satpos.y = stVec.pos.y;
+  satpos.z = stVec.pos.z;
   return satpos;
+}
+
+static Vector get_position(float **localHeights, meta_parameters *meta_in, int line, int samp)
+{
+  double lat, lon;
+  Vector v;
+  meta_get_latLon(meta_in, line, samp, 0, &lat, &lon);
+  geodetic_to_ecef(lat, lon, localHeights[1][samp], &v);
+  return v;
+}
+
+static Vector * calculate_normal(meta_parameters *meta, float **localHeights, int line, int sample)
+{
+  double lat, lon, h;
+  Vector v1, v2, v;
+  Vector *normal;
+
+  h = localHeights[0][sample];
+  meta_get_latLon(meta, line-1, sample, 0, &lat, &lon); 
+  geodetic_to_ecef(lat, lon, h, &v1);
+
+  h = localHeights[2][sample];
+  meta_get_latLon(meta, line+1, sample, 0, &lat, &lon); 
+  geodetic_to_ecef(lat, lon, h, &v);
+  vector_subtract(&v1, &v);
+
+  h = localHeights[1][sample-1];
+  meta_get_latLon(meta, line, sample-1, 0, &lat, &lon); 
+  geodetic_to_ecef(lat, lon, h, &v2);
+
+  h = localHeights[1][sample+1];
+  meta_get_latLon(meta, line, sample+1, 0, &lat, &lon); 
+  geodetic_to_ecef(lat, lon, h, &v);
+  vector_subtract(&v2, &v);
+
+  normal = vector_cross(&v2, &v1);
+  vector_multiply(normal, 1./vector_magnitude(normal));
+  
+  return normal;
 }
 
 static float
@@ -337,47 +179,50 @@ int rtc(char *input_file, char *dem_file, int maskFlag, char *mask_file,
   meta_parameters *meta_dem = meta_read(demMeta);
   if (!meta_dem) asfPrintError("Failed to read metadata: %s\n", demMeta);
   meta_parameters *meta_corr = NULL;
-  if (corrImg) meta_corr = meta_read(inputMeta);
-
-  asfPrintStatus("Reading DEM...\n");
-  float *demData = read_dem(meta_dem, demImg);
+  if (corrImg) meta_corr = meta_read(demMeta);
 
   int ns = meta_in->general->sample_count;
   int nl = meta_in->general->line_count;
   int nb = meta_in->general->band_count;
-  int ii, jj, kk;
+  int dns = meta_dem->general->sample_count;
+  int dnl = meta_dem->general->line_count;
+  assert(ns == dns);
+  assert(nl == dnl);
+
+  float **localHeights = MALLOC(sizeof(float*)*3);
+  memset(localHeights, 0, sizeof(float*)*3);
 
   FILE *fpIn = FOPEN(inputImg, "rb");
   FILE *fpOut = FOPEN(outputImg, "wb");
   FILE *fpCorr = NULL;
   if (corrImg) fpCorr = FOPEN(corrImg, "wb");
+  FILE *dem_fp = FOPEN(demImg, "rb");
 
   float *corr = MALLOC(sizeof(float)*ns);
   float *bufIn = MALLOC(sizeof(float)*ns);
 
-  asfPrintStatus("Interpolating DEM ...\n");
-  Vector *normals, *pos;
-  float *heights;
-  calculate_normals(meta_in, meta_dem, demData, &normals, &pos, &heights);
-  FREE(demData);
+  int ii, jj, kk;
+  for(ii = 1; ii < 3; ++ii) {
+    float *demLine = MALLOC(sizeof(float)*dns);
+    get_float_line(dem_fp, meta_dem, ii - 1, demLine);
+    localHeights[ii] = demLine;
+  }
 
-  asfPrintStatus("Performing radiometric correction...\n");
+  // We aren't applying the correction to the edges of the image
+  for(kk = 0; kk < nb; ++kk) {
+    get_band_float_line(fpIn, meta_in, kk, 0, bufIn);
+    put_band_float_line(fpOut, meta_out, kk, 0, bufIn);
+  }
 
-  for (ii=0; ii<nl; ++ii) {
-    Vector sat_pos = get_satpos(meta_in, ii);
-    for (jj=0; jj<ns; ++jj) {
-      kk = ii*ns + jj;
-      if (skip_it(kk)) {
-        corr[jj] = 1;
-      } else {
-        float h = height_at(heights, nl, ns, ii, jj);
-        Vector *n = normals + kk;
-        Vector *p = pos + kk;
-        corr[jj] = calculate_correction(meta_in, ii, jj, h, &sat_pos, n, p);
-        if (ii==6617) {
-          printf("%d: %f\n", jj, corr[jj]);
-        }
-      }
+  for(ii = 1; ii < nl - 1; ++ii) {
+    push_next_height_line(localHeights, meta_dem, dem_fp, dns, ii+1);
+    corr[0] = corr[ns-1] = 1;
+    Vector satpos = get_satpos(meta_in, ii);
+    for(jj = 1; jj < ns - 1; ++jj) {
+      Vector * normal = calculate_normal(meta_in, localHeights, ii, jj);
+      Vector position = get_position(localHeights, meta_in, ii, jj);
+      corr[jj] = calculate_correction(meta_in, ii, jj, localHeights[1][jj], &satpos, normal, &position);
+      vector_free(normal);
     }
 
     if (corrImg)
@@ -392,6 +237,17 @@ int rtc(char *input_file, char *dem_file, int maskFlag, char *mask_file,
 
     asfLineMeter(ii, nl);
   }
+
+    // We aren't applying the correction to the edges of the image
+  for(kk = 0; kk < nb; ++kk) {
+    get_band_float_line(fpIn, meta_in, kk, nl-1, bufIn);
+    put_band_float_line(fpOut, meta_out, kk, nl-1, bufIn);
+  }
+
+  for(ii = 0; ii < 3; ++ii) {
+    FREE(localHeights[ii]);
+  }
+  FREE(localHeights);
 
   FCLOSE(fpOut);
   FCLOSE(fpIn);
@@ -410,8 +266,6 @@ int rtc(char *input_file, char *dem_file, int maskFlag, char *mask_file,
 
   FREE(bufIn);
   FREE(corr);
-  FREE(normals);
-  FREE(pos);
 
   FREE(inputImg);
   FREE(inputMeta);
@@ -422,8 +276,6 @@ int rtc(char *input_file, char *dem_file, int maskFlag, char *mask_file,
   FREE(maskImg);
   FREE(maskMeta);
 
-  printf("Coverage: %.2f\n", 100.*num_in/(float)num_tot);
   return TRUE;
 }
-
 
