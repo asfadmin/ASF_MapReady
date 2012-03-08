206dc98f00f5583b01630a9ab83329327a022b8f
khogenso <khogenso@50fedc65-a7ff-0310-b841-aa85ec389475>
2012-03-08 20:01:47 +0000

    [de553] Got the pixel size wrong for EL1, it is 12.5m

--- src/asf_meta/meta_init_ceos.c
+++ src/asf_meta/meta_init_ceos.c
@@ -353,12 +353,12 @@ void ceos_init_sar_general(ceos_description *ceos, const char *in_fName,
   meta->general->x_pixel_size     = dssr->pixel_spacing;
   meta->general->y_pixel_size     = dssr->line_spacing;
   if (dssr->pixel_spacing == 0.0 && strncmp(dssr->beam1, "EL1", 3)==0) {
-    asfPrintWarning("No range pixel size specified for EL1, using 30m\n");
-    meta->general->x_pixel_size = 30;
+    asfPrintWarning("No range pixel size specified for EL1, using 12.5m\n");
+    meta->general->x_pixel_size = 12.5;
   }
   if (dssr->line_spacing == 0.0 && strncmp(dssr->beam1, "EL1", 3)==0) {
-    asfPrintWarning("No azimuth pixel size specified for EL1, using 30m\n");
-    meta->general->y_pixel_size = 30;
+    asfPrintWarning("No azimuth pixel size specified for EL1, using 12.5m\n");
+    meta->general->y_pixel_size = 12.5;
   }
   meta->general->center_latitude  = dssr->pro_lat;
   meta->general->center_longitude = dssr->pro_long;
