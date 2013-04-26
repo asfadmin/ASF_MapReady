011f2da937c6057bf6631e5465356184222785f3
Rudi Gens <rgens@alaska.edu>
2013-04-25 19:03:02 -0800

    Minor edits.

--- src/measures2csv/measures2csv.c
+++ src/measures2csv/measures2csv.c
@@ -129,12 +129,11 @@ void usage(char *name)
 {
   printf("\n"
 	 "USAGE:\n"
-	 "   %s [ -log <logFile> ] <measures> <csv> <grid>\n",name);
+	 "   %s [ -log <logFile> ] <measures> <csv>\n",name);
   printf("\n"
 	 "REQUIRED ARGUMENTS:\n"
 	 "   measures   MEaSUReS data file\n"
-	 "   csv        data dump\n"
-	 "   grid       initial grid location\n");
+	 "   csv        data dump\n");
   printf("\n"
 	 "DESCRIPTION:\n"
 	 "   %s converts an RGPS MEaSUReS file into a csv file.\n",
