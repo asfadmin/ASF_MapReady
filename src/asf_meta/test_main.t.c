8041fd826ca090dd961f5a2fc79079f046fb3c9c
Kirk Hogenson <kahogenson@alaska.edu>
2013-09-10 23:22:09 +0000

    Unit tests for the date routines.

--- src/asf_meta/test_main.t.c
+++ src/asf_meta/test_main.t.c
@@ -5,6 +5,8 @@ void test_meta_get_latLon();
 void test_meta_get_lineSamp();
 void test_read_proj_file();
 void test_meta_read();
+void test_date();
+void test_longdate();
 
 int main()
 {
@@ -25,6 +27,8 @@ int main()
    if ((NULL == CU_add_test(pSuite, "xml", test_xml)) ||
        //(NULL == CU_add_test(pSuite, "read_proj_file", test_read_proj_file)) ||
        (NULL == CU_add_test(pSuite, "meta_read", test_meta_read)) ||
+       (NULL == CU_add_test(pSuite, "date", test_date)) ||
+       (NULL == CU_add_test(pSuite, "longdate", test_longdate)) ||
        (NULL == CU_add_test(pSuite, "meta_get_latLon", test_meta_get_latLon)) ||
        (NULL == CU_add_test(pSuite, "meta_get_lineSamp", test_meta_get_lineSamp)))
    {
