c9ccccb0aa64385bd9fad194472f036d10d90608
Rudiger Gens <rudigens@Rudis-MacBook-Pro.local>
2013-12-10 15:03:52 -0900

    Removed convert2roipac tool (not needed any longer). Removed XSLT library header references from export_hdf.c.

--- src/libasf_export/export_hdf.c
+++ src/libasf_export/export_hdf.c
@@ -31,13 +31,8 @@
 #include <libxml/debugXML.h>
 #include <libxml/HTMLtree.h>
 #include <libxml/xmlIO.h>
-#include <libxml/DOCBparser.h>
 #include <libxml/xinclude.h>
 #include <libxml/catalog.h>
-#include <libxslt/xslt.h>
-#include <libxslt/xsltInternals.h>
-#include <libxslt/transform.h>
-#include <libxslt/xsltutils.h>
 #include <xml_util.h>
 
 #define RES 16
